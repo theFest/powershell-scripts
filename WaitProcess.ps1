9f17294363e8251df96bb2cd7bce098cfa32c7f3
Fest White <stjepanb83@gmail.com>
2021-05-26 20:52:18 +0200

    Update WaitProcess.ps1
    Interval check and timeout.

--- WaitProcess.ps1
+++ WaitProcess.ps1
@@ -7,12 +7,17 @@
     This function waits for a process and it's interactive window to start.
     
     .PARAMETER Time
-    Mandatory - retry timeframe in seconds before script quits.
+    Mandatory - retry timeframe in seconds before script timeouts/quits.
     .PARAMETER Process
-    Mandatory - process and it's interactive window that you want to wait for, finally returns path.
+    Mandatory - process and it's interactive window that you want to wait for.
+    .PARAMETER CheckInterval
+    Mandatory - interval between every next check, default is 1 second.
+
+    .NOTES
+    After successful check, process path is returned.
 
     .EXAMPLE
-    WaitProcess -Time 120 -Process regedit
+    WaitProcess -Time 120 -Process mspaint -CheckInterval 10
     #>
     [CmdletBinding()]
     Param(
@@ -20,29 +25,35 @@
         [string]$Time,
 
         [Parameter(Mandatory = $true)]
-        [string]$Process
+        [string]$Process,
+
+        [Parameter(Mandatory = $false)]
+        [string]$CheckInterval
     )
     $TimeSpan = New-TimeSpan -Seconds $Time
     $StopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
     $StopWatch.Start()
     do {
-        Write-Output -InputObject 'Checking if process|main window is running...'
+        Write-Output -InputObject 'Checking if Process and/or MainWindow is running...'
         $WaitProcess = $(Get-Process -Name $Process -ErrorAction SilentlyContinue)
         if ($WaitProcess) {
             Write-Output -InputObject ('Process is running in background, PID {0}' -f $WaitProcess.Id)
         }
         elseif (!$WaitProcess) { 
-            Write-Output -InputObject ('Process is not running.')
+            Write-Output -InputObject ('Process is not running!')
         }
         elseif (!$WaitProcess.Responding) {
-            Write-Output -InputObject ('Process is not responding.')
+            Write-Output -InputObject ('Process is not responding!')
         }
-        Start-Sleep -Seconds 5
+        Start-Sleep -Seconds $CheckInterval
     }
     until (($WaitProcess.MainWindowTitle) -or ($StopWatch.Elapsed -ge $TimeSpan))
     if ($WaitProcess) {
         $WaitProcess.Refresh()
         Write-Output -InputObject ('Process main window is now running, PID {0}' -f $WaitProcess.Id)
+    }
+    else {
+        Write-Output -InputObject ('Process check has timed out')
     }
     $StopWatch.Stop()
     return $WaitProcess.Path
