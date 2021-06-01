cb7359f0a14685be938eb2a824a41c6d463bb0d0
Fest White <stjepanb83@gmail.com>
2021-06-01 09:48:19 +0200

    Initial GetFileS3
    See SYNOPSIS/DESCRIPTION

---
+++ GetFileS3.ps1
@@ -0,0 +1,59 @@
+﻿Function GetFileS3 {
+    <#
+    .SYNOPSIS
+    This function downloads file from Internet.
+    
+    .DESCRIPTION
+    This function downloads file from Internet. It can be S3 bucket or any other source.
+    
+    .PARAMETER Key
+    NotMandatory - credentials to authentificate against some source to be able to download.
+    .PARAMETER FileUrl
+    Mandatory - URL to the file on S3 bucket or source from you want to download.
+    .PARAMETER DestinationPath
+    NotMandatory - path to location where you want the file to be saved.
+
+    .NOTES
+    If absent, content will be downloaded in "$env:TEMP"
+    Local and remote sources will be compared if same filename is present on disk.
+    Finally it will return time taken in for download in a format of HH/MM/SS
+    *for reusability purposes, remove following: WriteLog(declare_at_the_end)
+
+    .EXAMPLE
+    GetFileS3 -Key $Key -FileUrl $your_url -DestinationPath $your_data_path
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $false)]
+        [string]$Key,
+
+        [Parameter(Mandatory = $true)]
+        [string]$FileUrl,
+
+        [Parameter(Mandatory = $false)]
+        [string]$DestinationPath
+    )
+    $StartTime = Get-Date
+    if (!$DestinationPath) {
+        $DestinationPath = $env:TEMP
+    }
+    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls, ssl3"
+    $FileName = [System.IO.Path]::GetFileName($FileUrl)
+    $DownloadFilePath = [System.IO.Path]::Combine($DestinationPath, $FileName)
+    $LocalPath = (Get-Item $DownloadFilePath -ErrorAction SilentlyContinue).Length
+    $RemotePath = (Invoke-WebRequest $FileUrl -UserAgent $Key -UseBasicParsing -Method Head -ErrorAction SilentlyContinue).Headers.'Content-Length'
+    if ([System.IO.File]::Exists($DownloadFilePath) -and $LocalPath -eq $RemotePath) {
+        Write-Output -InputObject ('File already exists. Local and remote files match, exiting.')
+        #WriteLog -LogMessage "File already exists. Local and remote files match." -LogSeverity Information
+    }
+    else {
+        Write-Output -InputObject ('File missing or local and remote files do not match, downloading...')
+        #WriteLog -LogMessage "File missing or local and remote files do not match!" -LogSeverity Warning
+        Remove-Item -Path $DownloadFilePath -ErrorAction SilentlyContinue
+        $DlClient = New-Object System.Net.WebClient;
+        $DlClient.Headers.Add("User-Agent", "$Key");   
+        $DlClient.DownloadFile($FileUrl, $DownloadFilePath);
+        $DlClient.Dispose()
+    }
+    return "Time taken to download; $((Get-Date).Subtract($StartTime).Duration() -replace ".{8}$")"
+}
