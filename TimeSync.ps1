a6fae6bd32bd478c00e1e87c8e77d1e6eefe61b6
Fest White <stjepanb83@gmail.com>
2021-06-27 01:30:52 +0200

    Update TimeSync.ps1
    Service install feature, paramaer for StartupType and output.

--- TimeSync.ps1
+++ TimeSync.ps1
@@ -4,13 +4,33 @@ Function TimeSync {
     This function is syncing time.
     
     .DESCRIPTION
-    This function starts Windows Time service, sets it to auto and syncs time.
-    
+    This function starts Windows Time service. Creates a service if one is not present.
+    After it sets it service to automatic by default, then resyncs time and outputs the results.
+
     .EXAMPLE
-    TimeSync
+    TimeSync -StartupType Automatic
     #>
-    $Time = Get-Service -Name W32Time
-    $Time | Set-Service -StartupType Automatic -PassThru | Start-Service
-    Start-Process W32tm -ArgumentList " /resync /force" -Wait -WindowStyle Hidden
-    Write-Host "Time sync finished."
+    [CmdletBinding()]
+    Param(
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet('Automatic', 'Boot', 'Disabled', 'Manual', 'System')]
+        [string]$StartupType = 'Automatic'
+    )
+    BEGIN {
+        if (!(Get-Service -Name W32Time -ErrorAction SilentlyContinue)) {
+            Write-Output 'Service is missing, installing and starting.'
+            Start-Process W32tm -ArgumentList " /register" -WindowStyle Hidden -Wait
+            Net Start W32Time
+        }
+    }
+    PROCESS {
+        Write-Output 'Setting service to Automatic and syncing time.'
+        Get-Service -Name W32Time | Set-Service -StartupType $StartupType
+        Start-Process W32tm -ArgumentList " /resync /force" -WindowStyle Hidden -Wait  
+        
+    }
+    END {
+        return W32tm /query /status
+        Write-Output 'Time sync finished.'
+    }
 }
