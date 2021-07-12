3dc9488485135f2d87fecd989b878f37835f1d55
Fest White <stjepanb83@gmail.com>
2021-07-12 09:51:37 +0200

    Initial CIMSessionExecution
    See SYNOPSIS/DESCRIPTION

---
+++ CIMSessionExecution.ps1
@@ -0,0 +1,69 @@
+﻿Function CIMSessionExecution {
+    <#
+    .SYNOPSIS
+    Execute scripts via CIM session.
+    
+    .DESCRIPTION
+    This function for example can be used to execute script, also provides output from remote computer.
+    
+    .PARAMETER User
+    Not mandatory - user of the target machine.    
+    .PARAMETER Password
+    Not mandatory - pass on target machine.   
+    .PARAMETER Computer
+    Mandatory - hostname of remote computer.   
+    .PARAMETER Class
+    NotMandatory - choose class.    
+    .PARAMETER Method
+    Not mandatory - choose class method.   
+    .PARAMETER ExecuteCommand
+    Not mandatory - execute or terminate your command on a remote computer.
+    
+    .EXAMPLE
+    CIMSessionExecution -User "your_user" -Password "your_pass" -Computer "your_computer_hostname" -ExecuteCommand 'powershell -executionpolicy bypass -Command "Invoke-WebRequest https://path_to_your_script/your_script.ps1 -UserAgent "your_secret" -UseBasicParsing -OutFile $env:TEMP\Script.ps1; & Invoke-Expression $env:TEMP\Script.ps1'
+    
+    .NOTES
+    https://docs.microsoft.com/en-us/windows/win32/wmisdk/cimclas
+    https://www.activexperts.com/admin/scripts/wmi/
+    https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/terminate-method-in-class-win32-process
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false)]
+        [string]$User,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Password,
+ 
+        [Parameter(Mandatory = $true)]
+        [string]$Computer,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('Process', 'Property')]
+        [string]$Class = 'Process',
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('Create', 'Terminate')]
+        [string]$Method = 'Create',
+
+        [Parameter(Mandatory = $true)]
+        [string]$ExecuteCommand
+    )
+    $Pass = ConvertTo-SecureString -AsPlainText $Password -Force
+    $SecureCredentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $Pass
+    $SessionArgs = @{
+        ComputerName  = $Computer 
+        Credential    = Get-Credential -Credential $SecureCredentials
+        SessionOption = New-CimSessionOption -Protocol Dcom
+
+    }
+    $MethodArgs = @{
+        ClassName  = "Win32_$Class"
+        MethodName = "$Method"
+        CimSession = New-CimSession @SessionArgs -Verbose
+        Arguments  = @{
+            CommandLine = $ExecuteCommand
+        }
+    }
+    Invoke-CimMethod @MethodArgs
+}
