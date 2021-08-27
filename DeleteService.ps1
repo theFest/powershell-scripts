ea47f53c5dd6f15da7b6c5d946d0d87164a5aa04
Fest White <stjepanb83@gmail.com>
2021-08-27 19:47:29 +0200

    Initial DeleteService
    See SYNOPSIS/DESCRIPTION

---
+++ DeleteService.ps1
@@ -0,0 +1,28 @@
+﻿Function DeleteService {
+    <#
+    .SYNOPSIS
+    Windows service removal script.
+    
+    .DESCRIPTION
+    With this funtion you can delete Windows services.
+    
+    .PARAMETER ServiceName
+    Mandator - declare name of the service you want to delete.
+    
+    .EXAMPLE
+    DeleteService -ServiceName spooler
+    
+    .NOTES
+    v1
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$ServiceName
+    )
+    $Service = Get-WmiObject -Class Win32_Service -Filter "Name='$ServiceName'"
+    $Service.StopService()
+    Start-Sleep -Seconds 1
+    $Service.Delete()
+    $Service.Dispose()
+}
