218eccebb9d4fcc96e1b439ef5abfbb0c93029f3
Fest White <stjepanb83@gmail.com>
2021-10-29 00:03:40 +0200

    Initial RenameFiles
    See SYNOPSIS/DESCRIPTION

---
+++ RenameFiles.ps1
@@ -0,0 +1,128 @@
+﻿Function RenameFiles {
+    <#
+    .SYNOPSIS
+    Rename your data.
+    
+    .DESCRIPTION
+    Simple function for renaming files, folders, extensions and more.
+    
+    .PARAMETER FilesPath
+    Mandatory - location of your data.  
+    .PARAMETER RenameFiles
+    NotMandatory - batch of files to be renamed.
+    .PARAMETER ReplaceStringIn
+    NotMandatory - declare string that you want to be removed.  
+    .PARAMETER ReplaceStringOut
+    NotMandatory - declare string that you want to be added.
+    .PARAMETER AddNewPrefix
+    NotMandatory - declare prefix string that you want to be added. 
+    .PARAMETER AddNewSuffix
+    NotMandatory - declare suffix string that you want to be added.  
+    .PARAMETER Substring
+    NotMandatory - define substring that you want to be removed.  
+    .PARAMETER PSIsContainer
+    NotMandatory - rename either files or folders with this switch.  
+    .PARAMETER Methods
+    NotMandatory - currently use available methods to upper/lower case your files. 
+    .PARAMETER ReplaceExtensionTo
+    NotMandatory - choose extension that you wish to replace.  
+    .PARAMETER Manage
+    Mandatory - choose operation that you want to use.
+    .PARAMETER OutputToFile
+    NotMandatory - if you want result to be written to file, use this switch together with Outfile. 
+    .PARAMETER OutFile
+    NotMandatory - define location where your output file will be populated and resides.
+    
+    .EXAMPLE
+    RenameFiles -FilesPath 'F:\Test\a' -Manage RenameWithIndex -RenameFiles test ##-->RenameFiles
+    RenameFiles -FilesPath 'F:\Test\a' -Manage ChangeExtension -ReplaceExtensionTo .csv ##-->ChangeExtension
+    RenameFiles -FilesPath 'F:\Test\a' -Manage ReplaceString -ReplaceStringIn 'test_' -ReplaceStringOut 'demo_' ##-->ReplaceString
+    RenameFiles -FilesPath 'F:\Test\a' -Manage SetNewPrefixSuffix -AddNewPrefix YYYYY -AddNewSuffix ZZZZZ ##-->SetNewPrefixSuffix
+    RenameFiles -FilesPath 'F:\Test\a' -Manage MethodOptions -Methods ToLower ##-->MethodOptions
+    RenameFiles -FilesPath 'F:\Test\a' -Manage SubString -Substring 1 ##-->SubString
+    * also you can use $OutputToFile switch with $OutFile parametar to write results to a file.
+
+    .NOTES
+    v1
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string]$FilesPath,
+
+        [Parameter(Mandatory = $false)]
+        [string]$RenameFiles = $false,
+
+        [Parameter(Mandatory = $false)]
+        [string]$ReplaceStringIn,
+
+        [Parameter(Mandatory = $false)]
+        [string]$ReplaceStringOut,
+
+        [Parameter(Mandatory = $false)]
+        [string]$AddNewPrefix,
+
+        [Parameter(Mandatory = $false)]
+        [string]$AddNewSuffix,
+
+        [Parameter(Mandatory = $false)]
+        [int]$Substring = $null,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$PSIsContainer,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('ToLower', 'ToUpper')]
+        [string]$Methods,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('.ps1', '.txt', '.csv', '.ini', '.json', '.exe', '.xml', '.jpg', '.msu', '.log', '.bin')]
+        [string]$ReplaceExtensionTo,
+
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateSet('RenameWithIndex', 'ChangeExtension', 'ReplaceString', 'MethodOptions', 'SubString', 'SetNewPrefixSuffix')]
+        [string]$Manage,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$OutputToFile,
+
+        [Parameter(Mandatory = $false)]
+        [string]$OutFile
+    )
+    Set-Location -Path $FilesPath -Verbose
+    #$Files = Get-ChildItem -Path $FilesPath -File:$FilesOnly -Filter $Filter -Include $Include -Exclude $Exclude -ReadOnly:$ReadOnly -Hidden:$Hidden -System:$System -Recurse:$Recurse -Depth $Depth | Where-Object { $_.PSIsContainer -eq $PSIsContainer }
+    $Files = Get-ChildItem -Path $FilesPath | Where-Object { $_.PSIsContainer -eq $PSIsContainer }
+    switch ($Manage) {
+        'RenameWithIndex' {
+            $WO = Write-Output "`nTotal : "$Files.Count "Files renamed to; $RenameFiles`n"
+            $AddExtension = $Files.Extension | Select-Object -First 1
+            $F = $Files | ForEach-Object -Begin { $Count = 1 } -Process { Rename-Item $_ -NewName $RenameFiles$Count$AddExtension -PassThru -Verbose; $Count++ } -End { Tee-Object -InputObject $WO -Variable 'RenamedFiles' | Out-Null }
+        }
+        'ChangeExtension' {
+            $WO = Write-Output "`nTotal : "$Files.Count "Files have changed extension to; $ReplaceExtensionTo`n"
+            $F = $Files | ForEach-Object -Process { Rename-Item $_ -NewName ([System.IO.Path]::ChangeExtension($_, "$ReplaceExtensionTo")) -PassThru -Verbose } -End { Tee-Object -InputObject $WO -Variable 'RenamedFiles' | Out-Null }
+        }
+        'ReplaceString' {
+            $WO = Write-Output "`nTotal : "$Files.Count "Files that have replaced string; $ReplaceStringIn>>$ReplaceStringOut`n"
+            $F = $Files | ForEach-Object -Process { Rename-Item $_ -NewName $_.Name.Replace("$ReplaceStringIn", "$ReplaceStringOut") -PassThru -Verbose } -End { Tee-Object -InputObject $WO -Variable 'RenamedFiles' | Out-Null }
+        }
+        'SetNewPrefixSuffix' {
+            $WO = Write-Output "`nTotal : "$Files.Count "Files that changed Prefix;$AddNewPrefix and/or suffix; $AddNewSuffix`n"
+            $F = $Files | ForEach-Object -Process { Rename-Item -Path $_ -NewName "$AddNewPrefix$($_.BaseName)$AddNewSuffix$($_.Extension)" -PassThru -Verbose } -End { Tee-Object -InputObject $WO -Variable 'RenamedFiles' | Out-Null }
+        }
+        'MethodOptions' {
+            $WO = Write-Output "`nTotal : "$Files.Count "Files that changed method; $Methods`n"
+            $F = $Files | Rename-Item -NewName { $_.BaseName.$Methods() + $_.Extension } -PassThru -Verbose
+            Tee-Object -InputObject $WO -Variable 'RenamedFiles' | Out-Null
+        }
+        'SubString' {
+            $WO = Write-Output "`nTotal : "$Files.Count "Files that have added substring; $Substring`n"
+            $F = $Files | Rename-Item -NewName { $_.BaseName.Substring($Substring) + $_.Extension } -PassThru -Verbose
+            Tee-Object -InputObject $WO -Variable 'RenamedFiles' | Out-Null
+        }
+    }
+    Write-Host $RenamedFiles
+    if ($OutputToFile.IsPresent) {
+        $F | Out-File $OutFile
+    }
+}
