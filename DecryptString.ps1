dea39b930f0ee01cc4116d1c859876981b1a0fe7
Fest White <stjepanb83@gmail.com>
2021-11-18 00:10:19 +0100

    Update DecryptString
    Define or generate switch, refactor.

--- DecryptString.ps1
+++ DecryptString.ps1
@@ -4,10 +4,12 @@
     Decrypt a string with it's password.
     
     .DESCRIPTION
-    With this simple function you can decrypt a string, mandatory to reuse defined Key from a encrypt form.
+    With this simple function you can decrypt a string, mandatory to reuse defined or generated key from a encrypt form.
     
-    .PARAMETER Key
-    Mandatory - your password with which you will decrypt your string. 
+    .PARAMETER GeneratedKey
+    NotMandatory - your randomly generated password with which you will decrypt your string.
+    .PARAMETER PreDefinedKey
+    NotMandatory - your predefined password with which you will decrypt your string. 
     .PARAMETER EncryptedString
     Mandatory - actual string you wanna to decrypt with your corresponding key. 
     .PARAMETER KeySize
@@ -20,15 +22,19 @@
     NotMandatory - choose padding mode, default is set to Zeros.
     
     .EXAMPLE
-    DecryptString -Key 'my_private_password' -EncryptedString 'oqbET916P0HPwYhepHqP3SPF3bd2qageFJgR2/oxpHI='
-    
+    DecryptString -PreDefinedKey 'my_private_password' -EncryptedString 'oqbET916P0HPwYhepHqP3SPF3bd2qageFJgR2/oxpHI='
+    DecryptString -GeneratedKey 'n+FnVLTeF0sFKbRonuzgz7ECSQb776wdvsnpSx1q7z4=' -EncryptedString 'R7M8uqAjkI15H9ha9Hxx3WFNlQoli+hl7Gg6vZeAa1Z6G4avLhMxLziC8IYkQLpb'
+
     .NOTES
-    v1
+    v2
     #>
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $true)]
-        [string]$Key,
+        [Parameter()]
+        [string]$GeneratedKey,
+
+        [Parameter()]
+        [string]$PreDefinedKey,
 
         [Parameter(Mandatory = $true)]
         [string]$EncryptedString,
@@ -40,7 +46,7 @@
         [int]$BlockSize = 128,
 
         [Parameter(Mandatory = $false)]
-        [ValidateSet('CBC', 'CFB', 'CTS', 'ECB', 'OFB', 'CTS', 'CTS', 'CTS')]
+        [ValidateSet('CBC', 'CFB', 'CTS', 'ECB', 'OFB')]
         $Mode = 'CBC',
 
         [Parameter(Mandatory = $false)]
@@ -56,7 +62,14 @@
         $AesManaged.Padding = [System.Security.Cryptography.PaddingMode]::$Padding
         $AesManaged.BlockSize = $BlockSize
         $AesManaged.KeySize = $KeySize
-        $AesManaged.Key = $ShaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Key))
+        switch ($PSBoundParameters.Keys) {
+            'GeneratedKey' {
+                $AesManaged.Key = [System.Convert]::FromBase64String($GeneratedKey)
+                }
+            'PreDefinedKey' {
+                $AesManaged.Key = $ShaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($PreDefinedKey))
+            }
+        }
         $CipherBytes = [System.Convert]::FromBase64String($EncryptedString)
         $AesManaged.IV = $CipherBytes[0..15]
         $Decryptor = $AesManaged.CreateDecryptor()
@@ -65,7 +78,13 @@
     }
     END {
         $AesManaged.Dispose()
-        Write-Output "Plaintext string: $Key"
-        Write-Output "Decrypted string: $PublicKey"
+        if ($GeneratedKey) {
+            Write-Host "PreGenerated string: $GeneratedKey" -ForegroundColor Yellow
+        }
+        if ($PreDefinedKey) {
+            Write-Host "PreDefined string: $PreDefinedKey" -ForegroundColor DarkYellow
+        }
+        Write-Host "Decrypted string: $PublicKey" -ForegroundColor Green
+        Clear-History -Confirm
     }
 }
