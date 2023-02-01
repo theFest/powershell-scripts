f5949610b28918a2f3448fa8aa0ec4e819183704
theFest <stjepanb83@gmail.com>
2023-02-01 23:03:58 +0100

    small fixes
    AddUsers renamed to AddUsersToGroup

--- PowerShell AD/AddUsersToGroup.ps1
+++ PowerShell AD/AddUsersToGroup.ps1
@@ -1,5 +1,5 @@
-#requires -version 3.0
-Function AddUsers {
+#requires -version 5.1
+Function AddUsersToGroup {
     <#
     .SYNOPSIS
     Function for adding user/users to Active Directory groups.
@@ -8,40 +8,39 @@ Function AddUsers {
     With this function you can add user/users to AD group either via pipeline or by importing csv file.
     
     .PARAMETER Users
-    Mandatory - users that you want to add.
+    Mandatory - users(identity or identites) that you want to add.
     .PARAMETER FromCSV
-    NotMandatory - add users from .csv file.   
+    NotMandatory - add users(identites from file) from .csv file.   
     .PARAMETER Filter
     NotMandatory - choose filter, SAM account name is predifined.  
     .PARAMETER GroupName
     Mandatory - Active Directory group in which users will be added.  
     .PARAMETER Delimiter
-    NotMandatory - delimiter present in csv file.
+    NotMandatory - delimiter present in csv file, if your delimiter is not comma, choose the one used in csv file.
     
     .EXAMPLE
-    "your_AD_group" | AddUsers -Users "first_user", "second_user" -Verbose
-    AddUsers -FromCSV "$env:USERPROFILE\Desktop\your_csv_with_users.csv" -GroupName 'your_AD_group' -Verbose
+    "your_AD_group" | AddUsersToGroup -Users "first_user", "second_user" -Verbose
+    AddUsersToGroup -FromCSV "$env:USERPROFILE\Desktop\your_csv_with_users.csv" -GroupName 'your_AD_group' -Verbose
     
     .NOTES
-    Features in new version: RSAT, exec. policy, all groups, etc.
-    v1
+    v1.0.1
     #>
     [CmdletBinding()]
     param(
-        [Parameter(ValueFromRemainingArguments = $true, Mandatory = $false, HelpMessage = "Identity or identites")]
-        $Users,
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromRemainingArguments = $true)]
+        [string[]]$Users,
  
-        [Parameter(Mandatory = $false, HelpMessage = "Identity or identites from file")]
+        [Parameter(Mandatory = $false)]
         $FromCSV,
 
-        [Parameter(ValueFromPipeline = $false, Mandatory = $false, HelpMessage = "UserPrincipalName, SamAccountName")]
+        [Parameter(ValueFromPipeline = $false, Mandatory = $false)]
         [ValidateSet("SamAccountName", "DisplayName", "Email", "UserPrincipalName")]
         [string]$Filter = "SamAccountName",
 
-        [Parameter(ValueFromPipeline = $true, Mandatory = $false, HelpMessage = "Choose your group")]
+        [Parameter(ValueFromPipeline = $true, Mandatory = $false)]
         [string]$GroupName,
 
-        [Parameter(Mandatory = $false, HelpMessage = "If your delimiter is not comma, choose the one used in csv file")]
+        [Parameter(Mandatory = $false)]
         [string]$Delimiter = ","
     )
     BEGIN {
@@ -49,15 +48,16 @@ Function AddUsers {
             Write-Output "Active Directory is missing, installing..."
             Import-Module -Name ActiveDirectory -Force -Verbose
         }
-        Write-Verbose "Selected group details: $(Get-ADGroup -Filter { Name -like $GroupName })"     
+        Write-Verbose -Message "Selected group details: $(Get-ADGroup -Filter { Name -like $GroupName })"     
     }
     PROCESS {
         if ($FromCSV) {
+            Write-Verbose -Message "Importing users from csv file..."
             $Users = (Import-Csv -Path $FromCSV -Delimiter $Delimiter -Header "Name").Name
         }
         $Users | ForEach-Object {
             try {
-                $User = Get-ADuser -Filter "$Filter -eq '$_'" | Select-Object ObjectGUID
+                $User = (Get-ADuser -Filter "$Filter -eq '$_'").ObjectGUID
                 if ($User) {
                     Write-Host "AD user: $_ found in the Active Directory, continuing..." -ForegroundColor Green
                 }
@@ -76,8 +76,9 @@ Function AddUsers {
         }
     }
     END {
-        Write-Host "Finished, cleaning up and exiting..." -ForegroundColor Cyan
+        Write-Verbose -Message "Finished, cleaning up and exiting. Time: $(Get-Date -Format "dddd | MM/dd/yyyy | HH:mm")"
         Clear-Variable -Name Users, GroupName -Force -Verbose -ErrorAction SilentlyContinue
         Clear-History -Verbose
+        exit
     }
 }
