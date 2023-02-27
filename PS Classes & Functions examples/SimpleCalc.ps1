c034892e37e463a240a765940ad1ae5144fde0dc
[S.B.]::FW <stjepanb83@gmail.com>
2023-02-28 00:54:50 +0100

    Update SimpleCalc
    Operation for Average added, description and help messages

--- PS Classes & Functions examples/SimpleCalc.ps1
+++ PS Classes & Functions examples/SimpleCalc.ps1
@@ -1,85 +1,108 @@
 Class Calculator {
     [int]$LeftOperand
     [int]$RightOperand
+    [string]$Operation
+
+    ## Constructor to initialize object properties
     Calculator(
         [int]$LeftOperand,
-        [int]$RightOperand) {
+        [int]$RightOperand,
+        [string]$Operation
+    ) {
         $this.LeftOperand = $LeftOperand
         $this.RightOperand = $RightOperand
+        $this.Operation = $Operation
     }
+    ## Method to perform addition
     [int] Add() {
         return $this.LeftOperand + $this.RightOperand
     }
+    ## Method to perform subtraction
     [int] Subtract() {
         return $this.LeftOperand - $this.RightOperand
     }
+    ## Method to perform multiplication
     [int] Multiply() {
         return $this.LeftOperand * $this.RightOperand
     }
+    ## Method to perform division
     [int] Divide() {
         if ($this.RightOperand -eq 0) {
             throw "You can't divide by zero!"
         }
         return $this.LeftOperand / $this.RightOperand
     }
+    ## Method to calculate average
+    [int] Average() {
+        return ($this.LeftOperand + $this.RightOperand) / 2
+    }
 }
-  
+
 Function SimpleCalc {
     <#
     .SYNOPSIS
-    Simple calculator.
-    
+    Simple calculator that shows how to use class.
+
     .DESCRIPTION
-    This basic function that uses class.
+    This script block takes in numerical input via the LeftOperand and RightOperand parameters, as well as the Numbers parameter for average calculations.
+    The Operation parameter determines which mathematical operation to perform on the LeftOperand and RightOperand parameters.
+    If the Average switch is specified, the script block will calculate the average of the numbers in the Numbers parameter.
+    If the Percentage switch is specified, the script block will calculate the percentage of the LeftOperand out of the RightOperand.
     
     .PARAMETER Operation
-    Mandatory - description
+    Mandatory - specifies the mathematical operation to perform.
     .PARAMETER LeftOperand
-    Mandatory - description
+    Mandatory - value on the left side of the mathematical operation.
     .PARAMETER RightOperand
-    Mandatory - description
+    Mandatory - value on the right side of the mathematical operation.
     
     .EXAMPLE
     SimpleCalc -Operation Add -LeftOperand 2 -RightOperand 2
     SimpleCalc -Operation Divide -LeftOperand 10 -RightOperand 1
     SimpleCalc -Operation Multiply -LeftOperand 5 -RightOperand 3 -Verbose
     SimpleCalc -Operation Subtract -LeftOperand 5 -RightOperand 3 -Verbose
+    SimpleCalc -Operation Average -LeftOperand 3 -RightOperand 111 -Verbose
     
     .NOTES
-    v0.5
+    v0.6.1
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
-        [ValidateSet("Add", "Subtract", "Multiply", "Divide")]
+        [Parameter(Mandatory = $true, Position = 0, HelpMessage = "Pick the math operation")]
+        [ValidateSet("Add", "Subtract", "Multiply", "Divide", "Average")]
         [string]$Operation,
 
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, Position = 1, HelpMessage = "Enter the left operand")]
         [int]$LeftOperand,
 
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, Position = 2, HelpMessage = "Enter the right operand")]
         [int]$RightOperand
     )
     BEGIN {
-        $SimpleCalc = [Calculator]::new($LeftOperand, $RightOperand)
+        Write-Verbose -Message $Operation
+        ## Here we can now initialize a new calculator object based on our class
+        $SimpleCalc = [Calculator]::new($LeftOperand, $RightOperand, $Operation)
     }
     PROCESS {
         switch ($Operation) {
             "Add" {
                 $Res = $SimpleCalc.Add()
             }
-            "Divide" {
-                $Res = $SimpleCalc.Divide()
+            "Subtract" {
+                $Res = $SimpleCalc.Subtract()
             }
             "Multiply" {
                 $Res = $SimpleCalc.Multiply()
             }
-            "Subtract" {
-                $Res = $SimpleCalc.Subtract()
+            "Divide" {
+                $Res = $SimpleCalc.Divide()
+            }
+            "Average" {
+                $Res = $SimpleCalc.Average()
             }
         }
     }
     END {
-        Write-Verbose -Message "Result: $Res"
+        Write-Output -InputObject "Result: $Res" 
     }
 }
