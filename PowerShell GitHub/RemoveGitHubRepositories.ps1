1a18184ddd839ed22e4ef60bb9ce24a8a896993e
[S.B.]::FW <stjepanb83@gmail.com>
2023-04-03 23:25:51 +0200

    Update RemoveGitHubRepositories
    refactor and improvements

--- PowerShell GitHub/RemoveGitHubRepositories.ps1
+++ PowerShell GitHub/RemoveGitHubRepositories.ps1
@@ -1,31 +1,36 @@
 Function RemoveGitHubRepositories {
     <#
     .SYNOPSIS
-    Deletes one or more repositories on GitHub.
+    Deletes one or more GitHub repositories using the GitHub CLI.
 
     .DESCRIPTION
-    This function can be used to delete one or more repositories on GitHub. It uses the GitHub CLI to authenticate and interact with the GitHub API.
+    This function uses the GitHub CLI to delete one or more GitHub repositories, with ability to use CSV file containing a list of repository names.
 
     .PARAMETER Repositories
-    Mandatory - array of repositories to be deleted. Each repository should be in the format "username/reponame".
+    NotMandatory - array of repositories to be deleted. Each repository should be in the format "username/reponame".
     .PARAMETER Token
-    Mandatory - the authentication token to use for interacting with the GitHub API.
+    Mandatory - the authentication token to use for interacting with the GitHub API, classic is recommended.
     .PARAMETER InputFile
     NotMandatory - string that specifies the path of a file containing a list of repositories to delete, one per line.
-    .PARAMETER Confirm
-    NotMandatory - switch parameter that prompts the user to confirm each repository deletion.
     .PARAMETER InputDelimiter
     NotMandatory - string that specifies the delimiter character used in the input file. The default value is a comma.
+    .PARAMETER IncludeForks
+    NotMandatory - if forked repositories should also be included for deletion, by default, this parameter is not enabled and only the repositories owned by the authenticated user are deleted.
+    .PARAMETER IncludeArchived
+    NotMandatory - if archived repositories should also be included for deletion, by default, this parameter is not enabled and only the active repositories are deleted, if this parameter is used, archived repositories will also be deleted.
+    .PARAMETER Confirm
+    NotMandatory - switch parameter that prompts the user to confirm each repository deletion.
 
     .EXAMPLE
-    RemoveGitHubRepositories -Repositories "username/repo1","username/repo2" -Token "ghp_xyz123"
+    "https://github.com/your_user/repo_1","https://github.com/your_user/repo_1" | RemoveGitHubRepositories -Token "ghp_xyz" -Verbose
+    RemoveGitHubRepositories -InputFile "$env:USERPROFILE\Desktop\ght.txt" -Token -Verbose
 
     .NOTES
-    v0.0.1
+    v0.0.3
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
         [string[]]$Repositories,
 
         [Parameter(Mandatory = $true)]
@@ -35,55 +40,83 @@ Function RemoveGitHubRepositories {
         [string]$InputFile,
 
         [Parameter(Mandatory = $false)]
-        [switch]$Confirm,
+        [string]$InputDelimiter = ",",
 
         [Parameter(Mandatory = $false)]
-        [string]$InputDelimiter = ","
+        [switch]$IncludeForks,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$IncludeArchived,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$Confirm
     )
     BEGIN {
+        Write-Verbose -Message "Checking for Chocolatey..."
+        if (-not(Get-Command choco.exe -ErrorAction SilentlyContinue)) {
+            try {
+                Write-Verbose -Message "Install Chocolatey..."
+                Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+            }
+            catch {
+                Write-Error "Failed to install Chocolatey: $($_.Exception.Message)"
+                return
+            }
+        }
+        Write-Verbose -Message "Installing Git and GitHub CLI using Chocolatey..."
+        if (-not(Get-Command git -ErrorAction SilentlyContinue)) {
+            try {
+                choco install git -y
+                Write-Output "Git CLI installation successful"
+            }
+            catch {
+                Write-Error "Failed to install Git CLI: $($_.Exception.Message)"
+            }
+        }
         if (-not(Get-Command gh -ErrorAction SilentlyContinue)) {
-            Write-Verbose "Installing GitHub CLI..."
-            $Ps = [System.Management.Automation.PowerShell]::Create()
-            $null = $Ps.AddScript('Invoke-WebRequest https://cli.github.com/packages/gh-stable-windows-amd64.zip -OutFile gh.zip')
-            $null = $Ps.AddScript('Expand-Archive gh.zip -DestinationPath $env:LOCALAPPDATA\gh')
-            $null = $Ps.AddScript('$env:Path = "$env:LOCALAPPDATA\gh\bin;$env:Path"')
-            $null = $Ps.AddScript('gh --version')
-            $null = $Ps.Invoke()
-            $ps.Dispose()
+            try {
+                choco install gh -y
+                Write-Output "GitHub CLI installation successful"
+            }
+            catch {
+                Write-Error "Failed to install GitHub CLI: $($_.Exception.Message)"
+            }
         }
-        Write-Verbose -Message "Checking if token is valid..."
-        $CheckToken = gh auth status --token=$Token
-        if ($CheckToken.Status -ne "Logged in to github.com") {
-            Write-Verbose -Message "Not logged in or invalid token."
+        Set-Content -Value $Token -Path "$env:USERPROFILE\Desktop\ght.txt" -Force -Verbose
+        Start-Process -FilePath "cmd" -ArgumentList "/c gh auth login --with-token < $env:USERPROFILE\Desktop\ght.txt" -WindowStyle Hidden -Wait
+        Start-Sleep -Seconds 3
+        if ($InputFile) {
+            $RepositoriesFromFile = Get-Content $InputFile | ForEach-Object { $_ -split $InputDelimiter } | Where-Object { $_ }
+            $Repositories += $RepositoriesFromFile
         }
-        gh auth login -t $Token
     }
     PROCESS {
-        if ($InputFile) {
-            if (!(Test-Path $InputFile)) {
-                throw "Input file '$InputFile' not found."
+        foreach ($Repo in $Repositories) {
+            $Owner, $Name = $Repo.split("/")[-2], $Repo.split("/")[-1]
+            $RepoInfo = gh api "/repos/$Owner/$Name"
+            if (!$RepoInfo) {
+                Write-Warning -Message "Repository $Repo not found"
+                continue
             }
-            $Repositories = Get-Content $InputFile `
-            | ConvertFrom-Csv -Delimiter $InputDelimiter `
-            | Select-Object -ExpandProperty Repositories
-        }
-        foreach ($repo in $Repositories) {
-            Write-Verbose -Message "Deleting repository $Repo..."
-            if ($Confirm) {
-                $ConfirmMessage = "Are you sure you want to delete repository $Repo?"
-                $Confirmed = $false
-                $Confirmed = $Host.UI.PromptForChoice('Confirm', $ConfirmMessage, @( 'Yes', 'No' ), 1)
-                if ($Confirmed -eq 1) {
-                    continue
-                }
+            if (!$IncludeForks -and $RepoInfo.fork) {
+                Write-Warning -Message "Repository $Repo is a fork and will not be deleted, use '-IncludeForks' to delete forks"
+                continue
             }
-            $Url = "repos/$Repo"
-            gh api $Url -X DELETE --token=$Token > $null
+            if (!$IncludeArchived -and $RepoInfo.archived) {
+                Write-Warning -Message "Repository $Repo is archived and will not be deleted, use '-IncludeArchived' to delete archived repositories"
+                continue
+            }
+            $ConfirmationMessage = "Are you sure you want to delete repository '$Owner/$Name'?"
+            if (!$Confirm -or (Read-Host -Prompt $ConfirmationMessage -AsSecureString) -as [string] -eq "yes") {
+                gh api "/repos/$Owner/$Name" -X DELETE > $null
+                Write-Host "Repository $Owner/$Name deleted successfully" -ForegroundColor Green
+            }
         }
     }
     END {
-        gh auth logout
-        Write-Host "All repositories deleted." -ForegroundColor Green
+        Write-Verbose -Message "Cleaning up, closing connection and exiting!"
+        gh auth logout --hostname "github.com"
+        Remove-Item -Path "$env:USERPROFILE\Desktop\ght.txt" -Force -Verbose
         Clear-Variable -Name Repositories, Token -Force -ErrorAction SilentlyContinue
     }
 }
