20603c9c12aae63be066ee71393713c275bfd511
theFest <stjepanb83@gmail.com>
2023-04-14 00:06:00 +0200

    Update ManageRepositoryBranches
    added functionality to either create or delete branch or multiple branches.

--- ps-github/ManageRepositoryBranches.ps1
+++ ps-github/ManageRepositoryBranches.ps1
@@ -1,44 +1,51 @@
 Function ManageRepositoryBranches {
     <#
     .SYNOPSIS
-    Manage GitHub repository branches.
+    GitHub repository branches manager.
 
     .DESCRIPTION
     With this function you can manage branches of GitHub repository's.
 
     .PARAMETER Action
-    Mandatory - description
+    Mandatory - action to be performed on the branches, accepted values are "View", "Create", or "Delete".
     .PARAMETER Repository
-    Mandatory - description
-    .PARAMETER NewBranchName
-    Mandatory - description
+    Mandatory - repository URL, parameter is validated to accept only URLs starting with "https://github.com/".
     .PARAMETER Token
-    Mandatory - description
+    NotMandatory - specifies the personal access token for authentication with GitHub.
+    .PARAMETER NewBranchName
+    NotMandatory - name of the new branch or branches to be created.
+    .PARAMETER DeleteBranchName
+    NotMandatory - names of the branche or branches to be deleted.
 
     .EXAMPLE
-    ManageRepositoryBranches -Action View "https://github.com/your_user/your_repo" -Token "ghp_xyz"
-    ManageRepositoryBranches -Action Create "https://github.com/your_user/your_repo" -Token "ghp_xyz" -NewBranchName "your_new_branch"
+    "https://github.com/your_user/your_repo" | ManageRepositoryBranches -Action View -Token "ghp_xyz"
+    ManageRepositoryBranches -Action Create -Repository "https://github.com/your_user/your_repo" -Token "ghp_xyz" -NewBranchName "your_new_branch_1", "your_new_branch_2"
+    ManageRepositoryBranches -Action Delete -Repository "https://github.com/your_user/your_repo" -Token "ghp_xyz" -DeleteBranchName "your_new_branch_1", "your_new_branch_2"
 
     .NOTES
-    v0.0.1
+    v0.0.3
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory, Position = 0, HelpMessage = "Enter 'View' or 'Create'")]
-        [ValidateSet("View", "Create")]
+        [Parameter(Mandatory = $true, Position = 0, HelpMessage = "Enter 'View', 'Create' or 'Delete'")]
+        [ValidateSet("View", "Create", "Delete")]
         [string]$Action,
 
-        [Parameter(Mandatory, Position = 1, HelpMessage = "Enter the GitHub repository URL")]
+        [Parameter(Mandatory = $true, Position = 1, ValueFromPipeline = $true, HelpMessage = "Enter the GitHub repository URL")]
         [ValidatePattern('https://github.com/.+/.+')]
         [string]$Repository,
 
+        [Parameter(Mandatory = $false, HelpMessage = "Token for auth with GitHub to be able to do actions/operation")]
+        [ValidateNotNullOrEmpty()]
+        [string]$Token,
+
         [Parameter(Mandatory = $false, HelpMessage = "Enter the name of the branch to create (only for 'Create' action)")]
         [ValidateNotNullOrEmpty()]
-        [string]$NewBranchName,
+        [string[]]$NewBranchName,
 
-        [Parameter(Mandatory = $false, HelpMessage = "Token for auth with GitHub")]
+        [Parameter(Mandatory = $false, HelpMessage = "Array of branch names to delete (only for 'Delete' action)")]
         [ValidateNotNullOrEmpty()]
-        [string]$Token
+        [string[]]$DeleteBranchName
     )
     BEGIN {
         Write-Verbose -Message "Checking for Chocolatey..."
@@ -71,15 +78,15 @@ Function ManageRepositoryBranches {
                 Write-Error "Failed to install GitHub CLI: $($_.Exception.Message)"
             }
         }
-        Write-Verbose -Message "Checking if token is valid..."
+        Write-Verbose -Message "checking if token is valid..."
         Set-Content -Value $Token -Path "$env:USERPROFILE\Desktop\ght.txt" -Force -Verbose
         Start-Process -FilePath "cmd" -ArgumentList "/c gh auth login --with-token < $env:USERPROFILE\Desktop\ght.txt" -WindowStyle Hidden -Wait
-        Write-Verbose -Message "Logged in to GitHub, continuing with selected operation $($Operation)..."
+        Write-Verbose -Message "logged in to GitHub, continuing with selected operation $($Operation)..."
     }
     PROCESS {
-        Write-Verbose -Message "split the repository URL to get owner and repository name"
+        Write-Verbose -Message "splitting the repository URL to get owner and repository name"
         $Owner, $RepoName = ($Repository -split '/')[-2, -1]
-        Write-Verbose -Message "set up API endpoints..."
+        Write-Verbose -Message "setting up API endpoints..."
         $BaseApiEndpoint = "https://api.github.com"
         $BranchesEndpoint = "$BaseApiEndpoint/repos/$Owner/$RepoName/branches"
         $RefsEndpoint = "$BaseApiEndpoint/repos/$Owner/$RepoName/git/refs"
@@ -93,7 +100,7 @@ Function ManageRepositoryBranches {
             "View" {
                 Write-Verbose -Message "get list of branches"
                 $Response = Invoke-RestMethod -Uri $BranchesEndpoint -Method Get -Headers $Headers
-                Write-Host "Branches in the '$RepoName' repository:"
+                Write-Host "Branches in the '$RepoName' repository:" -ForegroundColor Yellow
                 foreach ($Branch in $Response) {
                     Write-Host "- $($Branch.name)" -ForegroundColor Green
                 }
@@ -110,17 +117,44 @@ Function ManageRepositoryBranches {
                 $ResponseUri = "https://api.github.com/repos/$Owner/$RepoName/git/ref/$DefaultBranchRef"
                 $Response = Invoke-RestMethod -Uri $ResponseUri -Method Get -Headers $Headers
                 $DefaultBranchSha = $Response.object.sha
-                Write-Verbose -Message "create new branch using GitHub API..."
-                $HeadRef = "refs/heads/$NewBranchName"
-                $Payload = @{
-                    ref = $HeadRef
-                    sha = $DefaultBranchSha
-                } | ConvertTo-Json
-                Invoke-RestMethod -Uri $RefsEndpoint -Method Post -Headers $Headers -Body $Payload | Out-Null
-                Write-Host "New branch '$NewBranchName' created successfully" -ForegroundColor Green
+                $NewBranchName | ForEach-Object {
+                    $BranchName = $_
+                    Write-Verbose -Message "create new branch '$BranchName' using GitHub API..."
+                    $HeadRef = "refs/heads/$BranchName"
+                    $Payload = @{
+                        ref = $HeadRef
+                        sha = $DefaultBranchSha
+                    } | ConvertTo-Json
+                    Invoke-RestMethod -Uri $RefsEndpoint -Method Post -Headers $Headers -Body $Payload | Out-Null
+                    Write-Host "New branch '$BranchName' created successfully" -ForegroundColor Green
+                }
+            }
+            "Delete" {
+                if (-not $DeleteBranchName) {
+                    $DeleteBranchName = @((Read-Host "Enter the name of the branch to delete").Trim())
+                }
+                $BranchesEndpoint = "$BaseApiEndpoint/repos/$Owner/$RepoName/branches"
+                $Response = Invoke-RestMethod -Uri $BranchesEndpoint -Method Get -Headers $Headers
+                $BranchesToDelete = @()
+                foreach ($BranchName in $DeleteBranchName) {
+                    if ($BranchName -notin $Response.name) {
+                        Write-Error "Branch '$BranchName' not found in '$RepoName' repository"
+                    }
+                    else {
+                        $BranchesToDelete += $BranchName
+                    }
+                }
+                if ($BranchesToDelete.Count -eq 0) {
+                    return
+                }
+                foreach ($BranchName in $BranchesToDelete) {
+                    $ApiEndpoint = "$BaseApiEndpoint/repos/$Owner/$RepoName/git/refs/heads/$BranchName"
+                    $Response = Invoke-RestMethod -Uri $ApiEndpoint -Method Delete -Headers $Headers
+                    Write-Host "Branch '$BranchName' deleted successfully" -ForegroundColor Green
+                }
             }
             default {
-                Write-Error "Invalid action: '$Action'. Supported actions are: View or Create."
+                Write-Error "Invalid action: '$Action'. Supported actions are: 'View', 'Create' or 'Delete'."
             }
         }
     }
@@ -129,7 +163,7 @@ Function ManageRepositoryBranches {
         gh auth logout --hostname "github.com"
         Clear-History -Verbose -ErrorAction SilentlyContinue
         Remove-Item -Path "$env:USERPROFILE\Desktop\ght.txt" -Force -Verbose -ErrorAction SilentlyContinue
-        Clear-Variable -Name Repository, Token, NewBranchName -Scope Global -Force -Verbose -ErrorAction SilentlyContinue
-        Remove-Variable -Name Repository, Token, NewBranchName -Scope Global -Force -Verbose -ErrorAction SilentlyContinue
+        Clear-Variable -Name Repository, Token, NewBranchName, DeleteBranchName -Scope Global -Force -Verbose -ErrorAction SilentlyContinue
+        Remove-Variable -Name Repository, Token, NewBranchName, DeleteBranchName -Scope Global -Force -Verbose -ErrorAction SilentlyContinue
     }
 }
