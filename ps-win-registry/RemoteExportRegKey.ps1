7f48163117df89f387a420325f8f6b9181e6f087
theFest <thefestwhite@gmail.com>
2023-06-25 05:59:26 +0200

    Update RemoteExportRegKey
    added back. jobs, pipeline input and little enhancements

--- ps-win-registry/RemoteExportRegKey.ps1
+++ ps-win-registry/RemoteExportRegKey.ps1
@@ -1,34 +1,34 @@
-Function RemoteExportRegKey {
+function RemoteExportRegKey {
     <#
     .SYNOPSIS
     Exports a registry key from a remote or local computer.
-    
+
     .DESCRIPTION
     This function exports a specified registry key from a remote or local computer and saves it to a specified local path.
-    
+
     .PARAMETER RegPath
-    Mandatory - registry path to export. This parameter is mandatory.
+    Specifies the registry path to export, either local or remote.
     .PARAMETER LocalExportPath
-    Mandatory - local path where the exported registry file will be saved. This parameter is mandatory.
+    Specifies the local path where the exported registry file will be saved.
     .PARAMETER RemoteExportPath
-    NotMandatory - remote path where the exported registry file will be temporarily saved on the remote computer, if not provided, a default path will be used.
+    Specifies the remote path where the exported registry file will be temporarily saved on the remote computer, if not provided, a default path will be used.
     .PARAMETER ComputerName
-    NotMandatory - name of the remote computer from which to export the registry key, if not provided, the export will be performed on the local computer.
+    Specifies the name of the remote computer from which to export the registry key, if not provided, the export will be performed on the local computer.
     .PARAMETER Username
-    NotMandatory - username to use for the remote connection, required when exporting from a remote computer.
+    Specifies the username to use for the remote connection, required when exporting from a remote computer.
     .PARAMETER Pass
-    NotMandatory - password to use for the remote connection, required when exporting from a remote computer.
-    
+    Specifies the password to use for the remote connection, required when exporting from a remote computer.
+
     .EXAMPLE
     RemoteExportRegKey -RegPath "HKCU\AppEvents" -LocalExportPath "$env:USERPROFILE\Desktop\AppEvents.reg" -Verbose
-    RemoteExportRegKey -RegPath "HKCU\your_key\you_subkey" -LocalExportPath "$env:USERPROFILE\Desktop\you_subkey.reg" -ComputerName "remote_hostname" -Username "remote_user" -Pass "remote_pass"
-    
+    "HKCU\you_key\you_subkey" | RemoteExportRegKey -LocalExportPath "$env:USERPROFILE\Desktop\you_subkey.reg" -ComputerName "remote_hostname" -Username "remote_user" -Pass "remote_pass"
+
     .NOTES
-    v0.0.1
+    Version: 0.0.2
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [ValidateNotNullOrEmpty()]
         [string]$RegPath,
 
@@ -55,10 +55,10 @@ Function RemoteExportRegKey {
                 param($Using:ComputerName)
                 $isReachable = (Test-Connection -ComputerName $Using:ComputerName -Count 1 -Quiet) -and (Test-WSMan -ComputerName $Using:ComputerName -Authentication Default)
                 if ($isReachable) {
-                    Write-Host "Connection to the remote computer '$Using:ComputerName' established" -ForegroundColor Green
+                    Write-Host "Connection to the remote computer '$Using:ComputerName' established..." -ForegroundColor Green
                 }
                 else {
-                    Write-Warning -Message "Failed to establish a connection to the remote computer '$Using:ComputerName'"
+                    Write-Host "Failed to establish a connection to the remote computer '$Using:ComputerName'" -ForegroundColor Red 
                 }
                 $isReachable
             }
@@ -71,12 +71,11 @@ Function RemoteExportRegKey {
                     Start-Process -FilePath 'reg.exe' -ArgumentList 'export', $RegPath, $RemoteExportPath -Wait -NoNewWindow
                 }
                 $ExportJob = Invoke-Command -Session $Session -AsJob -ScriptBlock $ExportScriptBlock -ArgumentList $RegPath, $RemoteExportPath
-                Wait-Job $ExportJob | Out-Null
-                Receive-Job $ExportJob | Out-Null
+                Wait-Job $ExportJob | Out-Null ; Receive-Job $ExportJob | Out-Null
                 Copy-Item -FromSession $Session -Path $RemoteExportPath -Destination $LocalExportPath -Force -Verbose
                 Invoke-Command -Session $Session -ScriptBlock {
                     param($RemoteExportPath)
-                    Remove-Item -Path $RemoteExportPath -Force -Verbose
+                    Remove-Item -Path $RemoteExportPath -Force
                 } -ArgumentList $RemoteExportPath
             }
             else {
@@ -87,15 +86,20 @@ Function RemoteExportRegKey {
             Write-Verbose -Message "Export on the local computer has started..."
             Start-Process -FilePath 'reg.exe' -ArgumentList 'export', $RegPath, $LocalExportPath -Wait -NoNewWindow
         }
-        Write-Host "Registry key exported to '$LocalExportPath'." -ForegroundColor Green
+        if (Test-Path $LocalExportPath) {
+            Write-Host "Registry key exported to '$LocalExportPath'" -ForegroundColor Green
+        }
+        else {
+            Write-Error -Message "Failed to export the registry key"
+        }
     }
     catch {
         Write-Error -Message $_.Exception.Message
     }
     finally {
         if ($Session) {
-            Write-Verbose -Message "Finished, closing PS Session and exiting"
-            Remove-PSSession -Session $Session -Verbose
+            Write-Verbose -Message "Finished, closing PS Session and exiting..."
+            Remove-PSSession -Session $Session -Verbose -ErrorAction SilentlyContinue
         }
     }
 }
