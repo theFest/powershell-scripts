13763a379f016184e05424392a953fae94bb8f8f
theFest <thefestwhite@gmail.com>
2023-06-25 07:01:10 +0200

    folder change
    n/a

--- ps-reusable-templates/InvokeFunction.ps1
+++ ps-reusable-templates/InvokeFunction.ps1
@@ -24,17 +24,24 @@ Function InvokeFunction {
     Parameter description
     .PARAMETER AdvancedParam3
     Parameter description
-
     .PARAMETER AdvancedParam4
     Parameter description
 
     .EXAMPLE
-    An example
+    This is an example of a PowerShell function that uses a background job to execute a command called "DoSomething" for each item in the "InputObject" array.
+    The function accepts several parameters that allow the user to customize the behavior of the command, such as specifying parameters and switches.
+    The function uses the PowerShell CmdletBinding attribute to provide a number of features, including support for ShouldProcess and ConfirmImpact.
+    The ShouldProcess feature allows the function to ask the user for confirmation before performing any actions that could have an impact, while the ConfirmImpact feature allows the function to specify the level of impact for which confirmation is required.
+    The function uses the Begin, Process, and End blocks to perform different tasks. In the Begin block, the function initializes variables and sets up the parameters that will be passed to the DoSomething command.
+    In the Process block, the function loops through each item in the InputObject array, calls the DoSomething command with the appropriate parameters, and adds the results to an array.
+    In the End block, the function either returns the results or starts a background job to run the function with the AsJob parameter set to true.
+    This function provides an example of how to use PowerShell's background job feature to perform tasks asynchronously, which can be useful for long-running tasks or tasks that need to be performed in the background while the user continues to work on other tasks.
+    The function also demonstrates how to use various PowerShell features, such as parameter validation and support for ShouldProcess and ConfirmImpact.
 
     .NOTES
     v0.0.1
     #>
-    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")]
     param(
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
         [string[]]$InputObject,
@@ -71,37 +78,47 @@ Function InvokeFunction {
         [string]$AdvancedParam4
     )
     BEGIN {
-        $results = @()
-        $params = @{
-            'Param1' = $Param1
-            'Param2' = $Param2
+        $Results = @()
+        $Params = @{
+            "Param1" = $Param1
+            "Param2" = $Param2
         }
         if ($Switch1) {
-            $params['Switch1'] = $true
+            $Params["Switch1"] = $true
         }
         if ($Switch2) {
-            $params['Switch2'] = $true
+            $Params["Switch2"] = $true
         }
     }
     PROCESS {
-        foreach ($item in $InputObject) {
-            if ($PSCmdlet.ShouldProcess($item)) {
-                $result = ## DoSomething -Input $item @params
-                $results += $result
+        foreach ($Item in $InputObject) {
+            if ($PSCmdlet.ShouldProcess($Item)) {
+                $Result = ## DoSomething -Input $item @params
+                $Results += $Result
             }
         }
     }
     END {
         if ($AsJob) {
-            $job = Start-Job -ScriptBlock {
+            $Job = Start-Job -ScriptBlock {
                 param($InputObject, $Params, $Switch1, $Switch2, $AdvancedParam1, $AdvancedParam2, $AdvancedParam3, $AdvancedParam4)
-                InvokeFunction -InputObject $InputObject -AsJob $false @Params -Switch1:$Switch1 -Switch2:$Switch2 -AdvancedParam1:$AdvancedParam1 -AdvancedParam2:$AdvancedParam2 -AdvancedParam3:$AdvancedParam3 -AdvancedParam4:$AdvancedParam4
+                InvokeFunction -InputObject $InputObject -AsJob $false @Params -Switch1:$Switch1 -Switch2:$Switch2 `
+                    -AdvancedParam1:$AdvancedParam1 `
+                    -AdvancedParam2:$AdvancedParam2 `
+                    -AdvancedParam3:$AdvancedParam3 `
+                    -AdvancedParam4:$AdvancedParam4
             } -ArgumentList $InputObject, $params, $Switch1, $Switch2, $AdvancedParam1, $AdvancedParam2, $AdvancedParam3, $AdvancedParam4
-            Write-Output "Job $($job.Id) started."
-            Write-Output $job
+            Write-Verbose -Message "Job $($Job.Id) started."
+            Write-Output -InputObject $Job
         }
         else {
-            Write-Output $results
+            Write-Output $Results
         }
     }
+}
+
+function DoSomething {
+    param($In, $Param1, $Param2, $Switch1, $Switch2, $AdvancedParam1, $AdvancedParam2, $AdvancedParam3, $AdvancedParam4)
+    $Output = "Input: $In, Param1: $Param1, Param2: $Param2, Switch1: $Switch1, Switch2: $Switch2, AdvancedParam1: $AdvancedParam1, AdvancedParam2: $AdvancedParam2, AdvancedParam3: $AdvancedParam3, AdvancedParam4: $AdvancedParam4"
+    Write-Output $Output
 }
