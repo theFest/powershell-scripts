1ce223de3289e3e49fcf7eed7254837a1acd5a84
theFest <thefestwhite@gmail.com>
2023-08-03 23:26:03 +0200

    Update GetDriversPnpUtil
    small fixes

--- ps-drivers/GetDriversPnpUtil.ps1
+++ ps-drivers/GetDriversPnpUtil.ps1
@@ -11,7 +11,7 @@ Function GetDriversPnpUtil {
     NotMandatory - name of the remote computer from which to retrieve the driver information. Default is the local computer.
     .PARAMETER Username
     NotMandatory - username to be used for connecting to the remote computer (if applicable).
-    .PARAMETER Password
+    .PARAMETER Pass
     NotMandatory - password for the provided username (if applicable).
     .PARAMETER IncludeMicrosoftDrivers
     NotMandatory - switch parameter to include Microsoft drivers in the output.
@@ -25,76 +25,61 @@ Function GetDriversPnpUtil {
     "remote_computer" | GetDriversPnpUtil -Username "remote_user" -Pass "remote_pass" -CsvPath "$env:USERPROFILE\Desktop\DriverList.csv"
 
     .NOTES
-    v0.0.1
+    v0.0.2
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+        [Parameter(Position = 0, ValueFromPipeline = $true, HelpMessage = "The name of the remote computer to retrieve drivers from")]
         [string]$ComputerName = $env:COMPUTERNAME,
-        
-        [Parameter(Mandatory = $false)]
+
+        [Parameter(Position = 1, HelpMessage = "Username for authentication (if required)")]
         [string]$Username,
-        
-        [Parameter(Mandatory = $false)]
+
+        [Parameter(Position = 2, HelpMessage = "Password for authentication (if required)")]
         [string]$Pass,
-        
-        [Parameter(Mandatory = $false)]
+
+        [Parameter(HelpMessage = "Include Microsoft drivers in the result")]
         [switch]$IncludeMicrosoftDrivers,
-        
-        [Parameter(Mandatory = $false)]
+
+        [Parameter(HelpMessage = "The path to export the drivers list to a CSV file")]
         [string]$CsvPath
     )
     if ($ComputerName -ne $env:COMPUTERNAME) {
-        Write-Verbose -Message "Testing connection to remote computer $ComputerName..."
+        Write-Verbose "Testing connection to remote computer $ComputerName..."
         if (-not (Test-Connection -ComputerName $ComputerName -Count 1 -Quiet)) {
-            Write-Error -Message "Unable to connect to the remote computer $ComputerName. Please check the computer name or network connectivity." -ForegroundColor Red
+            Write-Error "Unable to connect to the remote computer $ComputerName. Please check the computer name or network connectivity." -ForegroundColor Red
             return
         }
-        Write-Verbose -Message "Testing WSMan connection to remote computer $ComputerName..."
+        Write-Verbose "Testing WSMan connection to remote computer $ComputerName..."
         if (-not (Test-WSMan -ComputerName $ComputerName -ErrorAction SilentlyContinue)) {
-            Write-Error -Message "Unable to establish a WSMan connection to the remote computer $ComputerName. Make sure WSMan is properly configured on the remote machine."
+            Write-Error "Unable to establish a WSMan connection to the remote computer $ComputerName. Make sure WSMan is properly configured on the remote machine."
             return
         }
-    }
-    Write-Verbose -Message "Checking if pnputil.exe is in the environment variables and add it if not found"
-    if (-not (Get-Command -Name "pnputil" -ErrorAction SilentlyContinue)) {
-        Write-Host "Adding pnputil.exe to the environment variables..." -ForegroundColor DarkGreen
-        $env:Path += ";$($env:SystemRoot)\System32"
     }
     if ($ComputerName -eq $env:COMPUTERNAME) {
-        Write-Host "Getting installed drivers on the local machine..." -ForegroundColor Cyan
-        $PnpUtilCommand = "pnputil.exe /enum-drivers"
-        $result = Invoke-Expression -Command $PnpUtilCommand
+        Write-Host "Getting installed drivers from the local machine..." -ForegroundColor Cyan
+        $Drivers = Get-CimInstance -Class Win32_PnPSignedDriver
     }
     else {
         Write-Host "Getting installed drivers on remote machine $ComputerName..." -ForegroundColor Cyan
         $SecurePassword = ConvertTo-SecureString $Pass -AsPlainText -Force
         $Credential = New-Object System.Management.Automation.PSCredential ($Username, $SecurePassword)
-        $PnpUtilCommand = "pnputil.exe /enum-drivers"
-        $Result = Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
-            param ($PnpUtilCommand)
-            Invoke-Expression -Command $PnpUtilCommand
-        } -ArgumentList $PnpUtilCommand
+        $Drivers = Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
+            Get-CimInstance -Class Win32_PnPSignedDriver
+        }
     }
     if ($IncludeMicrosoftDrivers) {
-        if ($ComputerName -eq $env:COMPUTERNAME) {
-            Write-Host "Including Microsoft drivers on the local machine..." -ForegroundColor Yellow
-            $MsDrivers = Get-WmiObject Win32_PnPSignedDriver | Where-Object { $_.Manufacturer -eq "Microsoft" } `
-            | Select-Object DeviceName, CompatID, Description, DeviceID, DriverProviderName, DriverVersion, HardWareID, InfName, IsSigned, Manufacturer, PDO, Signer 
-            $Result += $MsDrivers | Format-Table -AutoSize | Out-String
-        }
-        else {
-            Write-Host "Including Microsoft drivers on remote machine $ComputerName..." -ForegroundColor Yellow
-            $MsDrivers = Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
-                Get-WmiObject Win32_PnPSignedDriver | Where-Object { $_.Manufacturer -eq "Microsoft" }
-            }
-            $MsDrivers = $MsDrivers | Select-Object DeviceName, CompatID, Description, DeviceID, DriverProviderName, DriverVersion, HardWareID, InfName, IsSigned, Manufacturer, PDO, Signer
-            $Result += $MsDrivers | Format-Table -AutoSize | Out-String
-        }
+        $MsDrivers = $Drivers | Where-Object { $_.Manufacturer -eq "Microsoft" }
+        $Result = $MsDrivers | Select-Object `
+            DeviceName, CompatID, Description, DeviceID, DriverProviderName, DriverVersion, HardWareID, InfName, IsSigned, Manufacturer, PDO, Signer
+    }
+    else {
+        $Result = $Drivers | Select-Object `
+            DeviceName, CompatID, Description, DeviceID, DriverProviderName, DriverVersion, HardWareID, InfName, IsSigned, Manufacturer, PDO, Signer
     }
     if ($CsvPath) {
-        Write-Verbose -Message "Exporting installed drivers to CSV: $CsvPath"
-        $Result | Out-File -FilePath $CsvPath -Encoding UTF8
+        Write-Verbose "Exporting installed drivers to CSV: $CsvPath"
+        $Result | Export-Csv -Path $CsvPath -NoTypeInformation
     }
     return $Result
 }
