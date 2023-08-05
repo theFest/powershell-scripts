940da4c8951e15a6816d9629c199f2adcf114ce2
theFest <thefestwhite@gmail.com>
2023-08-05 02:45:23 +0200

    Update FileCreationForm
    styling, small fixes...

--- ps-forms/FileCreationForm.ps1
+++ ps-forms/FileCreationForm.ps1
@@ -2,40 +2,39 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 Function ShowAboutDialog {
-    [System.Windows.Forms.MessageBox]::Show("FW File Creation Form`nVersion 0.0.0.4", "About", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+    [System.Windows.Forms.MessageBox]::Show("FW File Creation Form`nVersion 0.0.0.5", "About", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 }
 
 Function ShowHelpDialog {
     [System.Windows.Forms.MessageBox]::Show(@"
 This application allows you to create multiple files with custom content.
 
-Enhancements in Version 0.0.0.4:
+Enhancements in Version 0.0.0.5:
 - Added support for setting custom Prefix, Suffix, and Extension.
 "@, "Help", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 }
 
 Function FileCreationForm {
-    [CmdletBinding()]
     param (
         [string]$DefaultPath = "$env:USERPROFILE\Desktop",
-        [string]$DefaultPrefix = "",
+        [string]$DefaultPrefix = "File",
         [string]$DefaultSuffix = "",
         [string]$DefaultExtension = "txt",
         [int]$DefaultNumberOfFiles = 5
     )
-
     $Form = New-Object Windows.Forms.Form
     $Form.Text = "FW File Creation Form"
-    $Form.Size = New-Object Drawing.Size(840, 580)
+    $Form.Size = New-Object Drawing.Size(800, 600)
     $Form.StartPosition = [Windows.Forms.FormStartPosition]::CenterScreen
-    $Form.BackColor = [System.Drawing.Color]::FromArgb(30, 30, 30)
+    $Form.BackColor = [System.Drawing.Color]::FromArgb(40, 40, 40)
     $Form.ForeColor = [System.Drawing.Color]::White
 
     $Style = [System.Windows.Forms.FlatStyle]::Flat
 
     $MenuBar = New-Object System.Windows.Forms.MenuStrip
     $MenuBar.ForeColor = [System.Drawing.Color]::White
-    $MenuBar.BackColor = [System.Drawing.Color]::FromArgb(50, 50, 50)
+    $MenuBar.BackColor = [System.Drawing.Color]::FromArgb(60, 60, 60)
+    $MenuBar.Font = New-Object System.Drawing.Font("Segoe UI", 11, [System.Drawing.FontStyle]::Regular)
 
     $FileMenu = New-Object System.Windows.Forms.ToolStripMenuItem
     $FileMenu.Text = "&File"
@@ -82,25 +81,27 @@ Function FileCreationForm {
 
     $LabelPath = New-Object Windows.Forms.Label
     $LabelPath.Text = "Path:"
-    $LabelPath.Location = New-Object Drawing.Point(20, 25)
+    $LabelPath.Location = New-Object Drawing.Point(20, 42)
     $LabelPath.AutoSize = $true
-    $LabelPath.Padding = New-Object System.Windows.Forms.Padding(10, 10, 10, 10)
     $LabelPath.ForeColor = [System.Drawing.Color]::White
+    $LabelPath.Font = New-Object System.Drawing.Font("Segoe UI", 12, [System.Drawing.FontStyle]::Bold)
     $Form.Controls.Add($LabelPath)
 
     $TextBoxPath = New-Object Windows.Forms.TextBox
     $TextBoxPath.Text = $DefaultPath
-    $TextBoxPath.Location = New-Object Drawing.Point(100, 30)
+    $TextBoxPath.Location = New-Object Drawing.Point(100, 40)
     $TextBoxPath.Size = New-Object Drawing.Size(550, 30)
+    $TextBoxPath.Font = New-Object System.Drawing.Font("Segoe UI", 11, [System.Drawing.FontStyle]::Regular)
     $Form.Controls.Add($TextBoxPath)
 
     $ButtonBrowse = New-Object Windows.Forms.Button
     $ButtonBrowse.Text = "Browse..."
-    $ButtonBrowse.Location = New-Object Drawing.Point(700, 25)
-    $ButtonBrowse.Size = New-Object Drawing.Size(100, 50)
+    $ButtonBrowse.Location = New-Object Drawing.Point(670, 40)
+    $ButtonBrowse.Size = New-Object Drawing.Size(100, 30)
     $ButtonBrowse.FlatStyle = $style
-    $ButtonBrowse.BackColor = [System.Drawing.Color]::FromArgb(60, 60, 60)
+    $ButtonBrowse.BackColor = [System.Drawing.Color]::FromArgb(80, 80, 80)
     $ButtonBrowse.ForeColor = [System.Drawing.Color]::White
+    $ButtonBrowse.Font = New-Object System.Drawing.Font("Segoe UI", 11, [System.Drawing.FontStyle]::Regular)
     $ButtonBrowse.Add_Click({
             $FolderBrowserDialog = New-Object Windows.Forms.FolderBrowserDialog
             $Result = $FolderBrowserDialog.ShowDialog()
@@ -117,6 +118,7 @@ Function FileCreationForm {
     $CheckBoxPrefix.AutoSize = $true
     $CheckBoxPrefix.Checked = $false
     $CheckBoxPrefix.ForeColor = [System.Drawing.Color]::White
+    $CheckBoxPrefix.Font = New-Object System.Drawing.Font("Segoe UI", 12, [System.Drawing.FontStyle]::Bold)
     $CheckBoxPrefix.Add_CheckStateChanged({
             $TextBoxPrefix.Enabled = $CheckBoxPrefix.Checked
         })
@@ -124,16 +126,18 @@ Function FileCreationForm {
 
     $TextBoxPrefix = New-Object Windows.Forms.TextBox
     $TextBoxPrefix.Text = $DefaultPrefix
-    $TextBoxPrefix.Location = New-Object Drawing.Point(100, 80)
-    $TextBoxPrefix.Size = New-Object Drawing.Size(300, 30)
+    $TextBoxPrefix.Location = New-Object Drawing.Point(100, 75)
+    $TextBoxPrefix.Size = New-Object Drawing.Size(200, 30)
+    $TextBoxPrefix.Font = New-Object System.Drawing.Font("Segoe UI", 11, [System.Drawing.FontStyle]::Regular)
     $Form.Controls.Add($TextBoxPrefix)
 
     $CheckBoxSuffix = New-Object Windows.Forms.CheckBox
     $CheckBoxSuffix.Text = "Suffix:"
-    $CheckBoxSuffix.Location = New-Object Drawing.Point(420, 80)
+    $CheckBoxSuffix.Location = New-Object Drawing.Point(320, 80)
     $CheckBoxSuffix.AutoSize = $true
     $CheckBoxSuffix.Checked = $false
     $CheckBoxSuffix.ForeColor = [System.Drawing.Color]::White
+    $CheckBoxSuffix.Font = New-Object System.Drawing.Font("Segoe UI", 12, [System.Drawing.FontStyle]::Bold)
     $CheckBoxSuffix.Add_CheckStateChanged({
             $TextBoxSuffix.Enabled = $CheckBoxSuffix.Checked
         })
@@ -141,8 +145,9 @@ Function FileCreationForm {
 
     $TextBoxSuffix = New-Object Windows.Forms.TextBox
     $TextBoxSuffix.Text = $DefaultSuffix
-    $TextBoxSuffix.Location = New-Object Drawing.Point(500, 80)
-    $TextBoxSuffix.Size = New-Object Drawing.Size(300, 30)
+    $TextBoxSuffix.Location = New-Object Drawing.Point(400, 75)
+    $TextBoxSuffix.Size = New-Object Drawing.Size(200, 30)
+    $TextBoxSuffix.Font = New-Object System.Drawing.Font("Segoe UI", 11, [System.Drawing.FontStyle]::Regular)
     $Form.Controls.Add($TextBoxSuffix)
 
     $LabelExtension = New-Object Windows.Forms.Label
@@ -150,70 +155,80 @@ Function FileCreationForm {
     $LabelExtension.Location = New-Object Drawing.Point(20, 130)
     $LabelExtension.AutoSize = $true
     $LabelExtension.ForeColor = [System.Drawing.Color]::White
+    $LabelExtension.Font = New-Object System.Drawing.Font("Segoe UI", 12, [System.Drawing.FontStyle]::Bold)
     $Form.Controls.Add($LabelExtension)
 
     $ComboBoxExtension = New-Object Windows.Forms.ComboBox
-    $ComboBoxExtension.Location = New-Object Drawing.Point(100, 130)
-    $ComboBoxExtension.Size = New-Object Drawing.Size(300, 30)
+    $ComboBoxExtension.Location = New-Object Drawing.Point(130, 125)
+    $ComboBoxExtension.Size = New-Object Drawing.Size(100, 30)
+    $ComboBoxExtension.Font = New-Object System.Drawing.Font("Segoe UI", 11, [System.Drawing.FontStyle]::Regular)
     $Form.Controls.Add($ComboBoxExtension)
 
     $LabelNumberOfFiles = New-Object Windows.Forms.Label
     $LabelNumberOfFiles.Text = "Number of Files:"
-    $LabelNumberOfFiles.Location = New-Object Drawing.Point(420, 130)
+    $LabelNumberOfFiles.Location = New-Object Drawing.Point(296, 130)
     $LabelNumberOfFiles.AutoSize = $true
     $LabelNumberOfFiles.ForeColor = [System.Drawing.Color]::White
+    $LabelNumberOfFiles.Font = New-Object System.Drawing.Font("Segoe UI", 12, [System.Drawing.FontStyle]::Bold)
     $Form.Controls.Add($LabelNumberOfFiles)
 
     $TextBoxNumberOfFiles = New-Object Windows.Forms.TextBox
     $TextBoxNumberOfFiles.Text = $DefaultNumberOfFiles
-    $TextBoxNumberOfFiles.Location = New-Object Drawing.Point(520, 130)
-    $TextBoxNumberOfFiles.Size = New-Object Drawing.Size(100, 30)
+    $TextBoxNumberOfFiles.Location = New-Object Drawing.Point(450, 125)
+    $TextBoxNumberOfFiles.Size = New-Object Drawing.Size(50, 30)
+    $TextBoxNumberOfFiles.Font = New-Object System.Drawing.Font("Segoe UI", 11, [System.Drawing.FontStyle]::Regular)
     $Form.Controls.Add($TextBoxNumberOfFiles)
 
     $CheckBoxOverwrite = New-Object Windows.Forms.CheckBox
     $CheckBoxOverwrite.Text = "Overwrite existing files"
     $CheckBoxOverwrite.Location = New-Object Drawing.Point(20, 180)
-    $CheckBoxOverwrite.Size = New-Object Drawing.Size(200, 30)
+    $CheckBoxOverwrite.AutoSize = $true
     $CheckBoxOverwrite.Checked = $true
     $CheckBoxOverwrite.ForeColor = [System.Drawing.Color]::White
+    $CheckBoxOverwrite.Font = New-Object System.Drawing.Font("Segoe UI", 12, [System.Drawing.FontStyle]::Bold)
     $Form.Controls.Add($CheckBoxOverwrite)
 
     $CheckBoxOpenFiles = New-Object Windows.Forms.CheckBox
     $CheckBoxOpenFiles.Text = "Open files after creation"
-    $CheckBoxOpenFiles.Location = New-Object Drawing.Point(20, 220)
-    $CheckBoxOpenFiles.Size = New-Object Drawing.Size(200, 30)
+    $CheckBoxOpenFiles.Location = New-Object Drawing.Point(20, 210)
+    $CheckBoxOpenFiles.AutoSize = $true
     $CheckBoxOpenFiles.ForeColor = [System.Drawing.Color]::White
+    $CheckBoxOpenFiles.Font = New-Object System.Drawing.Font("Segoe UI", 12, [System.Drawing.FontStyle]::Bold)
     $Form.Controls.Add($CheckBoxOpenFiles)
 
     $LabelContentTemplate = New-Object Windows.Forms.Label
     $LabelContentTemplate.Text = "Content Template:"
-    $LabelContentTemplate.Location = New-Object Drawing.Point(20, 260)
+    $LabelContentTemplate.Location = New-Object Drawing.Point(20, 250)
     $LabelContentTemplate.AutoSize = $true
     $LabelContentTemplate.ForeColor = [System.Drawing.Color]::White
+    $LabelContentTemplate.Font = New-Object System.Drawing.Font("Segoe UI", 12, [System.Drawing.FontStyle]::Bold)
     $Form.Controls.Add($LabelContentTemplate)
 
     $TextBoxContentTemplate = New-Object Windows.Forms.TextBox
-    $TextBoxContentTemplate.Location = New-Object Drawing.Point(150, 260)
+    $TextBoxContentTemplate.Location = New-Object Drawing.Point(20, 280)
     $TextBoxContentTemplate.Multiline = $true
-    $TextBoxContentTemplate.Size = New-Object Drawing.Size(650, 100)
+    $TextBoxContentTemplate.Size = New-Object Drawing.Size(760, 120)
+    $TextBoxContentTemplate.Font = New-Object System.Drawing.Font("Segoe UI", 11, [System.Drawing.FontStyle]::Regular)
     $Form.Controls.Add($TextBoxContentTemplate)
 
     $LabelCreationDate = New-Object Windows.Forms.Label
     $LabelCreationDate.Text = "Creation Date:"
-    $LabelCreationDate.Location = New-Object Drawing.Point(20, 380)
+    $LabelCreationDate.Location = New-Object Drawing.Point(20, 420)
     $LabelCreationDate.AutoSize = $true
     $LabelCreationDate.ForeColor = [System.Drawing.Color]::White
+    $LabelCreationDate.Font = New-Object System.Drawing.Font("Segoe UI", 12, [System.Drawing.FontStyle]::Bold)
     $Form.Controls.Add($LabelCreationDate)
 
     $DateTimePickerCreationDate = New-Object Windows.Forms.DateTimePicker
     $DateTimePickerCreationDate.Format = [Windows.Forms.DateTimePickerFormat]::Custom
     $DateTimePickerCreationDate.CustomFormat = "yyyy-MM-dd HH:mm:ss"
-    $DateTimePickerCreationDate.Location = New-Object Drawing.Point(150, 380)
+    $DateTimePickerCreationDate.Location = New-Object Drawing.Point(150, 415)
+    $DateTimePickerCreationDate.Font = New-Object System.Drawing.Font("Segoe UI", 11, [System.Drawing.FontStyle]::Regular)
     $Form.Controls.Add($DateTimePickerCreationDate)
 
     $ButtonCreate = New-Object Windows.Forms.Button
     $ButtonCreate.Text = "Create Files"
-    $ButtonCreate.Location = New-Object Drawing.Point(330, 460)
+    $ButtonCreate.Location = New-Object Drawing.Point(330, 470)
     $ButtonCreate.Size = New-Object Drawing.Size(150, 40)
     $ButtonCreate.FlatStyle = $Style
     $ButtonCreate.BackColor = [System.Drawing.Color]::FromArgb(60, 60, 60)
@@ -234,12 +249,11 @@ Function FileCreationForm {
     PopulateExtensionComboBox
 
     $Form.ShowDialog()
-
 }
 
 Function PopulateExtensionComboBox {
-    $standardExtensions = @("ps1", "py", "sh", "txt", "exe", "docx", "xlsx", "csv", "pdf", "jpg", "png", "zip", "mp3", "mp4")
-    $ComboBoxExtension.Items.AddRange($standardExtensions)
+    $StandardExtensions = @("ps1", "py", "sh", "txt", "exe", "docx", "xlsx", "csv", "pdf", "jpg", "png", "zip", "mp3", "mp4")
+    $ComboBoxExtension.Items.AddRange($StandardExtensions)
 }
 
 Function ValidateFormFields {
