e2198081b89b1b745be5f1b1322935e1431be0d9
theFest <8981383+theFest@users.noreply.github.com>
2023-08-11 16:03:04 +0200

    Update GetGitHubContent
    auth. method, encoding.

--- ps-github/GetGitHubContent.ps1
+++ ps-github/GetGitHubContent.ps1
@@ -2,37 +2,39 @@ Function GetGitHubContent {
     <#
     .SYNOPSIS
     GitHub content downloader.
-    
+
     .DESCRIPTION
-    This function enables you to download single or multiple file direcly from GitHub using Personal Access Token.
-    
-    .PARAMETER PAT
-    Mandatory - Personal Access Token (PAT) for GitHub authentication.
+    This function enables you to download single or multiple files/directories directly from GitHub using various authentication methods.
+
+    .PARAMETER AuthToken
+    Mandatory - GitHub Personal Access Token (PAT) for authentication.
+    .PARAMETER AuthMethod
+    NotMandatory - authentication method to use, possible values: "PAT" (Personal Access Token), "Basic" (Basic Authentication).
     .PARAMETER Paths
-    Mandatory - paths to download from the repository.
+    Mandatory - paths to download from the repository, can include files or directories.
     .PARAMETER Owner
-    Mandatory - owner of the GitHub repository
+    Mandatory - owner of the GitHub repository.
     .PARAMETER Repository
     Mandatory - name of the GitHub repository.
     .PARAMETER Branch
-    Mandatory - branch name of the GitHub repository.
+    NotMandatory - branch name of the GitHub repository, default is "main".
     .PARAMETER DestinationPath
-    NotMandatory - local destination path to save the downloaded files.
+    NotMandatory - local destination path to save the downloaded files, default is "$env:USERPROFILE\Desktop".
     .PARAMETER GitHubBaseUrl
-    NotMandatory - base URL for the GitHub content.
-    
+    NotMandatory - base URL for the GitHub content, default is "https://raw.githubusercontent.com".
+
     .EXAMPLE
-    #GetGitHubContent -Owner "your_GitHub_name" -Repository "your_repo_name" -Branch "you_branch" -PAToken "ghp_xyz" -Paths "your_repo_folder/your_file_on_GitHub.ps1"
-    "ghp_xyz" | GetGitHubContent -Owner "your_GitHub_name" -Repository "your_repo_name" -Branch "you_branch" -Paths "your_repo_folder/your_file_2.ps1", "your_repo_folder/your_file_2.ps1"
+    Get-GitHubContent -Owner "your_GitHub_name" -Repository "your_repo_name" -Branch "your_branch" -AuthToken "ghp_xyz" -Paths "your_repo_folder/your_file_on_GitHub.ps1"
+    "ghp_xyz" | Get-GitHubContent -Owner "your_GitHub_name" -Repository "your_repo_name" -Branch "your_branch" -Paths "your_repo_folder/your_file_1.ps1", "your_repo_folder/your_file_2.ps1"
     
     .NOTES
-    v0.0.1
+    v0.0.2
     #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [ValidateNotNullOrEmpty()]
-        [string]$PAToken,
+        [string]$AuthToken,
 
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
@@ -55,14 +57,19 @@ Function GetGitHubContent {
         [string]$DestinationPath = "$env:USERPROFILE\Desktop",
 
         [Parameter(Mandatory = $false)]
-        [string]$GitHubBaseUrl = "https://raw.githubusercontent.com"
+        [string]$GitHubBaseUrl = "https://raw.githubusercontent.com",
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("PAT", "Basic")]
+        [string]$AuthMethod = "PAT"
     )
     try {
         if (-not (Test-Path -Path $DestinationPath)) {
-            New-Item -ItemType Directory -Path $DestinationPath -Force -Verbose
+            Write-Verbose -Message "Creating new folder..."
+            New-Item -ItemType Directory -Path $DestinationPath -Force | Out-Null
         }
         foreach ($Path in $Paths) {
-            Write-Verbose -Message "Preparing to download file's..."
+            Write-Verbose -Message "Preparing to download files..."
             $DecodedPath = [Uri]::UnescapeDataString($Path)
             $ContentUrl = "$GitHubBaseUrl/$Owner/$Repository/$Branch/$DecodedPath"
             $DestinationFile = Join-Path -Path $DestinationPath -ChildPath $DecodedPath
@@ -71,13 +78,25 @@ Function GetGitHubContent {
                 New-Item -ItemType Directory -Path $DestinationDirectory -Force -Verbose
             }
             try {
-                $Response = Invoke-WebRequest -Uri $ContentUrl -Headers @{
-                    "Authorization" = "Bearer $PAToken"
-                    "User-Agent"    = "powershell-script"
-                } -UseBasicParsing -ErrorAction Stop
-                $Content = $Response.Content
-                $Content | Out-File -FilePath $DestinationFile -Force -ErrorAction Stop
-                Write-Output -InputObject "Downloaded: $($Response.BaseResponse.ResponseUri.AbsoluteUri)"
+                $Headers = @{
+                    "User-Agent" = "powershell-script"
+                }
+                if ($AuthMethod -eq "PAT") {
+                    $Headers["Authorization"] = "Bearer $AuthToken"
+                }
+                elseif ($AuthMethod -eq "Basic") {
+                    $Base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$AuthToken"))
+                    $Headers["Authorization"] = "Basic $Base64Auth"
+                }
+                $Response = Invoke-WebRequest -Uri $ContentUrl -Headers $Headers -UseBasicParsing -ErrorAction Stop
+                if ($Response.StatusCode -eq 200) {
+                    $ContentBytes = [System.Text.Encoding]::UTF8.GetBytes($Response.Content)
+                    [System.IO.File]::WriteAllBytes($DestinationFile, $ContentBytes)
+                    Write-Output -InputObject "Downloaded: $($Response.BaseResponse.ResponseUri.AbsoluteUri)"
+                }
+                else {
+                    Write-Error -Message "Failed to download: $ContentUrl. Status code: $($Response.StatusCode)"
+                }
             }
             catch {
                 Write-Error -Message "Failed to download: $ContentUrl. Error: $($_.Exception.Message)"
@@ -89,6 +108,6 @@ Function GetGitHubContent {
     }
     Write-Verbose -Message "Finished, cleaning and exiting..."
     Clear-History -Verbose -ErrorAction SilentlyContinue
-    Clear-Variable -Name PAToken, Owner, Repository, Paths -Scope Global -Force -Verbose -ErrorAction SilentlyContinue
-    Remove-Variable -Name PAToken, Owner, Repository, Paths -Scope Global -Force -Verbose -ErrorAction SilentlyContinue
+    Clear-Variable -Name AuthToken, Owner, Repository, Paths -Scope Global -Force -ErrorAction SilentlyContinue
+    Remove-Variable -Name AuthToken, Owner, Repository, Paths -Scope Global -Force -ErrorAction SilentlyContinue
 }
