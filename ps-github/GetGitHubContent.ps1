43919568632b5b901aec8bc10102a162b39d2a7c
theFest <8981383+theFest@users.noreply.github.com>
2023-08-11 17:03:50 +0200

    Update GetGitHubContent
    tags and other improvments.

--- ps-github/GetGitHubContent.ps1
+++ ps-github/GetGitHubContent.ps1
@@ -1,10 +1,10 @@
 Function GetGitHubContent {
     <#
     .SYNOPSIS
-    GitHub content downloader.
+    GitHub content downloader with extended capabilities.
 
     .DESCRIPTION
-    This function enables you to download single or multiple files/directories directly from GitHub using various authentication methods.
+    This function enables you to download single or multiple files/directories directly from GitHub using various authentication methods. It also supports downloading from specific commits and tagging.
 
     .PARAMETER AuthToken
     Mandatory - GitHub Personal Access Token (PAT) for authentication.
@@ -18,17 +18,22 @@ Function GetGitHubContent {
     Mandatory - name of the GitHub repository.
     .PARAMETER Branch
     NotMandatory - branch name of the GitHub repository, default is "main".
+    .PARAMETER Commit
+    NotMandatory - commit hash to download content from a specific commit.
+    .PARAMETER Tag
+    NotMandatory - tag name to download content from a specific tag.
     .PARAMETER DestinationPath
     NotMandatory - local destination path to save the downloaded files, default is "$env:USERPROFILE\Desktop".
     .PARAMETER GitHubBaseUrl
     NotMandatory - base URL for the GitHub content, default is "https://raw.githubusercontent.com".
 
     .EXAMPLE
-    Get-GitHubContent -Owner "your_GitHub_name" -Repository "your_repo_name" -Branch "your_branch" -AuthToken "ghp_xyz" -Paths "your_repo_folder/your_file_on_GitHub.ps1"
-    "ghp_xyz" | Get-GitHubContent -Owner "your_GitHub_name" -Repository "your_repo_name" -Branch "your_branch" -Paths "your_repo_folder/your_file_1.ps1", "your_repo_folder/your_file_2.ps1"
+    GetGitHubContent -Owner "your_GitHub_name" -Repository "your_repo_name" -Branch "your_branch" -AuthToken "ghp_xyz" -Paths "your_repo_folder/your_file_on_GitHub.ps1" -Verbose
+    GetGitHubContent -Owner "your_GitHub_name" -Repository "your_repo_name" -Branch "your_branch" -AuthToken "ghp_xyz" -Paths "your_repo_folder/your_file_on_GitHub.ps1", "your_repo_folder/your_file_on_GitHub.ps1"
+    "ghp_xyz" | GetGitHubContent -Owner "theFest" -Repository "PowerShell-Scripts" -Branch "main" -Paths "your_repo_folder/your_file_on_GitHub.ps1" -Commit "commit_hash" -DestinationPath "$env:SystemDrive\Temp"
     
     .NOTES
-    v0.0.2
+    v0.0.3
     #>
     [CmdletBinding()]
     param (
@@ -36,7 +41,7 @@ Function GetGitHubContent {
         [ValidateNotNullOrEmpty()]
         [string]$AuthToken,
 
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false)]
         [ValidateNotNullOrEmpty()]
         [string[]]$Paths,
 
@@ -54,6 +59,14 @@ Function GetGitHubContent {
 
         [Parameter(Mandatory = $false)]
         [ValidateNotNullOrEmpty()]
+        [string]$Commit,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Tag,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
         [string]$DestinationPath = "$env:USERPROFILE\Desktop",
 
         [Parameter(Mandatory = $false)]
@@ -63,51 +76,65 @@ Function GetGitHubContent {
         [ValidateSet("PAT", "Basic")]
         [string]$AuthMethod = "PAT"
     )
-    try {
+    BEGIN {
         if (-not (Test-Path -Path $DestinationPath)) {
             Write-Verbose -Message "Creating new folder..."
-            New-Item -ItemType Directory -Path $DestinationPath -Force | Out-Null
+            New-Item -ItemType Directory -Path $DestinationPath -InformationAction SilentlyContinue -Verbose
         }
-        foreach ($Path in $Paths) {
-            Write-Verbose -Message "Preparing to download files..."
-            $DecodedPath = [Uri]::UnescapeDataString($Path)
-            $ContentUrl = "$GitHubBaseUrl/$Owner/$Repository/$Branch/$DecodedPath"
-            $DestinationFile = Join-Path -Path $DestinationPath -ChildPath $DecodedPath
-            $DestinationDirectory = Split-Path -Path $DestinationFile -Parent
-            if (-not (Test-Path -Path $DestinationDirectory)) {
-                New-Item -ItemType Directory -Path $DestinationDirectory -Force -Verbose
-            }
-            try {
-                $Headers = @{
-                    "User-Agent" = "powershell-script"
+    }
+    PROCESS {
+        try {
+            foreach ($Path in $Paths) {
+                Write-Verbose -Message "Preparing to download files..."
+                $DecodedPath = [Uri]::UnescapeDataString($Path)
+                if ($Commit) {
+                    $ContentUrl = "$GitHubBaseUrl/$Owner/$Repository/$Commit/$DecodedPath"
                 }
-                if ($AuthMethod -eq "PAT") {
-                    $Headers["Authorization"] = "Bearer $AuthToken"
-                }
-                elseif ($AuthMethod -eq "Basic") {
-                    $Base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$AuthToken"))
-                    $Headers["Authorization"] = "Basic $Base64Auth"
-                }
-                $Response = Invoke-WebRequest -Uri $ContentUrl -Headers $Headers -UseBasicParsing -ErrorAction Stop
-                if ($Response.StatusCode -eq 200) {
-                    $ContentBytes = [System.Text.Encoding]::UTF8.GetBytes($Response.Content)
-                    [System.IO.File]::WriteAllBytes($DestinationFile, $ContentBytes)
-                    Write-Output -InputObject "Downloaded: $($Response.BaseResponse.ResponseUri.AbsoluteUri)"
+                elseif ($Tag) {
+                    $ContentUrl = "$GitHubBaseUrl/$Owner/$Repository/$Tag/$DecodedPath"
                 }
                 else {
-                    Write-Error -Message "Failed to download: $ContentUrl. Status code: $($Response.StatusCode)"
+                    $ContentUrl = "$GitHubBaseUrl/$Owner/$Repository/$Branch/$DecodedPath"
+                }
+                $DestinationFile = Join-Path -Path $DestinationPath -ChildPath $DecodedPath
+                $DestinationDirectory = Split-Path -Path $DestinationFile -Parent
+                if (-not (Test-Path -Path $DestinationDirectory)) {
+                    New-Item -ItemType Directory -Path $DestinationDirectory -Force -Verbose
+                }
+                try {
+                    $Headers = @{
+                        "User-Agent" = "powershell-script"
+                    }
+                    if ($AuthMethod -eq "PAT") {
+                        $Headers["Authorization"] = "Bearer $AuthToken"
+                    }
+                    elseif ($AuthMethod -eq "Basic") {
+                        $Base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$AuthToken"))
+                        $Headers["Authorization"] = "Basic $Base64Auth"
+                    }
+                    $Response = Invoke-WebRequest -Uri $ContentUrl -Headers $Headers -UseBasicParsing -ErrorAction Stop
+                    if ($Response.StatusCode -eq 200) {
+                        $ContentBytes = [System.Text.Encoding]::UTF8.GetBytes($Response.Content)
+                        [System.IO.File]::WriteAllBytes($DestinationFile, $ContentBytes)
+                        Write-Output -InputObject "Downloaded: $($Response.BaseResponse.ResponseUri.AbsoluteUri)"
+                    }
+                    else {
+                        Write-Error -Message "Failed to download: $ContentUrl. Status code: $($Response.StatusCode)"
+                    }
+                }
+                catch {
+                    Write-Error -Message "Failed to download: $ContentUrl. Error: $($_.Exception.Message)"
                 }
             }
-            catch {
-                Write-Error -Message "Failed to download: $ContentUrl. Error: $($_.Exception.Message)"
-            }
+        }
+        catch {
+            Write-Error -Message "An error occurred while downloading content. Error: $($_.Exception.Message)"
         }
     }
-    catch {
-        Write-Error -Message "An error occurred while downloading content. Error: $($_.Exception.Message)"
+    END {
+        Write-Host "Finished, cleaning and exiting..."
+        Clear-History -Verbose -ErrorAction SilentlyContinue
+        Clear-Variable -Name AuthToken, Owner, Repository, Paths -Scope Global -Force -ErrorAction SilentlyContinue
+        Remove-Variable -Name AuthToken, Owner, Repository, Paths -Scope Global -Force -ErrorAction SilentlyContinue
     }
-    Write-Verbose -Message "Finished, cleaning and exiting..."
-    Clear-History -Verbose -ErrorAction SilentlyContinue
-    Clear-Variable -Name AuthToken, Owner, Repository, Paths -Scope Global -Force -ErrorAction SilentlyContinue
-    Remove-Variable -Name AuthToken, Owner, Repository, Paths -Scope Global -Force -ErrorAction SilentlyContinue
 }
