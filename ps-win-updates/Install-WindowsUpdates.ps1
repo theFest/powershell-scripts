37d5304570b0b2badbd97ec24dacd6608b1000ea
theFest <8981383+theFest@users.noreply.github.com>
2023-08-30 06:54:18 +0200

    Update Install-WindowsUpdates
    rename, improve and fixed errors

--- ps-win-updates/Install-WindowsUpdates.ps1
+++ ps-win-updates/Install-WindowsUpdates.ps1
@@ -1,11 +1,10 @@
-Function InstallWindowsUpdates {
+Function Install-WindowsUpdates {
     <#
     .SYNOPSIS
     Install available Windows updates on a remote computer.
     
     .DESCRIPTION
-    This function connects to a remote computer and installs available Windows updates.
-    It provides various options to control the installation process and display update details.
+    This function connects to a remote computer and installs available Windows updates, it provides various options to control the installation process and display update details.
     
     .PARAMETER ComputerName
     NotMandatory - name of the remote computer where updates will be installed.
@@ -14,27 +13,28 @@ Function InstallWindowsUpdates {
     .PARAMETER Pass
     NotMandatory - password for the provided username used to authenticate to the remote computer.
     .PARAMETER IncludeHidden
-    NotMandatory - if specified, hidden updates will also be installed.
+    NotMandatory - hidden updates will also be installed.
     .PARAMETER IncludeInstalled
-    NotMandatory - if specified, already installed updates will be reinstalled.
+    NotMandatory - already installed updates will be reinstalled.
     .PARAMETER IncludeRebootRequired
-    NotMandatory - if specified, updates requiring a system reboot will also be installed.
+    NotMandatory - updates requiring a system reboot will also be installed.
     .PARAMETER Reboot
-    NotMandatory - if specified, reboot the remote computer after updates are installed.
+    NotMandatory - reboot the remote computer after updates are installed.
     .PARAMETER RebootDelay
     NotMandatory - specify the delay in seconds before initiating a reboot.
     .PARAMETER Wait
-    NotMandatory - if specified, wait for the specified number of seconds before proceeding with installation.
+    NotMandatory - wait for the specified number of seconds before proceeding with installation.
     .PARAMETER ShowProgress
-    NotMandatory - if specified, show progress of update installation.
+    NotMandatory - show progress of update installation.
     .PARAMETER ShowUpdateDetails
-    NotMandatory - if specified, show details about available updates on the remote computer.
+    NotMandatory - show details about available updates on the remote computer.
     
     .EXAMPLE
-    InstallWindowsUpdates -ComputerName "remote_host" -Username "remote_user" -Pass "remote_pass" -IncludeRebootRequired -Reboot -RebootDelay 300 -Wait 60 -ShowProgress -ShowUpdateDetails -Verbose
+    Install-WindowsUpdates -ShowUpdateDetails -Verbose
+    Install-WindowsUpdates -ComputerName "remote_host" -Username "remote_user" -Pass "remote_pass" -IncludeRebootRequired -Reboot -RebootDelay 300 -Wait 60 -ShowProgress -ShowUpdateDetails
     
     .NOTES
-    v0.0.4
+    v0.0.5
     #>
     [CmdletBinding()]
     param (
@@ -77,12 +77,6 @@ Function InstallWindowsUpdates {
             $SecurePassword = ConvertTo-SecureString -String $Pass -AsPlainText -Force
             $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, $SecurePassword
         }
-        if ($ShowUpdateDetails) {
-            $UpdateSession = New-Object -ComObject Microsoft.Update.Session
-            $Searcher = $UpdateSession.CreateUpdateSearcher()
-            $SearchResult = $Searcher.Search("IsInstalled=0")
-            $SearchResult.Updates | Select-Object Title, Description, IsHidden, IsMandatory, RebootRequired, LastDeploymentChangeTime, MoreInfoUrls
-        }
         $InstallScriptBlock = {
             param (
                 $IncludeHidden,
@@ -97,7 +91,7 @@ Function InstallWindowsUpdates {
                 $Criteria += " or IsHidden=1"
             }
             if ($IncludeInstalled) {
-                $Criteria = "IsInstalled=1"
+                $Criteria += " or IsInstalled=1"
             }
             if ($IncludeRebootRequired) {
                 $Criteria += " or RebootRequired=1"
@@ -129,15 +123,18 @@ Function InstallWindowsUpdates {
                 Write-Verbose -Message "Removing the remote session with $ComputerName..."
                 Remove-PSSession -Session $Session -ErrorAction SilentlyContinue
             }
+            if ($Reboot) {
+                Write-Verbose -Message "Rebooting the remote computer in $RebootDelay seconds..."
+                Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
+                    param ($Delay)
+                    Start-Sleep -Seconds $Delay
+                    Restart-Computer -Force
+                } -ArgumentList $RebootDelay
+            }
         }
         else {
             Write-Verbose -Message "Installing Windows updates locally..."
             & $InstallScriptBlock -IncludeHidden $IncludeHidden -IncludeInstalled $IncludeInstalled -IncludeRebootRequired $IncludeRebootRequired -ShowProgress $ShowProgress
-        }
-        if ($Reboot) {
-            Write-Verbose -Message "Rebooting the computer in $RebootDelay seconds..."
-            Start-Sleep -Seconds $RebootDelay
-            Restart-Computer -Force
         }
         if ($Wait -gt 0) {
             Write-Verbose -Message "Waiting for $Wait seconds..."
