fd7e4f97c8b69837e8677b035e26743abd267051
theFest <8981383+theFest@users.noreply.github.com>
2023-08-31 02:24:49 +0200

    Update Get-DisplayInfo
    fixes and methods

--- ps-screen-display/Get-DisplayInfo.ps1
+++ ps-screen-display/Get-DisplayInfo.ps1
@@ -1,25 +1,25 @@
 Function Get-DisplayInfo {
     <#
     .SYNOPSIS
-    Retrieves information about computer display monitors.
+    Retrieves information about computer display monitors using multiple methods.
 
     .DESCRIPTION
-    This function retrieves various information about computer display monitors, including their names, manufacturer, type, screen dimensions, and more, using alternative methods.
+    This function retrieves various information about computer display monitors, including their names, manufacturer, type, screen dimensions, and more, using multiple methods.
 
     .PARAMETER Detailed
-    NotMandatory - specifies whether to include detailed information about the displays, such as refresh rate and pixels per logical inch.
+    NotMandatory - Specifies whether to include detailed information about the displays, such as refresh rate and pixels per logical inch.
     .PARAMETER IncludeAllResolutions
-    NotMandatory - specifies whether to include all available display resolutions.
+    NotMandatory - Specifies whether to include all available display resolutions.
     .PARAMETER IncludePhysicalDimensions
-    NotMandatory - specifies whether to include the physical dimensions of the displays in inches.
+    NotMandatory - Specifies whether to include the physical dimensions of the displays in inches.
     .PARAMETER ExportPath
-    NotMandatory - specifies the path to export the display information to a CSV file.
+    NotMandatory - Specifies the path to export the display information to a CSV file.
 
     .EXAMPLE
-    Get-DisplayInfo -Detailed -IncludeAllResolutions -IncludePhysicalDimensions -ExportPath "$env:USERPROFILE\Desktop\DisplayInfo.csv"
+    Get-DisplayInfo -Detailed -IncludeAllResolutions -IncludePhysicalDimensions -ExportPath "$env:USERPROFILE\Desktop\DisplayInfo.csv" -Verbose
 
     .NOTES
-    v0.0.3
+    v0.0.4
     #>
     [CmdletBinding()]
     param (
@@ -29,38 +29,57 @@ Function Get-DisplayInfo {
         [string]$ExportPath
     )
     $DisplayInfoList = @()
+    Write-Verbose -Message "Getting display information using Win32_DesktopMonitor class, with first approach..."
     $DisplayMonitors = Get-CimInstance -ClassName Win32_DesktopMonitor
     foreach ($Display in $DisplayMonitors) {
         $DisplayInfo = @{
-            Name                = $Display.DeviceID
-            MonitorManufacturer = $Display.Manufacturer
-            MonitorType         = $Display.PNPDeviceID
-            ScreenWidth         = $Display.ScreenWidth
-            ScreenHeight        = $Display.ScreenHeight
-        }
-        if ($Detailed) {
-            $DisplayInfo.RefreshRate = $Display.RefreshRate
-            $DisplayInfo.PixelsPerXLogicalInch = $Display.PixelsPerXLogicalInch
-            $DisplayInfo.PixelsPerYLogicalInch = $Display.PixelsPerYLogicalInch
-        }        
-        if ($IncludeAllResolutions) {
-            $VideoControllers = Get-CimInstance -ClassName Win32_VideoController
-            $Resolutions = @()
-            foreach ($Controller in $VideoControllers) {
-                $Resolutions += $Controller.VideoModeDescription
-            }
-            $DisplayInfo.AllResolutions = $Resolutions
-        }     
-        if ($IncludePhysicalDimensions) {
-            $DisplayInfo.PhysicalWidth = $Display.ScreenWidth / $Display.PixelsPerXLogicalInch
-            $DisplayInfo.PhysicalHeight = $Display.ScreenHeight / $Display.PixelsPerYLogicalInch
+            Name                  = $Display.DeviceID
+            MonitorManufacturer   = $Display.Manufacturer
+            MonitorType           = $Display.PNPDeviceID
+            ScreenWidth           = $Display.ScreenWidth
+            ScreenHeight          = $Display.ScreenHeight
+            PixelsPerXLogicalInch = $Display.PixelsPerXLogicalInch
+            PixelsPerYLogicalInch = $Display.PixelsPerYLogicalInch
+            RefreshRate           = $Display.RefreshRate
+            PhysicalWidth         = [math]::Round($Display.ScreenWidth / $Display.PixelsPerXLogicalInch, 2)
+            PhysicalHeight        = [math]::Round($Display.ScreenHeight / $Display.PixelsPerYLogicalInch, 2)
+            AllResolutions        = $Display.VideoModeDescription -join "; "
         }
         $DisplayObject = New-Object PSObject -Property $DisplayInfo
         $DisplayInfoList += $DisplayObject
     }
-    Write-Output -InputObject $DisplayInfoList
+    Write-Verbose -Message "Getting display information using Win32_VideoController class, second method..."
+    $VideoControllers = Get-CimInstance -ClassName Win32_VideoController
+    foreach ($Controller in $VideoControllers) {
+        $DisplayInfo = @{
+            Name           = $Controller.Caption
+            Manufacturer   = $Controller.Manufacturer
+            VideoMode      = $Controller.CurrentHorizontalResolution
+            RefreshRate    = $Controller.CurrentRefreshRate
+            VideoProcessor = $Controller.VideoProcessor
+            MaxRefreshRate = $Controller.MaxRefreshRate
+        }
+        $DisplayObject = New-Object PSObject -Property $DisplayInfo
+        $DisplayInfoList += $DisplayObject
+    }
+    Write-Verbose -Message "Getting display information using Win32_DisplayConfiguration class, third method..."
+    $DisplayConfigurations = Get-WmiObject -Namespace "root\cimv2" -Class Win32_DisplayConfiguration
+    foreach ($DisplayConfig in $DisplayConfigurations) {
+        $DisplayInfo = @{
+            DeviceName       = $DisplayConfig.DeviceName
+            BitsPerPel       = $DisplayConfig.BitsPerPel
+            PelsWidth        = $DisplayConfig.PelsWidth
+            PelsHeight       = $DisplayConfig.PelsHeight
+            DisplayFrequency = $DisplayConfig.DisplayFrequency
+        }
+        $DisplayObject = New-Object PSObject -Property $DisplayInfo
+        $DisplayInfoList += $DisplayObject
+    }
+    Write-Verbose -Message "Outputting all display information..."
+    $DisplayInfoList | Format-Table -AutoSize Name, MonitorManufacturer, MonitorType, ScreenWidth, ScreenHeight, `
+        PixelsPerXLogicalInch, PixelsPerYLogicalInch, RefreshRate, PhysicalWidth, PhysicalHeight, AllResolutions
     if ($ExportPath) {
         $DisplayInfoList | Export-Csv -Path $ExportPath -NoTypeInformation
-        Write-Host "Display information exported to $ExportPath" -ForegroundColor Cyan
+        Write-Host "Display information exported to $ExportPath" -ForegroundColor DarkCyan
     }
 }
