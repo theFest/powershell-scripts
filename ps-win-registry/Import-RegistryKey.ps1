87772750ae024924a683c976f1825525f88bf228
theFest <8981383+theFest@users.noreply.github.com>
2023-10-21 00:01:50 +0200

    Update Import-RegistryKe
    rename and bump

--- ps-win-registry/Import-RegistryKey.ps1
+++ ps-win-registry/Import-RegistryKey.ps1
@@ -1,10 +1,11 @@
-Function RemoteImportRegKey {
+Function Import-RegistryKey {
     <#
     .SYNOPSIS
     Imports a registry key from a local or remote machine.
     
     .DESCRIPTION
-    This function allows you to import a registry key from either a local or remote machine. It establishes a remote PowerShell session if the ComputerName parameter is provided, copies the registry file to the remote machine, imports the registry key using the 'reg.exe' command, and removes the copied file afterwards. If no ComputerName is specified, the function imports the registry key locally.
+    This function allows you to import a registry key from either a local or remote machine.
+    It establishes a remote PowerShell session if the ComputerName parameter is provided, copies the registry file to the remote machine, imports the registry key using the 'reg.exe' command, and removes the copied file afterwards. If no ComputerName is specified, the function imports the registry key locally.
     
     .PARAMETER ImportPath
     Mandatory - path to the registry file that needs to be imported. This parameter is mandatory.
@@ -12,14 +13,14 @@ Function RemoteImportRegKey {
     NotMandatory - name of the remote computer from which to import the registry key. This parameter is not mandatory. If provided, a remote session will be established.
     .PARAMETER Username
     NotMandatory - username for authentication when connecting to a remote computer. This parameter is not mandatory and is only required when the ComputerName parameter is provided.
-    .PARAMETER Pass
+    .PARAMETER Password
     NotMandatory - password for authentication when connecting to a remote computer. This parameter is not mandatory and is only required when the ComputerName parameter is provided.
     
     .EXAMPLE
-    RemoteImportRegKey -ImportPath "$env:USERPROFILE\Desktop\import.reg" -ComputerName "remote_hostename" -Username "remote_user" -Pass "remote_pass" -Verbose
+    Import-RegistryKey -ImportPath "$env:USERPROFILE\Desktop\import.reg" -ComputerName "remote_hostname" -Username "remote_user" -Password "remote_pass" -Verbose
     
     .NOTES
-    v0.0.2
+    v0.0.3
     #>
     [CmdletBinding()]
     param (
@@ -34,11 +35,11 @@ Function RemoteImportRegKey {
         [string]$Username,
         
         [Parameter(Mandatory = $false)]
-        [string]$Pass
+        [string]$Password
     )
     try {
         if ($ComputerName) {
-            $Session = New-PSSession -ComputerName $ComputerName -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, (ConvertTo-SecureString -String $Pass -AsPlainText -Force))
+            $Session = New-PSSession -ComputerName $ComputerName -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, (ConvertTo-SecureString -String $Password -AsPlainText -Force))
             $TestConnectionScriptBlock = {
                 (Test-Connection -ComputerName $Using:ComputerName -Count 1 -Quiet) -and (Test-WSMan -ComputerName $Using:ComputerName -Authentication Default)
             }
