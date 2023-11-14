1f3cd824273b53b359b76d13b28b358e239e874d
theFest <8981383+theFest@users.noreply.github.com>
2023-11-14 01:08:20 +0100

    Update Get-ServiceInfo
    rename, improve and bump

--- ps-services/Get-ServiceInfo.ps1
+++ ps-services/Get-ServiceInfo.ps1
@@ -1,10 +1,10 @@
-Function GetServiceInfo {
+Function Get-ServiceInfo {
     <#
     .SYNOPSIS
     Retrieves information about services on a specified computer.
 
     .DESCRIPTION
-    This function retrieves information about services on a local or remote computer, allows filtering by service name, provides options to include or exclude stopped and manually started services, and supports various output formats.
+    Retrieves information about services on a local or remote computer, allows filtering by service name, provides options to include or exclude stopped and manually started services, and supports various output formats.
 
     .PARAMETER ComputerName
     NotMandatory - name of the computer on which to retrieve the service information. If not provided, the local computer is used by default.
@@ -13,7 +13,7 @@ Function GetServiceInfo {
     .PARAMETER UserName
     NotMandatory - username to use for authentication when retrieving service information from a remote computer.
     .PARAMETER Password
-    NotMandatory - password to use for authentication when retrieving service information from a remote computer. The password should be provided as a SecureString object.
+    NotMandatory - password to use for authentication when retrieving service information from a remote computer, should be provided as a SecureString object.
     .PARAMETER PingBefore
     NotMandatory - indicates whether to ping the specified computer before retrieving the services. This can be useful to verify the computer's availability before performing the operation.
     .PARAMETER IncludeStopped
@@ -25,49 +25,55 @@ Function GetServiceInfo {
     .PARAMETER ProcessAsJob
     NotMandatory - executes the retrieval of service information as a background job. This allows for parallel processing and asynchronous execution.
     .PARAMETER FormatAsTable
-    NotMandatory - the output as a table. If specified, the output is displayed in a table format.
+    NotMandatory - the output as a table, if specified, the output is displayed in a table format.
     .PARAMETER ExportCsvPath
     NotMandatory - specifies the path to export the retrieved service information as a CSV file.
+    .PARAMETER FilterWildcard
+    NotMandatory - a wildcard pattern used to filter services based on their display names, only services with display names matching the pattern will be included in the output.
+
     .EXAMPLE
     GetServiceInfo -ComputerName "Server01" -ServiceName "MyService"
 
     .NOTES
-    v0.0.1
+    v0.0.2
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false, Position = 0)]
+        [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline = $true)]
         [string]$ComputerName = $env:COMPUTERNAME,
-        
+
         [Parameter(Mandatory = $false)]
         [string]$ServiceName,
-        
+
         [Parameter(Mandatory = $false)]
         [string]$UserName,
-        
+
         [Parameter(Mandatory = $false)]
         [SecureString]$Password,
-        
+
         [Parameter(Mandatory = $false)]
         [switch]$PingBefore,
-        
+
         [Parameter(Mandatory = $false)]
         [switch]$IncludeStopped,
-        
+
         [Parameter(Mandatory = $false)]
         [switch]$IncludeManualStart,
-        
+
         [Parameter(Mandatory = $false)]
         [switch]$SortByStatus,
-        
+
         [Parameter(Mandatory = $false)]
         [switch]$ProcessAsJob,
-        
+
         [Parameter(Mandatory = $false)]
         [switch]$FormatAsTable,
-        
+
         [Parameter(Mandatory = $false)]
-        [string]$ExportCsvPath
+        [string]$ExportCsvPath,
+
+        [Parameter(Mandatory = $false)]
+        [string]$FilterWildcard
     )
     BEGIN {
         $params = @{
@@ -98,6 +104,9 @@ Function GetServiceInfo {
             Write-Error "Failed to retrieve services: $($_.Exception.Message)"
             return
         }
+        if ($FilterWildcard) {
+            $Services = $Services | Where-Object { $_.DisplayName -like $FilterWildcard }
+        }
         if (-not $IncludeStopped) {
             $Services = $Services | Where-Object { $_.Status -ne 'Stopped' }
         }
@@ -112,7 +121,7 @@ Function GetServiceInfo {
         }
         else {
             foreach ($Service in $Services) {
-                $ServiceInfo = @{
+                $ServiceInfo = [PSCustomObject]@{
                     Name        = $Service.Name
                     DisplayName = $Service.DisplayName
                     Status      = $Service.Status
@@ -122,11 +131,13 @@ Function GetServiceInfo {
                     Account     = $Service.ServiceAccountName
                     StartName   = $Service.StartName
                 }
+
                 if ($ProcessAsJob) {
                     $Job = Start-Job -ScriptBlock {
                         param($Info)
                         Write-Output $Info
                     } -ArgumentList $ServiceInfo
+
                     $ServiceInfo.JobId = $Job.Id
                 }
                 Write-Output $ServiceInfo
