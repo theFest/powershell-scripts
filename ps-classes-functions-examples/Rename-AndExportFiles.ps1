c93819a9d7065829bafc1ab807ba089b862f06a4
Fest White <8981383+theFest@users.noreply.github.com>
2023-12-03 02:53:07 +0100

    Update Rename-AndExportFiles
    fix and improve, remove class

--- ps-classes-functions-examples/Rename-AndExportFiles.ps1
+++ ps-classes-functions-examples/Rename-AndExportFiles.ps1
@@ -1,71 +1,3 @@
-Class FileOperations {
-    [string]$FilesPath
-    [string]$FileExtension
-    [string]$OutFile
-    [string]$RenameStringIn
-    [string]$RenameStringOut
-    [switch]$CSVFormat
-    [switch]$IncludeSubdirectories
-    [switch]$AppendToOutput
-    [switch]$SkipExistingFiles
-    [switch]$LogActions
-    [string]$LogFilePath
-    FileOperations(
-        [string]$FilesPath,
-        [string]$FileExtension,
-        [string]$OutFile
-    ) {
-        $this.FilesPath = $FilesPath
-        $this.FileExtension = $FileExtension
-        $this.OutFile = $OutFile
-    }
-    [void] ProcessFiles() {
-        try {
-            if (!(Test-Path -Path $this.FilesPath -PathType Container)) {
-                throw "The specified folder does not exist."
-            }
-            $Filter = "*.*"
-            if ($this.FileExtension) {
-                $Filter = "*.$($this.FileExtension)"
-            }
-            $Files = Get-ChildItem -Path $this.FilesPath -File -Recurse:$this.IncludeSubdirectories -Filter $Filter
-            if ($Files.Count -eq 0) {
-                throw "No files found with the specified extension."
-            }
-            if ($this.LogActions -and $this.LogFilePath) {
-                $LogEntries = @()
-            }
-            foreach ($File in $Files) {
-                if ($this.RenameStringIn) {
-                    $NewName = $File.Name -replace $this.RenameStringIn, $this.RenameStringOut
-                    if ($NewName -ne $File.Name) {
-                        if ($this.SkipExistingFiles -and Test-Path (Join-Path $File.DirectoryName $NewName)) {
-                            if ($this.LogActions -and $this.LogFilePath) {
-                                $LogEntries += "Skipped renaming $($File.FullName) to $($NewName) (File already exists)."
-                            }
-                        } else {
-                            Rename-Item -Path $File.FullName -NewName $NewName -Force
-                            if ($this.LogActions -and $this.LogFilePath) {
-                                $LogEntries += "Renamed $($file.FullName) to $($NewName)."
-                            }
-                        }
-                    }
-                }
-                if ($this.CSVFormat) {
-                    $File | Export-Csv -Path $this.OutFile -NoTypeInformation -Append:$this.AppendToOutput -Force
-                } else {
-                    $File.Name | Out-File -FilePath $this.OutFile -Append:$this.AppendToOutput -Force
-                }
-            }
-            if ($this.LogActions -and $this.LogFilePath -and $LogEntries.Count -gt 0) {
-                $LogEntries | Out-File -FilePath $this.LogFilePath -Append -Force
-            }
-        } catch {
-            Write-Error -Message $_
-        }
-    }
-}
-
 Function Rename-AndExportFiles {
     <#
     .SYNOPSIS
@@ -101,7 +33,7 @@ Function Rename-AndExportFiles {
     Rename-AndExportFiles -FilesPath "C:\Windows" -OutFile "C:\Temp\results.csv" -CSVFormat -IncludeSubdirectories -LogActions -LogFilePath "C:\Temp\rename_log.txt"
 
     .NOTES
-    Version: 0.3.2
+    Version: 0.3.3
     #>
     [CmdletBinding()]
     param(
@@ -139,21 +71,47 @@ Function Rename-AndExportFiles {
         [string]$LogFilePath
     )
     try {
-        $FileProcessor = [FileOperations]::new(
-            $FilesPath,
-            $FileExtension,
-            $OutFile
-        )
-        $FileProcessor.RenameStringIn = $RenameStringIn
-        $FileProcessor.RenameStringOut = $RenameStringOut
-        $FileProcessor.CSVFormat = $CSVFormat
-        $FileProcessor.IncludeSubdirectories = $IncludeSubdirectories
-        $FileProcessor.AppendToOutput = $AppendToOutput
-        $FileProcessor.SkipExistingFiles = $SkipExistingFiles
-        $FileProcessor.LogActions = $LogActions
-        $FileProcessor.LogFilePath = $LogFilePath
-        $FileProcessor.ProcessFiles()
-    } catch {
+        if (!(Test-Path -Path $FilesPath -PathType Container)) {
+            throw "The specified folder does not exist!"
+        }
+        $Filter = "*.*"
+        if ($FileExtension) {
+            $Filter = "*.$FileExtension"
+        }
+        $Files = Get-ChildItem -Path $FilesPath -File -Recurse:$IncludeSubdirectories -Filter $Filter
+        if ($Files.Count -eq 0) {
+            throw "No files found with the specified extension!"
+        }
+        $LogEntries = @()
+        foreach ($File in $Files) {
+            if ($RenameStringIn) {
+                $NewName = $File.Name -replace $RenameStringIn, $RenameStringOut
+                if ($NewName -ne $File.Name) {
+                    if (($SkipExistingFiles) -and (Test-Path (Join-Path $File.DirectoryName $NewName))) {
+                        if ($LogActions -and $LogFilePath) {
+                            $LogEntries += "Skipped renaming $($File.FullName) to $($NewName) (File already exists)."
+                        }
+                    }
+                    else {
+                        Rename-Item -Path $File.FullName -NewName $NewName -Force -Verbose
+                        if ($LogActions -and $LogFilePath) {
+                            $LogEntries += "Renamed $($file.FullName) to $($NewName)."
+                        }
+                    }
+                }
+            }
+            if ($CSVFormat) {
+                $File | Export-Csv -Path $OutFile -NoTypeInformation -Append:$AppendToOutput -Force
+            }
+            else {
+                $File.Name | Out-File -FilePath $OutFile -Append:$AppendToOutput -Force
+            }
+        }
+        if ($LogActions -and $LogFilePath -and $LogEntries.Count -gt 0) {
+            $LogEntries | Out-File -FilePath $LogFilePath -Append -Force
+        }
+    }
+    catch {
         Write-Error -Message $_
     }
 }
