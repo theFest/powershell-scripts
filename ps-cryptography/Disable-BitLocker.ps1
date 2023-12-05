d5983a6c185fe5f58978fcba2097f02b3b27e055
Fest White <8981383+theFest@users.noreply.github.com>
2023-12-05 08:02:42 +0100

    Init Disable-BitLocker
    See SYNOPSIS/DESCRIPTION

--- ps-cryptography/Disable-BitLocker.ps1
+++ ps-cryptography/Disable-BitLocker.ps1
@@ -4,7 +4,7 @@ Function Disable-BitLocker {
     Disables BitLocker on the specified drive.
     
     .DESCRIPTION
-    This function disables BitLocker encryption on the specified drive, it checks the current status of BitLocker on the drive and if it's enabled, it proceeds to disable BitLocker.
+    This function disables BitLocker encryption on the specified drive. It checks the current status of BitLocker on the drive, verifies TPM status, and if BitLocker is enabled, it proceeds to disable BitLocker.
 
     .PARAMETER MountPoint
     Mandatory - the drive letter or mount point for which BitLocker will be disabled.
@@ -13,12 +13,22 @@ Function Disable-BitLocker {
     Disable-BitLocker -MountPoint "C:"
 
     .NOTES
+    v0.0.2
     #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
         [string]$MountPoint
     )
+    $TPMStatus = Get-WmiObject -Namespace "Root\CIMv2\Security\MicrosoftTpm" -Class Win32_Tpm
+    if (!$TPMStatus) {
+        Write-Warning -Message "TPM is not available on this system. BitLocker cannot be disabled as it cannot be enabled!"
+        return
+    }
+    if ($TPMStatus.IsActivated -eq $false -or $TPMStatus.IsEnabled -eq $false) {
+        Write-Warning -Message "TPM is either not activated or not enabled. BitLocker cannot be disabled, not supported!"
+        return
+    }
     $DriveInfo = Get-BitLockerVolume -MountPoint $MountPoint
     if ($DriveInfo.ProtectionStatus -eq "On") {
         Disable-BitLockerVolume -MountPoint $MountPoint -Confirm:$false
