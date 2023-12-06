7e33a4fdd9ce4339bca993fa8130b9e1c0b0c911
Fest White <8981383+theFest@users.noreply.github.com>
2023-12-07 00:07:01 +0100

    Update Expand-GZipFile.ps1
    disk space check, end block calculate

--- ps-misc/Expand-GZipFile.ps1
+++ ps-misc/Expand-GZipFile.ps1
@@ -17,7 +17,7 @@ Function Expand-GZipFile {
     Expand-GZipFile -InputFile "C:\Files\InputFile.gz" -OutputFile "C:\Files\OutputFile.exe"
 
     .NOTES
-    v0.0.1
+    v0.0.2
     #>
     [CmdletBinding()]
     param (
@@ -30,28 +30,50 @@ Function Expand-GZipFile {
         [Parameter(Mandatory = $false)]
         [switch]$Overwrite
     )
-    try {
-        if (-not $Overwrite -and (Test-Path -Path $OutputFile -PathType Leaf)) {
-            throw "Output file already exists. To overwrite, use -Overwrite switch!"
+    BEGIN {
+        $DriveLetter = [System.IO.Path]::GetPathRoot($InputFile)
+        $Drive = [System.IO.DriveInfo]::GetDrives() | Where-Object { $_.Name -eq $DriveLetter }
+        if ($Drive) {
+            $FreeSpaceGB = $Drive.AvailableFreeSpace / 1GB
+            if ($FreeSpaceGB -ge 1) {
+                Write-Host "There is enough free space on disk for expansion" -ForegroundColor Magenta
+            }
+            else {
+                Write-Host "Error: Not enough free space on disk for expansion" -ForegroundColor DarkGray
+                return
+            }
         }
-        Write-Host "Expanding $($InputFile) to $($OutputFile)" -ForegroundColor Cyan
-        $InputStream = New-Object IO.FileStream($InputFile, [IO.FileMode]::Open, [IO.FileAccess]::Read)
-        $OutputStream = New-Object IO.FileStream($OutputFile, [IO.FileMode]::Create, [IO.FileAccess]::Write)
-        $GzipStream = New-Object System.IO.Compression.GZipStream($InputStream, [IO.Compression.CompressionMode]::Decompress)
-        $Buffer = New-Object byte[](4096)
-        $TotalBytes = 0
-        $BytesRead = 0
-        do {
-            $BytesRead = $GzipStream.Read($Buffer, 0, $Buffer.Length)
-            $TotalBytes += $BytesRead
-            $OutputStream.Write($Buffer, 0, $BytesRead)
-        } while ($BytesRead -gt 0)
-        Write-Host "Expanded $($TotalBytes) bytes." -ForegroundColor DarkGreen
+        else {
+            Write-Host "Error: Drive information not found" -ForegroundColor DarkGray
+            return
+        }
+    }
+    PROCESS {
+        try {
+            if (-not $Overwrite -and (Test-Path -Path $OutputFile -PathType Leaf)) {
+                throw "Output file already exists. To overwrite, use -Overwrite switch!"
+            }
+            Write-Host "Expanding $($InputFile) to $($OutputFile)" -ForegroundColor Cyan
+            $InputStream = New-Object IO.FileStream($InputFile, [IO.FileMode]::Open, [IO.FileAccess]::Read)
+            $OutputStream = New-Object IO.FileStream($OutputFile, [IO.FileMode]::Create, [IO.FileAccess]::Write)
+            $GzipStream = New-Object System.IO.Compression.GZipStream($InputStream, [IO.Compression.CompressionMode]::Decompress)
+            $Buffer = New-Object byte[](4096)
+            $TotalBytes = 0
+            $BytesRead = 0
+            do {
+                $BytesRead = $GzipStream.Read($Buffer, 0, $Buffer.Length)
+                $TotalBytes += $BytesRead
+                $OutputStream.Write($Buffer, 0, $BytesRead)
+            } while ($BytesRead -gt 0)
+            Write-Host "Expanded $($TotalBytes) bytes." -ForegroundColor DarkGreen
+        }
+        catch {
+            Write-Error -Message "Error: $_"
+        }
+    }
+    END {
         $GzipStream.Dispose()
         $OutputStream.Dispose()
         $InputStream.Dispose()
-    }
-    catch {
-        Write-Error -Message "Error: $_"
     }
 }
