fce99ad96941c82f33a505ce4cda7eeb49afa212
Fest White <8981383+theFest@users.noreply.github.com>
2023-12-13 02:24:08 +0100

    Update Get-SystemDrivers
    sort and export features

--- ps-drivers/Get-SystemDrivers.ps1
+++ ps-drivers/Get-SystemDrivers.ps1
@@ -7,17 +7,20 @@ Function Get-SystemDrivers {
     This function retrieves information about device drivers installed on the system using Get-CimInstance. It provides options to display all drivers or filter them by a specified manufacturer.
 
     .PARAMETER ShowAll
-    NotMandatory - specifies whether to display all device drivers.
-
+    NotMandatory - whether to display all device drivers.
     .PARAMETER FilterManufacturer
     NotMandatory - specifies the manufacturer to filter device drivers by.
+    .PARAMETER SortBy
+    NotMandatory - the property to sort the drivers by.
+    .PARAMETER ExportToCSV
+    NotMandatory - a path to export the driver information as a CSV file.
     
     .EXAMPLE
     Get-SystemDrivers -ShowAll
-    Get-SystemDrivers -FilterManufacturer "Intel"
+    Get-SystemDrivers -FilterManufacturer "Intel" -ExportToCSV "$env:USERPROFILE\Desktop\DriversInfo.csv"
     
     .NOTES
-    v0.0.1
+    v0.0.2
     #>
     [CmdletBinding()]
     param (
@@ -25,18 +28,31 @@ Function Get-SystemDrivers {
         [switch]$ShowAll,
 
         [Parameter(Mandatory = $false)]
-        [string]$FilterManufacturer
+        [string]$FilterManufacturer,
+
+        [Parameter(Mandatory = $false)]
+        [string]$SortBy,
+
+        [Parameter(Mandatory = $false)]
+        [string]$ExportToCSV
     )
-    Write-Verbose -Message "Get all device drivers using Get-CimInstance"
     $Drivers = Get-CimInstance -Class Win32_PnPSignedDriver | Select-Object DeviceName, Manufacturer, DriverVersion, Description
-    Write-Verbose -Message "Filter drivers by manufacturer if specified"
     if ($FilterManufacturer) {
+        Write-Verbose -Message "Filtering drivers by manufacturer if specified"
         $Drivers = $Drivers | Where-Object { $_.Manufacturer -like "*$FilterManufacturer*" }
+    }
+    if ($SortBy) {
+        Write-Verbose -Message "Sorting drivers by $SortBy"
+        $Drivers = $Drivers | Sort-Object -Property $SortBy
     }
     if ($ShowAll) {
         $Drivers
     }
     else {
         $Drivers | Select-Object DeviceName, Manufacturer, DriverVersion, Description | Format-Table
+    }
+    if ($ExportToCSV) {
+        Write-Verbose -Message "Exporting driver information to $ExportToCSV"
+        $Drivers | Export-Csv -Path $ExportToCSV -NoTypeInformation
     }
 }
