1e07c222ba8a700501e8d1bb9dfca81a4a435203
theFest <8981383+theFest@users.noreply.github.com>
2024-01-03 04:57:04 +0100

    Update New-WebBrowserWindow
    added providers, params and additional functionalities

--- ps-forms/New-WebBrowserWindow.ps1
+++ ps-forms/New-WebBrowserWindow.ps1
@@ -1,10 +1,10 @@
 Function New-WebBrowserWindow {
     <#
     .SYNOPSIS
-    Creates a new web browser window using System.Windows.Forms.WebBrowser.
+    Creates a new window with an embedded web browser.
 
     .DESCRIPTION
-    This function creates a new window with an embedded web browser control to display web content.
+    This function creates a window containing a web browser. It allows users to specify various parameters such as the title, dimensions, search provider, and more.
 
     .PARAMETER Title
     NotMandatory - title of the browser window, default is "PS Browser Window".
@@ -13,14 +13,20 @@ Function New-WebBrowserWindow {
     .PARAMETER Height
     NotMandatory - specifies the height of the browser window, default is 900 pixels.
     .PARAMETER SearchProvider
-    NotMandatory - search provider to be used for initial navigation, valid values are "Google", "Bing", or "DuckDuckGo". Default is "Google".
+    NotMandatory - search provider to be used for initial navigation, available options are: "Google", "Bing", "DuckDuckGo", "Yahoo", "Ask".
+    .PARAMETER Resizable
+    NotMandatory - specifies whether the browser window is resizable.
+    .PARAMETER FullScreen
+    NotMandatory - specifies whether the browser window should open in full-screen mode.
+    .PARAMETER ClearCache
+    NotMandatory - clear the browser cache before closing the window.
 
     .EXAMPLE
-    New-WebBrowserWindow
-    New-WebBrowserWindow -Width 1600 -Height 1200 -SearchEngine Bing
+    New-WebBrowserWindow -Resizable -ClearCache
+    New-WebBrowserWindow -Width 1600 -Height 1200 -SearchProvider Bing
 
     .NOTES
-    v0.0.2
+    v0.0.3
     #>
     [CmdletBinding()]
     param (
@@ -34,8 +40,17 @@ Function New-WebBrowserWindow {
         [int]$Height = 900,
 
         [Parameter(Mandatory = $false, Position = 3)]
-        [ValidateSet("Google", "Bing", "DuckDuckGo")]
-        [string]$SearchProvider = "Google"
+        [ValidateSet("Google", "Bing", "DuckDuckGo", "Yahoo", "Ask")]
+        [string]$SearchProvider = "Google",
+
+        [Parameter(Mandatory = $false)]
+        [switch]$Resizable,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$FullScreen,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$ClearCache
     )
     BEGIN {
         Add-Type -AssemblyName System.Windows.Forms
@@ -43,6 +58,8 @@ Function New-WebBrowserWindow {
             "Google"     = "https://www.google.com"
             "Bing"       = "https://www.bing.com"
             "DuckDuckGo" = "https://www.duckduckgo.com"
+            "Yahoo"      = "https://www.yahoo.com"
+            "Ask"        = "https://www.ask.com"
         }
     }
     PROCESS {
@@ -52,18 +69,31 @@ Function New-WebBrowserWindow {
         }
         $SearchURL = $SearchProviders[$SearchProvider]
         $Form = New-Object System.Windows.Forms.Form
-        $Form.Text = $Title
-        $Form.Size = New-Object System.Drawing.Size($Width, $Height)
-        $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::Sizable
+        if ($FullScreen) {
+            $Form.WindowState = [System.Windows.Forms.FormWindowState]::Maximized
+        }
+        else {
+            $Form.Size = New-Object System.Drawing.Size($Width, $Height)
+            $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::Sizable
+        }
+        if (-not $Resizable) {
+            $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
+        }
         $Form.AutoScaleMode = [System.Windows.Forms.AutoScaleMode]::Font
+        $Form.Text = $Title
         $WebBrowser = New-Object System.Windows.Forms.WebBrowser
         $WebBrowser.ScriptErrorsSuppressed = $true
         $WebBrowser.Dock = [System.Windows.Forms.DockStyle]::Fill
         $WebBrowser.Navigate($SearchURL)
         $Form.Controls.Add($WebBrowser)
+        $Form.Add_Shown({ $Form.Activate() })
         $Form.ShowDialog()
     }
     END {
+        if ($ClearCache) {
+            $WebBrowser.Navigate("about:blank")
+            $WebBrowser.Document.ExecCommand("ClearAuthenticationCache", $false, $null)
+        }
         if ($null -ne $WebBrowser) {
             $WebBrowser.Dispose()
         }
