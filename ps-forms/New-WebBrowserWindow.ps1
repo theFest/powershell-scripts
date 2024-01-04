3db6218454fa1f8a0c49873ce4e2e10814dd67a2
theFest <8981383+theFest@users.noreply.github.com>
2024-01-05 00:09:23 +0100

    Update New-WebBrowserWindow
    Control panel, etc.

--- ps-forms/New-WebBrowserWindow.ps1
+++ ps-forms/New-WebBrowserWindow.ps1
@@ -4,7 +4,7 @@ Function New-WebBrowserWindow {
     Creates a new window with an embedded web browser.
 
     .DESCRIPTION
-    This function creates a window containing a web browser. It allows users to specify various parameters such as the title, dimensions, search provider, and more.
+    This function creates a window with web browser allowing navigation to different search engines and functionalities like title, dimensions, search provider, browsing history, refresh, new tabs and more.
 
     .PARAMETER Title
     NotMandatory - title of the browser window, default is "PS Browser Window".
@@ -26,7 +26,7 @@ Function New-WebBrowserWindow {
     New-WebBrowserWindow -Width 1600 -Height 1200 -SearchProvider Bing
 
     .NOTES
-    v0.0.3
+    v0.0.4
     #>
     [CmdletBinding()]
     param (
@@ -54,6 +54,7 @@ Function New-WebBrowserWindow {
     )
     BEGIN {
         Add-Type -AssemblyName System.Windows.Forms
+        Add-Type -AssemblyName System.Drawing
         $SearchProviders = @{
             "Google"     = "https://www.google.com"
             "Bing"       = "https://www.bing.com"
@@ -61,6 +62,7 @@ Function New-WebBrowserWindow {
             "Yahoo"      = "https://www.yahoo.com"
             "Ask"        = "https://www.ask.com"
         }
+        $Tabs = @()
     }
     PROCESS {
         if (-not $SearchProviders.ContainsKey($SearchProvider)) {
@@ -69,33 +71,86 @@ Function New-WebBrowserWindow {
         }
         $SearchURL = $SearchProviders[$SearchProvider]
         $Form = New-Object System.Windows.Forms.Form
-        if ($FullScreen) {
-            $Form.WindowState = [System.Windows.Forms.FormWindowState]::Maximized
-        }
-        else {
-            $Form.Size = New-Object System.Drawing.Size($Width, $Height)
-            $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::Sizable
-        }
-        if (-not $Resizable) {
-            $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
-        }
-        $Form.AutoScaleMode = [System.Windows.Forms.AutoScaleMode]::Font
         $Form.Text = $Title
+        $Form.Width = $Width
+        $Form.Height = $Height
+        $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::Sizable
+        $Form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+        $Form.BackColor = [System.Drawing.Color]::FromArgb(240, 240, 240)
+        $TabControl = New-Object System.Windows.Forms.TabControl
+        $TabControl.Dock = [System.Windows.Forms.DockStyle]::Fill
+        $Form.Controls.Add($TabControl)
+        $AddressBar = New-Object System.Windows.Forms.TextBox
+        $AddressBar.Dock = [System.Windows.Forms.DockStyle]::Top
+        $AddressBar.Text = $SearchURL
+        $Form.Controls.Add($AddressBar)
+        $ControlButtonsPanel = New-Object System.Windows.Forms.FlowLayoutPanel
+        $ControlButtonsPanel.Dock = [System.Windows.Forms.DockStyle]::Top
+        $ControlButtonsPanel.FlowDirection = [System.Windows.Forms.FlowDirection]::LeftToRight
+        $ControlButtonsPanel.BackColor = [System.Drawing.Color]::FromArgb(200, 200, 200)
+        $ControlButtonsPanel.Height = 30
+        $Buttons = @("Back", "Forward", "Refresh")
+        foreach ($BtnText in $Buttons) {
+            $Button = New-Object System.Windows.Forms.Button
+            $Button.Text = $BtnText
+            $Button.ForeColor = [System.Drawing.Color]::Black
+            $Button.BackColor = [System.Drawing.Color]::FromArgb(220, 220, 220)
+            $Button.Add_Click({
+                    $CurrentTab = $TabControl.SelectedIndex
+                    $WebBrowser = $Tabs[$CurrentTab]
+                    switch ($BtnText) {
+                        "Back" { if ($WebBrowser.CanGoBack) { $WebBrowser.GoBack() } }
+                        "Forward" { if ($WebBrowser.CanGoForward) { $WebBrowser.GoForward() } }
+                        "Refresh" { $WebBrowser.Refresh() }
+                    }
+                })
+            $ControlButtonsPanel.Controls.Add($Button)
+        }
+        $NewTabButton = New-Object System.Windows.Forms.Button
+        $NewTabButton.Text = "New Tab"
+        $NewTabButton.ForeColor = [System.Drawing.Color]::Black
+        $NewTabButton.BackColor = [System.Drawing.Color]::FromArgb(220, 220, 220)
+        $NewTabButton.Add_Click({
+                $NewWebBrowser = New-Object System.Windows.Forms.WebBrowser
+                $NewWebBrowser.ScriptErrorsSuppressed = $true
+                $NewWebBrowser.Dock = [System.Windows.Forms.DockStyle]::Fill
+                $NewWebBrowser.Navigate($SearchURL)
+                $NewTabPage = New-Object System.Windows.Forms.TabPage
+                $NewTabPage.Text = "New Tab"
+                $NewTabPage.Controls.Add($NewWebBrowser)
+                $TabControl.TabPages.Add($NewTabPage)
+                $Tabs += $NewWebBrowser
+            })
+        $ControlButtonsPanel.Controls.Add($NewTabButton)
+        $ToggleButtonsVisibilityButton = New-Object System.Windows.Forms.Button
+        $ToggleButtonsVisibilityButton.Text = "Show/Hide Controls"
+        $ToggleButtonsVisibilityButton.Dock = [System.Windows.Forms.DockStyle]::Top
+        $ToggleButtonsVisibilityButton.Add_Click({
+                $ControlButtonsPanel.Visible = -not $ControlButtonsPanel.Visible
+            })
+        $Form.Controls.Add($ToggleButtonsVisibilityButton)
+        $Form.Controls.Add($ControlButtonsPanel)
         $WebBrowser = New-Object System.Windows.Forms.WebBrowser
         $WebBrowser.ScriptErrorsSuppressed = $true
         $WebBrowser.Dock = [System.Windows.Forms.DockStyle]::Fill
         $WebBrowser.Navigate($SearchURL)
-        $Form.Controls.Add($WebBrowser)
+        $TabPage = New-Object System.Windows.Forms.TabPage
+        $TabPage.Text = "New Tab"
+        $TabPage.Controls.Add($WebBrowser)
+        $TabControl.TabPages.Add($TabPage)
+        $Tabs += $WebBrowser
         $Form.Add_Shown({ $Form.Activate() })
         $Form.ShowDialog()
     }
     END {
         if ($ClearCache) {
-            $WebBrowser.Navigate("about:blank")
-            $WebBrowser.Document.ExecCommand("ClearAuthenticationCache", $false, $null)
+            foreach ($Tab in $Tabs) {
+                $Tab.Navigate("about:blank")
+                $Tab.Document.ExecCommand("ClearAuthenticationCache", $false, $null)
+            }
         }
-        if ($null -ne $WebBrowser) {
-            $WebBrowser.Dispose()
+        foreach ($Tab in $Tabs) {
+            $Tab.Dispose()
         }
         if ($null -ne $Form) {
             $Form.Dispose()
