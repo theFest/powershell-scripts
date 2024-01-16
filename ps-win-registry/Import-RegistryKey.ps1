45d4023e0d0e4383720b5a922723621923ea9406
theFest <8981383+theFest@users.noreply.github.com>
2024-01-16 10:46:46 +0100

    Update Import-RegistryKey
    aliases and small changes

--- ps-win-registry/Import-RegistryKey.ps1
+++ ps-win-registry/Import-RegistryKey.ps1
@@ -8,38 +8,46 @@ Function Import-RegistryKey {
     It establishes a remote PowerShell session if the ComputerName parameter is provided, copies the registry file to the remote machine, imports the registry key using the 'reg.exe' command, and removes the copied file afterwards. If no ComputerName is specified, the function imports the registry key locally.
     
     .PARAMETER ImportPath
-    Mandatory - path to the registry file that needs to be imported. This parameter is mandatory.
+    Mandatory - path to the registry file that needs to be imported.
     .PARAMETER ComputerName
-    NotMandatory - name of the remote computer from which to import the registry key. This parameter is not mandatory. If provided, a remote session will be established.
-    .PARAMETER Username
-    NotMandatory - username for authentication when connecting to a remote computer. This parameter is not mandatory and is only required when the ComputerName parameter is provided.
-    .PARAMETER Password
-    NotMandatory - password for authentication when connecting to a remote computer. This parameter is not mandatory and is only required when the ComputerName parameter is provided.
+    NotMandatory - name of the remote computer from which to import the registry key, if provided, a remote session will be established.
+    .PARAMETER User
+    NotMandatory - username for authentication when connecting to a remote computer, only required when the ComputerName parameter is provided.
+    .PARAMETER Pass
+    NotMandatory - password for authentication when connecting to a remote computer, only required when the ComputerName parameter is provided.
     
     .EXAMPLE
-    Import-RegistryKey -ImportPath "$env:USERPROFILE\Desktop\import.reg" -ComputerName "remote_hostname" -Username "remote_user" -Password "remote_pass" -Verbose
+    Import-RegistryKey -ImportPath "$env:USERPROFILE\Desktop\import.reg" -ComputerName "remote_host" -User "remote_user" -Pass "remote_pass" -Verbose
     
     .NOTES
-    v0.0.3
+    v0.0.4
     #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
+        [Alias("i")]
         [ValidateNotNullOrEmpty()]
         [string]$ImportPath,
 
         [Parameter(Mandatory = $false)]
+        [Alias("r")]
+        [string]$RemoteImportPath = "C:\Users\Default\AppData\Local\Temp\import.reg",
+
+        [Parameter(Mandatory = $false)]
+        [Alias("c")]
         [string]$ComputerName,
 
         [Parameter(Mandatory = $false)]
-        [string]$Username,
+        [Alias("u")]
+        [string]$User,
         
         [Parameter(Mandatory = $false)]
-        [string]$Password
+        [Alias("p")]
+        [string]$Pass
     )
     try {
         if ($ComputerName) {
-            $Session = New-PSSession -ComputerName $ComputerName -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, (ConvertTo-SecureString -String $Password -AsPlainText -Force))
+            $Session = New-PSSession -ComputerName $ComputerName -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (ConvertTo-SecureString -String $Pass -AsPlainText -Force))
             $TestConnectionScriptBlock = {
                 (Test-Connection -ComputerName $Using:ComputerName -Count 1 -Quiet) -and (Test-WSMan -ComputerName $Using:ComputerName -Authentication Default)
             }
@@ -49,12 +57,11 @@ Function Import-RegistryKey {
                     param($Path)
                     Start-Process -FilePath 'reg.exe' -ArgumentList 'import', $Path -Wait -NoNewWindow
                 }
-                $RemoteImportPath = "C:\Users\Default\AppData\Local\Temp\import.reg"
                 Copy-Item -Path $ImportPath -Destination $RemoteImportPath -ToSession $Session -Force -Verbose
                 Invoke-Command -Session $Session -ScriptBlock $ImportScriptBlock -ArgumentList $RemoteImportPath
                 Invoke-Command -Session $Session -ScriptBlock {
                     param($Path)
-                    Remove-Item $Path -Force
+                    Remove-Item $Path -Force -Verbose
                 } -ArgumentList $RemoteImportPath
             }
             else {
