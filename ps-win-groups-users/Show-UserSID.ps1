0ca054b755d69cf8f9e43653d3710cd94102177e
theFest <8981383+theFest@users.noreply.github.com>
2024-01-17 07:47:46 +0100

    Update Show-UserSID
    multiple users support

--- ps-win-groups-users/Show-UserSID.ps1
+++ ps-win-groups-users/Show-UserSID.ps1
@@ -11,45 +11,46 @@ Function Show-UserSID {
 
     .EXAMPLE
     "your_user" | Show-UserSID
-    Show-UserSID -UserName "your_user" -OutputFormat List
+    Show-UserSID -UserName "user1", "user2" -OutputFormat List
 
     .NOTES
-    v0.0.3
+    v0.0.4
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, HelpMessage = "Specify the user name")]
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, HelpMessage = "Specify the user names")]
         [ValidateNotNullOrEmpty()]
         [Alias("u")]
-        [string]$UserName,
+        [string[]]$UserName,
 
-        [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Specify the output format. Options: 'Table' (default), 'List', 'JSON'")]
+        [Parameter(Position = 1, HelpMessage = "Specify the output format. Options: 'Table' (default), 'List', 'JSON'")]
         [ValidateSet("Table", "List", "JSON")]
         [string]$OutputFormat = "Table"
     )
     PROCESS {
-        try {
-            $NTAccount = New-Object System.Security.Principal.NTAccount($UserName)
-            $UserSID = $NTAccount.Translate([System.Security.Principal.SecurityIdentifier]).Value
-            $Result = [PSCustomObject]@{
-                UserName = $UserName
-                SID      = $UserSID
+        foreach ($User in $UserName) {
+            try {
+                $NTAccount = New-Object System.Security.Principal.NTAccount($User)
+                $UserSID = $NTAccount.Translate([System.Security.Principal.SecurityIdentifier]).Value
+                $Result = [PSCustomObject]@{
+                    UserName = $User
+                    SID      = $UserSID
+                }
+                switch ($OutputFormat) {
+                    "List" {
+                        Write-Output $Result.PSObject.Properties | ForEach-Object { "$($_.Name): $($_.Value)" }
+                    }
+                    "JSON" {
+                        $Result | ConvertTo-Json
+                    }
+                    default {
+                        Write-Output $Result | Format-Table -AutoSize
+                    }
+                }
             }
-            switch ($OutputFormat) {
-                "List" {
-                    Write-Output $Result.PSObject.Properties | ForEach-Object { "$($_.Name): $($_.Value)" }
-                }
-                "JSON" {
-                    $Result | ConvertTo-Json
-                }
-                default {
-                    Write-Output $Result | Format-Table -AutoSize
-                }
+            catch {
+                Write-Error -Message "Failed to get SID for user '$User'. $_"
             }
-        }
-        catch {
-            Write-Error -Message "Failed to get SID for user '$UserName'. $_"
-            return $null
         }
     }
 }
