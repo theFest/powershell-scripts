3a451a64273a201a139fd4884556f0fbb80e70aa
theFest <8981383+theFest@users.noreply.github.com>
2024-01-17 08:18:30 +0100

    Update Show-UserSID
    domain and log to file features, other features and enhancements

--- ps-win-groups-users/Show-UserSID.ps1
+++ ps-win-groups-users/Show-UserSID.ps1
@@ -1,20 +1,28 @@
 Function Show-UserSID {
     <#
     .SYNOPSIS
-    Gets the Security Identifier (SID) for a specified user.
+    Gets the Security Identifier (SID) for specified users.
 
     .DESCRIPTION
-    Retrieves the Security Identifier (SID) for a user by translating the account name.
+    Retrieves the Security Identifier (SID) for users by translating their account names.
 
     .PARAMETER UserName
-    Specifies the user name for which to retrieve the SID.
+    Specifies the user names for which to retrieve the SID, accepts an array of user names.
+    .PARAMETER OutputFormat
+    Specifies the format for displaying the output, options: 'Table' (default), 'List', 'JSON'.
+    .PARAMETER Domain
+    Domain to filter users, if specified, only users from this domain will be processed.
+    .PARAMETER LogToFile
+    File path to log verbose information.
+    .PARAMETER IncludeFullName
+    Indicates whether to include the user's full name in the output.
 
     .EXAMPLE
-    "your_user" | Show-UserSID
-    Show-UserSID -UserName "user1", "user2" -OutputFormat List
+    "user" | Get-UserSID
+    Get-UserSID -UserName "user1", "user2" -OutputFormat JSON
 
     .NOTES
-    v0.0.4
+    v0.0.5
     #>
     [CmdletBinding()]
     param (
@@ -23,18 +31,39 @@ Function Show-UserSID {
         [Alias("u")]
         [string[]]$UserName,
 
-        [Parameter(Position = 1, HelpMessage = "Specify the output format. Options: 'Table' (default), 'List', 'JSON'")]
+        [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Specify the output format")]
         [ValidateSet("Table", "List", "JSON")]
-        [string]$OutputFormat = "Table"
+        [Alias("o")]
+        [string]$OutputFormat = "Table",
+
+        [Parameter(Mandatory = $false, Position = 2, HelpMessage = "Specify the domain to filter users")]
+        [Alias("d")]
+        [string]$Domain,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Specify a file path to log verbose information.")]
+        [Alias("l")]
+        [string]$LogToFile,
+
+        [Parameter(Mandatory = $false, HelpMessage = "User's full name in output")]
+        [Alias("if")]
+        [switch]$IncludeFullName
     )
     PROCESS {
         foreach ($User in $UserName) {
             try {
+                if ($Domain -and $User -notmatch "@$Domain") {
+                    Write-Warning -Message "Skipping user '$User' as it does not belong to the specified domain '$Domain'"
+                    continue
+                }
                 $NTAccount = New-Object System.Security.Principal.NTAccount($User)
                 $UserSID = $NTAccount.Translate([System.Security.Principal.SecurityIdentifier]).Value
+                $FullName = if ($IncludeFullName) {
+                    $NTAccount.Translate([System.Security.Principal.NTAccount]).Value
+                }
                 $Result = [PSCustomObject]@{
                     UserName = $User
                     SID      = $UserSID
+                    FullName = if ($FullName) { $FullName } else { 'n/a' }
                 }
                 switch ($OutputFormat) {
                     "List" {
@@ -46,6 +75,9 @@ Function Show-UserSID {
                     default {
                         Write-Output $Result | Format-Table -AutoSize
                     }
+                }
+                if ($LogToFile) {
+                    Add-Content -Path $LogToFile -Value "$($Result.UserName): $($Result.SID) $($Result.FullName)"
                 }
             }
             catch {
