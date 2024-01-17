e7e9a4745067ec0a782cf78e6d2879113b2a3cde
theFest <8981383+theFest@users.noreply.github.com>
2024-01-17 07:34:30 +0100

    Update Show-UserSID
    pipeline, custom object, process block, etc.

--- ps-win-groups-users/Show-UserSID.ps1
+++ ps-win-groups-users/Show-UserSID.ps1
@@ -4,30 +4,37 @@ Function Show-UserSID {
     Gets the Security Identifier (SID) for a specified user.
 
     .DESCRIPTION
-    This function retrieves the Security Identifier (SID) for a given user by translating the user's account name.
-    It utilizes the New-Object cmdlet to create an NTAccount object and then translates it to a SecurityIdentifier object to obtain the SID.
+    Retrieves the Security Identifier (SID) for a user by translating the account name.
 
     .PARAMETER UserName
-    Specifies the name of the user for whom to retrieve the SID.
+    Specifies the user name for which to retrieve the SID.
 
     .EXAMPLE
     Show-UserSID -UserName "your_user"
 
     .NOTES
-    v0.0.1
+    v0.0.2
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory, Position = 0, HelpMessage = "Specify the user name")]
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline = $true, HelpMessage = "Specify the user name")]
         [ValidateNotNullOrEmpty()]
+        [Alias("u")]
         [string]$UserName
     )
-    try {
-        $UserSID = (New-Object Security.Principal.NTAccount($UserName)).Translate([Security.Principal.SecurityIdentifier]).Value
-        return $UserSID
-    }
-    catch {
-        Write-Error -Message "Error getting SID for user '$UserName'. $_"
-        return $null
+    process {
+        try {
+            $NTAccount = New-Object System.Security.Principal.NTAccount($UserName)
+            $UserSID = $NTAccount.Translate([System.Security.Principal.SecurityIdentifier]).Value
+            $Result = [PSCustomObject]@{
+                UserName = $UserName
+                SID      = $UserSID
+            }
+            Write-Output -InputObject $Result
+        }
+        catch {
+            Write-Error -Message "Failed to get SID for user '$UserName'. $_"
+            return $null
+        }
     }
 }
