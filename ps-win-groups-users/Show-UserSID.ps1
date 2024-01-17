19437b8c56473973292b09005419e7a030fba45a
theFest <8981383+theFest@users.noreply.github.com>
2024-01-17 07:44:41 +0100

    Update Show-UserSID
    output format feature added

--- ps-win-groups-users/Show-UserSID.ps1
+++ ps-win-groups-users/Show-UserSID.ps1
@@ -10,19 +10,24 @@ Function Show-UserSID {
     Specifies the user name for which to retrieve the SID.
 
     .EXAMPLE
-    Show-UserSID -UserName "your_user"
+    "your_user" | Show-UserSID
+    Show-UserSID -UserName "your_user" -OutputFormat List
 
     .NOTES
-    v0.0.2
+    v0.0.3
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory, Position = 0, ValueFromPipeline = $true, HelpMessage = "Specify the user name")]
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, HelpMessage = "Specify the user name")]
         [ValidateNotNullOrEmpty()]
         [Alias("u")]
-        [string]$UserName
+        [string]$UserName,
+
+        [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Specify the output format. Options: 'Table' (default), 'List', 'JSON'")]
+        [ValidateSet("Table", "List", "JSON")]
+        [string]$OutputFormat = "Table"
     )
-    process {
+    PROCESS {
         try {
             $NTAccount = New-Object System.Security.Principal.NTAccount($UserName)
             $UserSID = $NTAccount.Translate([System.Security.Principal.SecurityIdentifier]).Value
@@ -30,7 +35,17 @@ Function Show-UserSID {
                 UserName = $UserName
                 SID      = $UserSID
             }
-            Write-Output -InputObject $Result
+            switch ($OutputFormat) {
+                "List" {
+                    Write-Output $Result.PSObject.Properties | ForEach-Object { "$($_.Name): $($_.Value)" }
+                }
+                "JSON" {
+                    $Result | ConvertTo-Json
+                }
+                default {
+                    Write-Output $Result | Format-Table -AutoSize
+                }
+            }
         }
         catch {
             Write-Error -Message "Failed to get SID for user '$UserName'. $_"
