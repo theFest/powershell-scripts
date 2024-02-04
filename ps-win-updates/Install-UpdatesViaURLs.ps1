3751c338413e36a47af0c771ae283163e9ce05d8
theFest <8981383+theFest@users.noreply.github.com>
2024-02-05 00:17:32 +0100

    Update Install-UpdatesViaURLs
    params, aliases, blocks, bump, etc.

--- ps-win-updates/Install-UpdatesViaURLs.ps1
+++ ps-win-updates/Install-UpdatesViaURLs.ps1
@@ -1,60 +1,95 @@
 Function Install-UpdatesViaURLs {
     <#
     .SYNOPSIS
-    Installs Windows updates from specified URLs, in this example from Windows Update Catalog.
+    Installs updates from specified URLs, in this example from Windows Update Catalog.
 
     .DESCRIPTION
-    This function downloads and installs Windows updates from a list of update URLs, it checks if an update is already downloaded and skips it if so.
-    After downloading, it uses the Windows Update Standalone Installer (wusa.exe) to install the updates silently. It tracks the number of updates installed and provides status messages.
+    This function installs updates from a list of URLs, it downloads the updates, installs them quietly, and provides information about the installation progress.
+
+    .PARAMETER UpdateUrls
+    Array of URLs pointing to the updates that need to be installed.
+    .PARAMETER DownloadPath
+    Path where the updates will be downloaded. Defaults to the system's temporary directory.
+    .PARAMETER Quiet
+    Indicates whether the installation should be done quietly without displaying prompts or messages.
+    .PARAMETER NoRestart
+    Indicates whether a system restart should be suppressed after installing the updates.
 
     .EXAMPLE
-    Install-UpdatesViaURLs
+    Install-UpdatesViaURLs -UpdateUrls "https://example.com/update1.msu", "https://example.com/update2.exe" -DownloadPath "C:\Temp" -Quiet -NoRestart
 
     .NOTES
-    Version: 0.0.1
+    v0.3.7
+    - Change UpdateUrls as needed
     #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)]
+        [Alias("u")]
         [string[]]$UpdateUrls = @(
             "https://catalog.s.download.windowsupdate.com/your_update_link-1.msu",
             "https://catalog.s.download.windowsupdate.com/your_update_link-2.exe",
             "https://catalog.s.download.windowsupdate.com/your_update_link-3.msu",
             "https://catalog.s.download.windowsupdate.com/your_update_link-4.exe"
             # Add more update URLs as needed
-        )
-    )
-    $TotalUpdates = $UpdateUrls.Count
-    $UpdatesInstalled = 0
-    Write-Host "Installing $TotalUpdates updates..." -ForegroundColor Cyan
-    foreach ($UpdateUrl in $UpdateUrls) {
-        $UpdateFileName = [System.IO.Path]::GetFileName($UpdateUrl)
-        $DownloadPath = Join-Path -Path $env:TEMP -ChildPath $UpdateFileName
+        ),
 
-        if (Test-Path -Path $DownloadPath) {
-            Write-Host "$UpdateFileName is already downloaded. Skipping..." -ForegroundColor Gray
-        }
-        else {
-            Write-Host "Downloading is starting $UpdateFileName..." -ForegroundColor Cyan
-            try {
-                Invoke-WebRequest -Uri $UpdateUrl -OutFile $DownloadPath -ErrorAction Stop
-                Write-Host "Downloaded $UpdateFileName." -ForegroundColor Green
-            }
-            catch {
-                Write-Error "Error downloading $UpdateFileName : $($_.Exception.Message)"
-                continue
-            }
-        }
-        Write-Host "Installing $UpdateFileName..." -ForegroundColor Yellow
-        $Result = Start-Process -FilePath "wusa.exe" -ArgumentList "/quiet /norestart $DownloadPath" -Wait -PassThru
-        if ($Result.ExitCode -eq 0) {
-            Write-Host "Update $UpdateFileName installed successfully." -ForegroundColor Green
-            $UpdatesInstalled++
-        }
-        else {
-            Write-Error "Failed to install $UpdateFileName (Exit Code: $($Result.ExitCode))."
-        }
-        Remove-Item -Path $DownloadPath -Force -Verbose
+        [Parameter(Mandatory = $false)]
+        [Alias("d")]
+        [string]$DownloadPath = $env:TEMP,
+
+        [Parameter(Mandatory = $false)]
+        [Alias("qt")]
+        [switch]$Quiet,
+
+        [Parameter(Mandatory = $false)]
+        [Alias("nr")]
+        [switch]$NoRestart
+    )
+    BEGIN {
+        $TotalUpdates = $UpdateUrls.Count
+        $UpdatesInstalled = 0
+        Write-Host "Installing $TotalUpdates updates..." -ForegroundColor Cyan
     }
-    Write-Host "Installed $UpdatesInstalled out of $TotalUpdates updates." -ForegroundColor Yellow
+    PROCESS {
+        foreach ($UpdateUrl in $UpdateUrls) {
+            $UpdateFileName = [System.IO.Path]::GetFileName($UpdateUrl)
+            $FullDownloadPath = Join-Path -Path $DownloadPath -ChildPath $UpdateFileName
+            if (Test-Path -Path $FullDownloadPath) {
+                Write-Host "$UpdateFileName is already downloaded. Skipping..." -ForegroundColor Gray
+            }
+            else {
+                Write-Host "Downloading is starting $UpdateFileName..." -ForegroundColor Cyan
+                try {
+                    Invoke-WebRequest -Uri $UpdateUrl -OutFile $FullDownloadPath -ErrorAction Stop
+                    Write-Host "Downloaded $UpdateFileName." -ForegroundColor Green
+                }
+                catch {
+                    Write-Error -Message "Error downloading $UpdateFileName : $($_.Exception.Message)"
+                    continue
+                }
+            }
+            Write-Host "Installing $UpdateFileName..." -ForegroundColor Yellow
+            $Arguments = "/quiet", "/norestart"
+            if ($Quiet) { 
+                $Arguments += "/quiet" 
+            }
+            if ($NoRestart) { 
+                $Arguments += "/norestart" 
+            }
+            $Arguments += $FullDownloadPath
+            $Result = Start-Process -FilePath "wusa.exe" -ArgumentList $Arguments -Wait -PassThru
+            if ($Result.ExitCode -eq 0) {
+                Write-Host "Update $UpdateFileName installed successfully." -ForegroundColor Green
+                $UpdatesInstalled++
+            }
+            else {
+                Write-Error -Message "Failed to install $UpdateFileName (Exit Code: $($Result.ExitCode))!"
+            }
+            Remove-Item -Path $FullDownloadPath -Force -Verbose
+        }
+    }
+    END {
+        Write-Host "Installed $UpdatesInstalled out of $TotalUpdates updates." -ForegroundColor Yellow
+    }
 }
