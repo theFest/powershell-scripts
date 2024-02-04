808067ac5a6170fe421738dab6716bcca8941880
theFest <8981383+theFest@users.noreply.github.com>
2024-02-04 18:22:02 +0100

    Update Disable-WindowsUpdate
    help messages, positions, bump, etc.

--- ps-win-updates/Disable-WindowsUpdate.ps1
+++ ps-win-updates/Disable-WindowsUpdate.ps1
@@ -1,67 +1,77 @@
 Function Disable-WindowsUpdate {
     <#
     .SYNOPSIS
-    Disables Windows updates using a third-party tool, credits to Sordum.
+    Disables Windows Update and protects it using the Windows Update disabler (WUB).
 
     .DESCRIPTION
-    This function downloads and runs a third-party tool to disable Windows updates.
+    This function downloads and executes the Windows Update disabler (WUB) to disable Windows Update and protect it from being re-enabled. Credits to Sordum.
 
     .PARAMETER UpdateOperation
-    NotMandatory - update operation to perform, the available options are '/D', '/E', and '/D /P' (default). '/D' disables updates, '/E' enables updates, and '/D /P' disables updates permanently. Default is '/D /P'.
-    .PARAMETER WudUrl
-    NotMandatory - specifies the URL of the Windows Update Disabler tool to download.
+    Operation to perform on Windows Update, accepts "/D" for Disable, "/E" for Enable, and "/D /P" for Disable & Protect.
+    .PARAMETER WubDownloadUrl
+    URL for downloading the Windows Update disabler (WUB), default URL: https://www.sordum.org/files/downloads.php?st-windows-update-blocker.
+    .PARAMETER WubVersion
+    Specifies the version of the Windows Update disabler (WUB) to download, default version: 1.8.
+    .PARAMETER DownloadPath
+    Path to the directory where the WUB will be downloaded and extracted, default path: $env:TEMP\WU_Disable.
     .PARAMETER RemoveWUB
-    NotMandatory - whether to remove Windows Update Disable tool after disabling Windows Updates.
+    If present, removes the temporary folder after the operation.
     .PARAMETER Restart
-    NotMandatory - indicates whether to restart the computer after applying the update operation.
+    If present, restarts the computer after the operation.
 
     .EXAMPLE
-    Disable-WindowsUpdate -Verbose
-    Disable-WindowsUpdate -UpdateOperation /E -Restart
+    Disable-WindowsUpdate -UpdateOperation '/D /P' -Restart
 
     .NOTES
-    v0.0.2
+    v0.4.9
     #>
     [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $false, HelpMessage = "'/D'-->disable | '/E'-->enable | '/D /P'-->disable&protect")]
+    param (
+        [Parameter(Mandatory = $false, Position = 0, HelpMessage = "'/D' --> Disable | '/E' --> Enable | '/D /P' --> Disable & Protect")]
         [ValidateSet("/D", "/E", "/D /P")]
         [string]$UpdateOperation = "/D /P",
 
-        [Parameter(Mandatory = $false)]
-        [uri]$WudUrl = "https://www.sordum.org/files/downloads.php?st-windows-update-blocker",
+        [Parameter(Mandatory = $false, Position = 1, HelpMessage = "URL for downloading the Windows Update disabler (WUB)")]
+        [uri]$WubDownloadUrl = "https://www.sordum.org/files/downloads.php?st-windows-update-blocker",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, Position = 2, HelpMessage = "Version of the Windows Update disabler (WUB) to download")]
+        [string]$WubVersion = "1.8",
+
+        [Parameter(Mandatory = $false, Position = 3, HelpMessage = "Path to the directory where the WUB will be downloaded and extracted")]
+        [string]$DownloadPath = "$env:TEMP\WU_Disable",
+
+        [Parameter(Mandatory = $false, HelpMessage = "Remove the temporary folder after the operation")]
         [switch]$RemoveWUB,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, HelpMessage = "Restart the computer after the operation")]
         [switch]$Restart
     )
     try {
-        New-Item -Path "$env:TEMP" -Name "WU_Disable" -ItemType Directory -Force | Out-Null
-        if (!(Test-Path -Path "$env:TEMP\WU_Disable\Wub_v1.8.zip")) {
+        Write-Host "Creating download directory..." -ForegroundColor Green
+        New-Item -Path $DownloadPath -ItemType Directory -Force | Out-Null
+        $WubZipPath = Join-Path $DownloadPath "Wub_v$WubVersion.zip"
+        if (!(Test-Path -Path $WubZipPath)) {
             Write-Host "Downloading Windows Update disabler..." -ForegroundColor Green
-            Invoke-WebRequest -Uri $WudUrl -OutFile "$env:TEMP\WU_Disable\Wub_v1.8.zip" -UseBasicParsing -Verbose
+            Invoke-WebRequest -Uri $WubDownloadUrl -OutFile $WubZipPath -UseBasicParsing -Verbose
         }
-        Expand-Archive -Path "$env:TEMP\WU_Disable\Wub_v1.8.zip" -DestinationPath "$env:TEMP\WU_Disable" -Force -Verbose
+        $WubExtractPath = Join-Path $DownloadPath "Wub"
+        Write-Host "Extracting Windows Update disabler..." -ForegroundColor Green
+        Expand-Archive -Path $WubZipPath -DestinationPath $WubExtractPath -Force -Verbose
         Clear-Host
-        Write-Verbose -Message "Downloaded and expanded, starting Wub to disable updates..."
-        Start-Process cmd -ArgumentList "/c $env:TEMP\WU_Disable\Wub\Wub_x64.exe $UpdateOperation" -WindowStyle Minimized -Wait
+        Write-Verbose -Message "Downloaded and expanded, starting Wub to $UpdateOperation updates..."
+        Start-Process cmd -ArgumentList "/c $($WubExtractPath)\Wub_x64.exe $UpdateOperation" -WindowStyle Minimized -Wait
     }
     catch {
         Write-Error -Message "An error occurred: $_"
     }
     finally {
-        if (("wuauserv", "WaaSMedicSvc" | Get-Service).Status -eq "Stopped") {
-            Write-Host "Windows Update services disabled" -ForegroundColor Cyan
-        }
-        else {
-            Write-Host "Windows Update services enabled" -ForegroundColor Cyan
-        }
+        $servicesStatus = Get-Service -Name 'wuauserv', 'WaaSMedicSvc' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Status
+        $statusMessage = if ($servicesStatus -eq 'Stopped') { "disabled" } else { "enabled" }
+        Write-Host "Windows Update services $statusMessage" -ForegroundColor Cyan
     }
     if ($RemoveWUB) {
-        Write-Warning -Message "Cleaning up, remove the temporary folder..."
-        Remove-Item -Path "$env:TEMP\WU_Disable" -Force -Recurse -Verbose
+        Write-Warning -Message "Cleaning up, removing the temporary folder..."
+        Remove-Item -Path $DownloadPath -Force -Recurse -Verbose
     }
     if ($Restart) {
         Write-Warning -Message "Restarting computer..."
