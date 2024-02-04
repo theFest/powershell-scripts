dab2a9341500cada9a0aa39d4ac654a4e27bf259
theFest <8981383+theFest@users.noreply.github.com>
2024-02-04 18:32:00 +0100

    Update Get-WindowsUpdates
    positions, align help info, bump, etc.

--- ps-win-updates/Get-WindowsUpdates.ps1
+++ ps-win-updates/Get-WindowsUpdates.ps1
@@ -1,67 +1,66 @@
 Function Get-WindowsUpdates {
     <#
     .SYNOPSIS
-    Retrieve information about available Windows updates on a remote computer.
-    
+    Retrieves Windows updates information from a local or remote machine.
+
     .DESCRIPTION
-    This function connects to a remote computer and retrieves information about available Windows updates, it provides various options to filter and control the number of updates returned.
-    
+    This function queries Windows updates on a local or remote machine, providing information about available updates based on specified criteria.
+
     .PARAMETER ComputerName
-    NotMandatory - name of the remote computer where updates will be checked.
-    .PARAMETER Username
-    NotMandatory - username used to authenticate to the remote computer.
+    Specifies the remote computer name, default is the local machine.
+    .PARAMETER User
+    Provides the username for remote authentication.
     .PARAMETER Pass
-    NotMandatory - password for the provided username used to authenticate to the remote computer.
+    Provides the password for the specified username.
     .PARAMETER MaxUpdates
-    NotMandatory - maximum number of updates to be returned. Default is 100.
+    Maximum number of updates to be returned, default is 100.
     .PARAMETER IncludeHidden
-    NotMandatory - if specified, hidden updates will be included in the results.
+    Include hidden updates in the results.
     .PARAMETER IncludeInstalled
-    NotMandatory - if specified, installed updates will also be included in the results.
+    Include installed updates in the results.
     .PARAMETER IncludeRebootRequired
-    NotMandatory - if specified, updates requiring a system reboot will also be included in the results.
+    Include updates requiring a system reboot in the results.
     .PARAMETER OutputFile
-    NotMandatory - if specified, the updates information will be saved to the specified file.
-    
+    File path where the updates information will be saved.
+
     .EXAMPLE
-    Get-WindowsUpdates -OutputFile "$env:USERPROFILE\Desktop\Updates.csv" -Verbose
-    Get-WindowsUpdates -ComputerName "remote_host" -Username "remote_user" -Pass "remote_pass" -OutputFile "$env:USERPROFILE\Desktop\Updates.csv" -Verbose
-    
+    Get-WindowsUpdates -ComputerName "remote_host" -User "remote_user" -Pass "remote_pass" -MaxUpdates 50 -IncludeHidden -IncludeInstalled -OutputFile "C:\UpdatesInfo.txt"
+
     .NOTES
-    v0.0.2
+    v0.3.1
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false, HelpMessage = "Specify the remote computer name")]
+        [Parameter(Position = 0, HelpMessage = "Specifies the remote computer name. Default is the local machine")]
         [string]$ComputerName = $env:COMPUTERNAME,
-
-        [Parameter(Mandatory = $false, HelpMessage = "Provide the username for remote authentication")]
-        [string]$Username,
-
-        [Parameter(Mandatory = $false, HelpMessage = "Provide the password for the specified username")]
+    
+        [Parameter(Position = 1, HelpMessage = "Provides the username for remote authentication")]
+        [string]$User,
+    
+        [Parameter(Position = 2, HelpMessage = "Provides the password for the specified username")]
         [string]$Pass,
-
-        [Parameter(Mandatory = $false, HelpMessage = "Specify the maximum number of updates to be returned")]
+    
+        [Parameter(Position = 3, HelpMessage = "Specifies the maximum number of updates to be returned")]
         [int]$MaxUpdates = 100,
-
-        [Parameter(Mandatory = $false, HelpMessage = "Include hidden updates in the results")]
+    
+        [Parameter(Position = 4, HelpMessage = "Switch to include hidden updates in the results")]
         [switch]$IncludeHidden,
-
-        [Parameter(Mandatory = $false, HelpMessage = "Include installed updates in the results")]
+    
+        [Parameter(Position = 5, HelpMessage = "Switch to include installed updates in the results")]
         [switch]$IncludeInstalled,
-
-        [Parameter(Mandatory = $false, HelpMessage = "Include updates requiring a system reboot in the results")]
+    
+        [Parameter(Position = 6, HelpMessage = "Switch to include updates requiring a system reboot in the results")]
         [switch]$IncludeRebootRequired,
-
-        [Parameter(Mandatory = $false, HelpMessage = "Specify the file path where the updates information will be saved")]
+    
+        [Parameter(Position = 7, HelpMessage = "Specifies the file path where the updates information will be saved")]
         [string]$OutputFile
     )
     try {
         $UsingCred = $null
-        if ($ComputerName -ne $env:COMPUTERNAME -and $Username -and $Pass) {
+        if ($ComputerName -ne $env:COMPUTERNAME -and $User -and $Pass) {
             Write-Verbose -Message "Creating PSCredential object..."
             $SecurePassword = ConvertTo-SecureString -String $Pass -AsPlainText -Force
-            $UsingCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, $SecurePassword
+            $UsingCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $SecurePassword
         }
         $ScriptBlock = {
             param (
@@ -92,7 +91,7 @@ Function Get-WindowsUpdates {
             $Updates = Invoke-Command -Session $Session -ScriptBlock $ScriptBlock -ArgumentList $IncludeHidden, $IncludeInstalled, $IncludeRebootRequired, $MaxUpdates
             if ($Session) {
                 Write-Verbose -Message "Removing the remote session with $ComputerName..."
-                Remove-PSSession -Session $Session -ErrorAction SilentlyContinue
+                Remove-PSSession -Session $Session -ErrorAction SilentlyContinue -Verbose
             }
         }
         else {
