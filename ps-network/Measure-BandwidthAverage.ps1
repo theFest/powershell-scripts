f95a3ddaba0dbb23918cb862b532f7af02bef6cd
theFest <8981383+theFest@users.noreply.github.com>
2024-02-10 02:07:00 +0100

    Update Measure-BandwidthAverage
    rename, align help info, aliases, etc.

--- ps-network/Measure-BandwidthAverage.ps1
+++ ps-network/Measure-BandwidthAverage.ps1
@@ -1,44 +1,42 @@
-Function MeasureBandwidthAverage {
+Function Measure-BandwidthAverage {
     <#
     .SYNOPSIS
-    Function that measures the average bandwidth utilization of a network interface over a specified duration and saves the results in a CSV file.
+    Measures the average bandwidth utilization of network interfaces over a specified duration.
     
     .DESCRIPTION
-    This function calculates the average bandwidth utilization of a network interface over a specified duration. It uses the Win32_PerfFormattedData_Tcpip_NetworkInterface WMI class to obtain network interface information.
-    The function takes three optional parameters: DurationMinutes, InterfaceName, and OutputCsvFile. If InterfaceName is provided, it measures the bandwidth utilization of the specified network interface; otherwise, it measures the utilization of all active interfaces.
-    Results are displayed on the console and saved to a CSV file if the OutputCsvFile parameter is specified.
+    This function calculates the percentage of bandwidth utilized by network interfaces over a specified duration, collects data on network traffic, calculates the bandwidth utilization percentage, and provides average statistics.
     
     .PARAMETER DurationMinutes
-    NotMandatory - specifies the duration in minutes for bandwidth measurement. Default value is 0.1 minutes.
+    Duration, in minutes, for which bandwidth utilization is measured, defaults to 0.1 minutes.
     .PARAMETER InterfaceName
-    NotMandatory - name of the network interface for bandwidth measurement. If not provided, the function measures the utilization of all active interfaces. 
+    Name of the network interface for which bandwidth utilization is measured, if not specified, bandwidth utilization for all interfaces is measured.
     .PARAMETER OutputCsvFile
-    NotMandatory -  output CSV file path to save the bandwidth data. If not provided, the results are not saved to a CSV file. The default path is the desktop of the current user, with the filename "BandwidthAverageMeasure.csv". 
+    Path to the CSV file to which the bandwidth measurement data will be exported, defaults to BandwidthAverageMeasure.csv on the user's desktop.
     
     .EXAMPLE
-    MeasureBandwidthAverage -Verbose
+    Measure-BandwidthAverage -DurationMinutes 5 -InterfaceName "Ethernet" -OutputCsvFile "C:\Temp\BandwidthMeasure.csv" -Verbose
     
     .NOTES
-    v0.0.1
+    v0.0.9
     #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false, HelpMessage = "Duration in minutes for bandwidth measurement")]
+        [Alias("d")]
         [double]$DurationMinutes = 0.1,
-
+    
         [Parameter(Mandatory = $false, HelpMessage = "Name of the network interface for bandwidth measurement")]
+        [Alias("i")]
         [string]$InterfaceName = "",
-
+    
         [Parameter(Mandatory = $false, HelpMessage = "Output CSV file path to save the bandwidth data")]
+        [Alias("o")]
         [string]$OutputCsvFile = "$env:USERPROFILE\Desktop\BandwidthAverageMeasure.csv"
     )
     $StartTime = Get-Date
     $EndTime = $StartTime.AddMinutes($DurationMinutes)
-    $TimeSpan = New-TimeSpan $StartTime $EndTime
-    $Count = 0
-    $TotalBandwidth = 0
     $Results = @()
-    while ($TimeSpan -gt 0) {
+    while ((Get-Date) -lt $EndTime) {
         $ColInterfaces = Get-CimInstance -Class Win32_PerfFormattedData_Tcpip_NetworkInterface | Where-Object { $_.PacketsPersec -gt 0 }
         foreach ($Interface in $ColInterfaces) {
             if (-not [string]::IsNullOrWhiteSpace($InterfaceName) -and $Interface.Name -ne $InterfaceName) {
@@ -47,37 +45,34 @@ Function MeasureBandwidthAverage {
             $BitsPerSec = $Interface.BytesTotalPersec * 8
             $TotalBits = $Interface.CurrentBandwidth
             if ($TotalBits -gt 0) {
-                $Result = (($BitsPerSec / $TotalBits) * 100)
+                $Result = [math]::Round(($BitsPerSec / $TotalBits) * 100, 2)
                 $InterfaceInfo = [PSCustomObject]@{
                     DateTime          = Get-Date
                     InterfaceName     = $Interface.Name
-                    BandwidthUtilized = $Result.ToString('N2')
+                    BandwidthUtilized = $Result
                 }
-                $Results += $TnterfaceInfo
-                Write-Host ("{0:s} - Interface: {1} Bandwidth utilized: {2}%" -f $InterfaceInfo.DateTime, $InterfaceInfo.InterfaceName, $InterfaceInfo.BandwidthUtilized)
-                $TotalBandwidth += $Result
-                $Count++
+                $Results += $InterfaceInfo
+                Write-Host ("{0:s} - Interface: {1} Bandwidth utilized: {2}%" -f $InterfaceInfo.DateTime, $InterfaceInfo.InterfaceName, $InterfaceInfo.BandwidthUtilized) -ForegroundColor DarkCyan
                 if (-not [string]::IsNullOrWhiteSpace($OutputCsvFile)) {
                     $InterfaceInfo | Export-Csv -Path $OutputCsvFile -NoTypeInformation -Append
                 }
             }
         }
         Start-Sleep -Milliseconds 100
-        $TimeSpan = New-TimeSpan $(Get-Date) $EndTime
     }
+    $Count = $Results.Count
     if ($Count -eq 0) {
         $Message = "No packets were sent/received during the measurement period."
     }
     else {
-        $AverageBandwidth = $TotalBandwidth / $Count
-        $Value = "{0:N2}" -f $AverageBandwidth
-        $Message = "Measurement completed at $(Get-Date).`nMeasurements:`t`t$Count`nAverage Bandwidth utilized:`t$Value %"
+        $AverageBandwidth = ($Results | Measure-Object -Property BandwidthUtilized -Average).Average
+        $Message = "Measurement completed at $(Get-Date).`nMeasurements:`t`t$Count`nAverage Bandwidth utilized:`t$AverageBandwidth %"
         if (-not [string]::IsNullOrWhiteSpace($OutputCsvFile)) {
             $Results | Export-Csv -Path $OutputCsvFile -NoTypeInformation -Append
             $Message += "`nResults appended to CSV file: $OutputCsvFile"
         }
     }
-    Write-Host $Message
+    Write-Host $Message -ForegroundColor Cyan
     if (-not [string]::IsNullOrWhiteSpace($OutputCsvFile)) {
         $Message | Out-String | Add-Content -Path $OutputCsvFile
     }
