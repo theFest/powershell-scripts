95e4f015d62d99b821a904579207872573f7ef53
theFest <8981383+theFest@users.noreply.github.com>
2024-03-11 11:32:14 +0100

    Update Get-Screenshot
    aliases, timestamp, align help info, etc.

--- ps-screen-display/Get-Screenshot.ps1
+++ ps-screen-display/Get-Screenshot.ps1
@@ -1,69 +1,91 @@
 Function Get-Screenshot {
     <#
     .SYNOPSIS
-    Takes a screenshot with customizable options.
-    
+    Captures a screenshot of the specified area of the screen.
+
     .DESCRIPTION
-    This function captures a screenshot of the specified area on the screen with various options such as bounds type, output path, image format, quality, and preview behavior.
-    
+    This function captures a screenshot of the specified area of the screen and saves it to the specified output path. Provides options to customize the screenshot dimensions, image format, quality, delay before capture, and more.
+
     .PARAMETER BoundsType
-    NotMandatory - type of screen area to capture. Options include 'FullHD', 'HD', '4K', or 'Custom'.
+    Type of bounds for the screenshot, values are "FullHD", "HD", "4K", or "Custom".
     .PARAMETER OutputPath
-    Mandatory - the path where the screenshot image will be saved.
+    Specifies the path where the screenshot will be saved.
     .PARAMETER ImageFormat
-    NotMandatory - format of the screenshot image. Options include 'Bmp', 'Png', 'Jpeg', 'Gif', or 'Tiff'.
+    Format of the output image, values are "Bmp", "Png", "Jpeg", "Gif", or "Tiff".
     .PARAMETER Quality
-    NotMandatory - image quality for formats that support it (0-100).
+    Quality of the output image for formats that support it (e.g., Jpeg).
     .PARAMETER CaptureDelay
-    NotMandatory - adds a delay before capturing the screenshot (in seconds).
+    Delay in seconds before capturing the screenshot.
     .PARAMETER ShowPreview
-    NotMandatory - switch to display the screenshot image in a preview window.
+    Show a preview of the captured screenshot.
     .PARAMETER AutoClosePreview
-    NotMandatory - switch to automatically close the preview window after a delay.
+    Automatically close the preview window after a specified delay.
     .PARAMETER PreviewCloseDelay
-    NotMandatory - delay before automatically closing the preview window.
+    Delay in seconds before automatically closing the preview window.
     .PARAMETER CustomPosition
-    NotMandatory - specifies a custom position (point) for the screenshot.
-    
+    Custom position (top-left corner) for capturing the screenshot.
+    .PARAMETER SuppressOutput
+    Indicates whether to suppress output messages.
+    .PARAMETER AddTimestamp
+    Append a timestamp to the filename of the screenshot.
+
     .EXAMPLE
     Get-Screenshot -BoundsType FullHD -OutputPath "$env:USERPROFILE\Desktop\screenshot" -ImageFormat Jpeg -Quality 90 -ShowPreview -AutoClosePreview
-    
+
     .NOTES
-    v0.0.2
+    This function requires administrative privileges to stop and start Windows Defender Real-time Protection.
+    v0.1.2
     #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)]
         [ValidateSet("FullHD", "HD", "4K", "Custom")]
+        [Alias("b")]
         [string]$BoundsType = "FullHD",
 
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
+        [Alias("o")]
         [string]$OutputPath,
 
         [Parameter(Mandatory = $false)]
         [ValidateSet("Bmp", "Png", "Jpeg", "Gif", "Tiff")]
+        [Alias("f")]
         [string]$ImageFormat = "Png",
 
         [Parameter(Mandatory = $false)]
         [ValidateRange(1, 100)]
+        [Alias("q")]
         [int]$Quality = 100,
 
         [Parameter(Mandatory = $false)]
+        [Alias("d")]
         [int]$CaptureDelay = 0,
 
         [Parameter(Mandatory = $false)]
-        [Switch]$ShowPreview,
+        [Alias("sp")]
+        [switch]$ShowPreview,
 
         [Parameter(Mandatory = $false)]
-        [Switch]$AutoClosePreview,
+        [Alias("ac")]
+        [switch]$AutoClosePreview,
 
         [Parameter(Mandatory = $false)]
+        [Alias("p")]
         [int]$PreviewCloseDelay = 3,
 
         [Parameter(Mandatory = $false)]
         [ValidateNotNull()]
-        [System.Drawing.Point]$CustomPosition
+        [Alias("c")]
+        [System.Drawing.Point]$CustomPosition,
+
+        [Parameter(Mandatory = $false)]
+        [Alias("s")]
+        [switch]$SuppressOutput,
+
+        [Parameter(Mandatory = $false)]
+        [Alias("t")]
+        [switch]$AddTimestamp
     )
     BEGIN {
         Add-Type -AssemblyName System.Drawing
@@ -71,20 +93,26 @@ Function Get-Screenshot {
     PROCESS {
         $Bounds = [Drawing.Rectangle]::Empty
         switch ($BoundsType) {
-            "FullHD" { $Bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1920, 1080) }
-            "HD" { $Bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1280, 720) }
-            "4K" { $Bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 3840, 2160) }
+            "FullHD" {
+                $Bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1920, 1080) 
+            }
+            "HD" {
+                $Bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1280, 720) 
+            }
+            "4K" {
+                $Bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 3840, 2160) 
+            }
             "Custom" {
                 $CustomDimensions = Read-Host "Enter custom dimensions for screenshot (format: Width Height)"
                 $Width, $Height = $CustomDimensions -split ' '
                 if (-not [int]::TryParse($Width, [ref]$null) -or -not [int]::TryParse($Height, [ref]$null)) {
-                    Write-Error -Message "Invalid custom dimensions provided."
+                    Write-Error -Message "Invalid custom dimensions provided!"
                     return
                 }
                 $Bounds = [Drawing.Rectangle]::FromLTRB(0, 0, [int]$Width, [int]$Height)
             }
             default {
-                Write-Warning -Message "Invalid BoundsType. Please choose from 'FullHD', 'HD', '4K', or 'Custom'."
+                Write-Warning -Message "Invalid BoundsType. Please choose from 'FullHD', 'HD', '4K', or 'Custom'!"
                 return
             }
         }
@@ -99,20 +127,34 @@ Function Get-Screenshot {
                 $Bounds.Location = $CustomPosition
             }    
             $Graphics.CopyFromScreen($Bounds.Location, [Drawing.Point]::Empty, $Bounds.Size)
+            if ($AddTimestamp) {
+                $Timestamp = Get-Date -Format "yyyyMMdd-HHmmss"
+                $OutputPath = Join-Path -Path $OutputPath -ChildPath "Screenshot_$Timestamp"
+            }
             $ImageFormatEnum = [System.Drawing.Imaging.ImageFormat]::Png
             switch ($ImageFormat) {
-                "Bmp" { $ImageFormatEnum = [System.Drawing.Imaging.ImageFormat]::Bmp }
-                "Jpeg" { $ImageFormatEnum = [System.Drawing.Imaging.ImageFormat]::Jpeg }
-                "Gif" { $ImageFormatEnum = [System.Drawing.Imaging.ImageFormat]::Gif }
-                "Tiff" { $ImageFormatEnum = [System.Drawing.Imaging.ImageFormat]::Tiff }
-                Default {
+                "Bmp" { 
+                    $ImageFormatEnum = [System.Drawing.Imaging.ImageFormat]::Bmp 
+                }
+                "Jpeg" {
+                    $ImageFormatEnum = [System.Drawing.Imaging.ImageFormat]::Jpeg 
+                }
+                "Gif" {
+                    $ImageFormatEnum = [System.Drawing.Imaging.ImageFormat]::Gif 
+                }
+                "Tiff" {
+                    $ImageFormatEnum = [System.Drawing.Imaging.ImageFormat]::Tiff 
+                }
+                default {
                     Write-Warning -Message "Invalid ImageFormat. Please choose from 'Bmp', 'Png', 'Jpeg', 'Gif', or 'Tiff'."
                     return
                 }
             }
             $OutputPathWithExtension = "$OutputPath.$($ImageFormatEnum.ToString().ToLower())"
             $Bitmap.Save($OutputPathWithExtension, $ImageFormatEnum)
-            Write-Host "Screenshot saved to $OutputPathWithExtension" -ForegroundColor Cyan
+            if (-not $SuppressOutput) {
+                Write-Host "Screenshot saved to $OutputPathWithExtension" -ForegroundColor Cyan
+            }
             if ($ShowPreview) {
                 $PreviewProcess = Start-Process -FilePath $OutputPathWithExtension -PassThru
                 if ($AutoClosePreview) {
