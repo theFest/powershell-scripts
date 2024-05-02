4bf9d12ad42d323ba77be53db2a02dd98a24932b
theFest <8981383+theFest@users.noreply.github.com>
2024-05-02 22:07:24 +0200

    Update Test-DriveSpace
    types, help messages, formatted output, etc.

--- ps-disk/Test-DriveSpace.ps1
+++ ps-disk/Test-DriveSpace.ps1
@@ -1,61 +1,65 @@
 Function Test-DriveSpace {
     <#
     .SYNOPSIS
-    This function queries free space with options.
-    
+    Tests the free space on a specified drive.
+
     .DESCRIPTION
-    This function checks if there is enough free space. With parametars you can declare greater then 'Size', and 'Drive letter'.
-    Default check is set to 'GreaterThan' based on free space. Choose any other operator as you wish.
-    
+    This function checks the free space on a specified drive and compares it with the provided size.
+
     .PARAMETER Drive
-    Mandatory - only local drives. Declare drive letter.
+    Specifies the drive letter (e.g., C).
     .PARAMETER Size
-    Mandatory - if free space is greater then the one declared.
+    Size to compare against.
     .PARAMETER Units
-    NotMandatory - choose your unit of choice.
+    Size units (KB, MB, GB, TB). Default is GB.
     .PARAMETER Type
-    NotMandatory - default if set to 3.
+    Drive type (2: Removable, 3: Local Disk, 4: Network Drive, 5: CD/DVD).
     .PARAMETER OptionSize
-    NotMandatory - pick an operator.
+    Comparison operator, values are GreaterThan, LessThan, GreaterThanOrEqualTo, LessThanOrEqualTo, EqualTo, NotEqualTo.
 
     .EXAMPLE
-    Test-DriveSpace -Drive C -Size 60
+    Test-DriveSpace -Drive C -Size 10 -Units GB
 
     .NOTES
-    v0.1.2
+    v0.2.1
     #>
     [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory = $true)]
-        [ValidateRange("A", "Z")]
+    param (
+        [Parameter(Mandatory, HelpMessage = "Drive letter (e.g., C)")]
+        [ValidatePattern("[A-Z]")]
         [string]$Drive,
 
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory, HelpMessage = "Size to compare against")]
+        [ValidateRange(0, [int]::MaxValue)]
         [int]$Size,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(HelpMessage = "Size units (KB, MB, GB, TB)")]
         [ValidateSet("KB", "MB", "GB", "TB")]
         [string]$Units = "GB",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(HelpMessage = "Drive type (2: Removable, 3: Local Disk, 4: Network Drive, 5: CD/DVD)")]
         [ValidateSet("2", "3", "4", "5")]
         [string]$Type = "3",
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(HelpMessage = "Select the comparison operator")]
         [ValidateSet("GreaterThan", "LessThan", "GreaterThanOrEqualTo", "LessThanOrEqualTo", "EqualTo", "NotEqualTo")]
         [string]$OptionSize = "GreaterThan"
     )
-    switch ($Disks = Get-CimInstance Win32_LogicalDisk -Filter DriveType=$Type | `
-            Select-Object DeviceID,
-        @{'Name' = 'Size'; 'Expression' = { [math]::truncate($_.Size / "1$Units") } }, 
-        @{'Name' = 'FreeSpace'; 'Expression' = { [math]::truncate($_.FreeSpace / "1$Units") } } | `
-            Where-Object -Property DeviceID -EQ $Drive':') {
-        { $OptionSize -eq "GreaterThan" } { $Disks.FreeSpace -gt $Size }
-        { $OptionSize -eq "LessThan" } { $Disks.FreeSpace -lt $Size }
-        { $OptionSize -eq "GreaterThanOrEqualTo" } { $Disks.FreeSpace -ge $Size }
-        { $OptionSize -eq "LessThanOrEqualTo" } { $Disks.FreeSpace -le $Size }
-        { $OptionSize -eq "NotEqualTo" } { $Disks.FreeSpace -eq $Size }
-        { $OptionSize -eq "EqualTo" } { $Disks.FreeSpace -ne $Size }
-    }
-    return $Disks
+    $driveType = @{
+        '2' = 'Removable'
+        '3' = 'Local Disk'
+        '4' = 'Network Drive'
+        '5' = 'CD/DVD'
+    }[$Type]
+    Write-Verbose -Message "Checking $driveType drives..."
+    $Disks = Get-CimInstance Win32_LogicalDisk -Filter "DriveType = $Type" | Where-Object DeviceID -eq "${Drive}:"
+    $Res = switch ($OptionSize) {
+        "GreaterThan" { $Disks | Where-Object { $_.FreeSpace / "1$Units" -gt $Size } }
+        "LessThan" { $Disks | Where-Object { $_.FreeSpace / "1$Units" -lt $Size } }
+        "GreaterThanOrEqualTo" { $Disks | Where-Object { $_.FreeSpace / "1$Units" -ge $Size } }
+        "LessThanOrEqualTo" { $Disks | Where-Object { $_.FreeSpace / "1$Units" -le $Size } }
+        "EqualTo" { $Disks | Where-Object { $_.FreeSpace / "1$Units" -eq $Size } }
+        "NotEqualTo" { $Disks | Where-Object { $_.FreeSpace / "1$Units" -ne $Size } }
+    } 
+    $Res | Select-Object DeviceID, VolumeName, @{Name = "Size($Units)"; Expression = { [math]::truncate($_.Size / "1$Units") } }, @{Name = "FreeSpace($Units)"; Expression = { [math]::truncate($_.FreeSpace / "1$Units") } }
 }
