cf742480f9f7b98509b43ab2d183421e9d30d234
theFest <8981383+theFest@users.noreply.github.com>
2024-05-13 10:23:29 +0200

    Update Set-VirtualMemory
    fixed, improved, etc.

--- ps-win-system/Set-VirtualMemory.ps1
+++ ps-win-system/Set-VirtualMemory.ps1
@@ -1,62 +1,80 @@
-Function Set-VirtualMemory {
+function Set-VirtualMemory {
     <#
     .SYNOPSIS
-    Manage virtual memory on Windows machines.
-    
+    Configures virtual memory settings including automatic pagefile management and custom pagefile sizes.
+
     .DESCRIPTION
-    With this function you can set, remove or disable VM on Win7/Win10.
-    
-    .PARAMETER Manage
-    Mandatory - choose between automatic(True) and manual(False), see examples.     
-    .PARAMETER InitialCustomSize
-    NotMandatory - declare minimum custom VM size, you can choose between 1024-4096 MB. You can also parametrize your range.    
-    .PARAMETER MaxCustomSize
-    NotMandatory - declare maximum custom VM size, you can choose between 4096-32768 MB. You can also parametrize your range.   
-    .PARAMETER RemovePageFile
-    NotMandatory - remove, delete and disable pagefile. Not recommended because of potential system instability, restart is necessary.
+    This function allows you to manage virtual memory settings on a Windows computer.
+    It can enable or disable automatic pagefile management, set custom pagefile sizes, and remove existing pagefiles.
     
     .EXAMPLE
-    Set-VirtualMemory -Manage true -Verbose  ##-->(Auto Management/System managed size)
-    Set-VirtualMemory -Manage false -InitialCustomSize 1024 -MaxCustomSize 8096 -Verbose  ##-->(Custom Min/Max sizes)
-    Set-VirtualMemory -Manage false -RemovePageFile -Verbose  ##-->(Remove and delete a pagefile/No paging file)
+    Set-VirtualMemory -ManageAutomaticPageFile On -Verbose                                      #-->(Auto Management/System managed size)
+    Set-VirtualMemory -ManageAutomaticPageFile Off -InitialCustomSize 8192 -MaxCustomSize 32384 #-->(Custom Min/Max sizes)
+    Set-VirtualMemory -ManageAutomaticPageFile Off -RemovePageFile                              #-->(Remove and delete a pagefile/No paging file)
     
     .NOTES
-    v0.0.2
+    v0.2.9
     #>
     [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [ValidateSet("True", "False")]
-        [string]$Manage,
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, HelpMessage = "Enable or disable automatic pagefile management, values are 'On' or 'Off'")]
+        [ValidateSet("On", "Off")]
+        [string]$ManageAutomaticPageFile,
 
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
-        [ValidateRange(1024, 4096)]$InitialCustomSize,
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, HelpMessage = "Initial size of the custom pagefile in megabytes")]
+        [ValidateRange(2048, 8192)]
+        [int]$InitialCustomSize,
 
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
-        [ValidateRange(4096, 65536)]$MaxCustomSize,
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, HelpMessage = "Maximum size of the custom pagefile in megabytes")]
+        [ValidateRange(8192, 131072)]
+        [int]$MaxCustomSize,
       
-        [Parameter(Mandatory = $false)]
-        [switch]$RemovePageFile
+        [Parameter(Mandatory = $false, HelpMessage = "Indicates whether to remove the existing pagefile")]
+        [switch]$RemovePageFile,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Path to the pagefile, default is 'C:\pagefile.sys'")]
+        [string]$PageFilePath = "C:\pagefile.sys"
     )
-    $ComputerSystem = Get-WmiObject -Class Win32_ComputerSystem -EnableAllPrivileges
-    switch ($Manage) {
-        Write-Verbose -Message "Disable/Enable automatic pagefile management"
-        "True" { $ComputerSystem.AutomaticManagedPagefile = $true } 
-        "False" { $ComputerSystem.AutomaticManagedPagefile = $false }
-        default { Write-Verbose -Message "Incorrect operation has executed." }  
+    try {
+        $ComputerSystem = Get-WmiObject -Class Win32_ComputerSystem -EnableAllPrivileges -ErrorAction Stop
+        switch ($ManageAutomaticPageFile) {
+            "On" {
+                $ComputerSystem.AutomaticManagedPagefile = $true
+                Write-Verbose -Message "Automatic pagefile management enabled"
+            }
+            "Off" {
+                $ComputerSystem.AutomaticManagedPagefile = $false
+                Write-Verbose -Message "Automatic pagefile management disabled"
+            }
+            default {
+                throw "Invalid value for 'ManageAutomaticPageFile', values are 'On' or 'Off'!"
+            }  
+        }
+        $ComputerSystem.Put() | Out-Null
+        if ($InitialCustomSize -and $MaxCustomSize) {
+            $PageFileSettings = Get-WmiObject -Class Win32_PageFileSetting -ErrorAction SilentlyContinue
+            if ($PageFileSettings) {
+                Write-Verbose -Message "Setting custom pagefile management"
+                $PageFileSettings.InitialSize = $InitialCustomSize
+                $PageFileSettings.MaximumSize = $MaxCustomSize
+                $PageFileSettings.Put() | Out-Null
+            }
+            else {
+                throw "Failed to retrieve page file settings"
+            }
+        }
+        if ($RemovePageFile) {
+            $PageFileSettings = Get-WmiObject -Class Win32_PageFileSetting -ErrorAction SilentlyContinue
+            if ($PageFileSettings) {
+                Write-Verbose -Message "Disabling and deleting a single pagefile.sys"
+                $PageFileSettings.Delete() | Out-Null
+            }
+            else {
+                Write-Warning -Message "No page file found to remove"
+            }
+        }
     }
-    Write-Verbose -Message "Setting for automatic pagefile management has changed to $Manage."
-    $ComputerSystem.Put() | Out-Null
-    $PFS = Get-WmiObject -Class Win32_PageFileSetting
-    if ($PFS) {
-        #$PFSM = Set-WmiInstance Win32_PageFileSetting -Arguments @{Name='C:\pagefile.sys'; InitialSize=0; MaximumSize=0}
-        Write-Verbose -Message "Setting custom pagefile management."
-        $PFS.InitialSize = $InitialCustomSize
-        $PFS.MaximumSize = $MaxCustomSize
-        $PFS.Put() | Out-Null
-    }
-    if ($RemovePageFile) {
-        Write-Verbose -Message "Disabling and deleting a single pagefile.sys if any."
-        $PFS.Delete()
+    catch {
+        Write-Error -Message $_.Exception.Message
     }
 }
