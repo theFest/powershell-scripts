0a6ee0366f6764737efcae67b608f4952128e63b
theFest <8981383+theFest@users.noreply.github.com>
2024-06-18 00:33:39 +0200

    Update New-FileShortcut.ps1
    params, help info, etc.

--- ps-win-system/New-FileShortcut.ps1
+++ ps-win-system/New-FileShortcut.ps1
@@ -1,61 +1,58 @@
-#Requires -Version 2.0
-Function New-FileShortcut {
+#requires -Version 2.0
+function New-FileShortcut {
     <#
     .SYNOPSIS
-    Creates a new file shortcut (.lnk).
-    
+    Creates a new shortcut (.lnk) file on the desktop pointing to a specified target file.
+
     .DESCRIPTION
-    This function generates a file shortcut (.lnk) for a specified target file.
-    
-    .PARAMETER Name
-    Name of the file shortcut.
-    .PARAMETER Target
-    Full path to the target file.
-    .PARAMETER PassThru
-    Return the created shortcut object.
-    
+    This function creates a new Windows shortcut (.lnk) file, allows specifying name of the shortcut and the full path to the target file. Optionally, it can return the created shortcut object and overwrite an existing shortcut if specified.
+
     .EXAMPLE
-    New-FileShortcut -Name "MyShortcut.lnk" -Target "C:\Path\To\Target\File.exe"
-    
+    New-FileShortcut -Name "7-Zip.lnk" -Target "C:\Program Files\7-Zip\7zFM.exe" -Force -Verbose
+
     .NOTES
-    v0.0.1
+    v0.4.9
     #>
     [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "Low")]
     param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $false, Position = 0, HelpMessage = "Enter the name of the file shortcut. It must end in .lnk")]
-        [ValidateScript({ 
-                $_.EndsWith(".lnk")
-            })]
+        [Parameter(Mandatory = $true, HelpMessage = "Enter the name of the file shortcut, must end in .lnk")]
+        [ValidateScript({ $_ -match '\.lnk$' })]
         [string]$Name,
-        
-        [Parameter(Mandatory = $true, ValueFromPipeline = $false, Position = 1, HelpMessage = "Enter the full path to the target file shortcut. The file must already exist")]
-        [ValidateScript({ 
-                Test-Path -Path $_
-            })]
+
+        [Parameter(Mandatory = $true, HelpMessage = "Enter the full path to the target file shortcut, file must already exist")]
+        [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
         [string]$Target,
-          
-        [Parameter(Mandatory = $false, HelpMessage = "Enter a valid value for PassThru (e.g., -PassThru)")]
-        [switch]$PassThru
+
+        [Parameter(HelpMessage = "Indicates whether to return the created shortcut object")]
+        [switch]$PassThru,
+
+        [Parameter(HelpMessage = "Allows overwriting an existing shortcut file if it already exists")]
+        [switch]$Force
     )
     BEGIN {
         $WShell = New-Object -ComObject "Wscript.Shell"
+        Set-Location -Path "$env:USERPROFILE\Desktop"
     }
     PROCESS {
-        Write-Verbose -Message "Creating shortcut: $Target as $Name..."
-        if (Split-Path $Name -Parent) {
-            $LinkPath = $Name
+        Write-Verbose -Message "Creating shortcut: $Target as $Name"
+        $LinkPath = if (Test-Path -Path $Name -PathType Container) {
+            $Name
         }
         else {
-            $LinkPath = Join-Path -Path (Get-Location) $Name
-            Write-Verbose -Message "Adjusting link file to $LinkPath..."
+            Join-Path -Path (Get-Location) -ChildPath $Name
+            Write-Verbose "Adjusted link file path to: $LinkPath"
+        }
+        if ((Test-Path -Path $LinkPath -PathType Leaf) -and (!$Force)) {
+            Write-Warning "Shortcut '$LinkPath' already exists, use -Force to overwrite!"
+            return
         }
         $Shortcut = $WShell.CreateShortcut($LinkPath)
         $Shortcut.TargetPath = $Target
-        if ($PSCmdlet.ShouldProcess($Target)) {
-            Write-Verbose -Message "Saving shortcut..."
+        if ($PSCmdlet.ShouldProcess($Target, "Create shortcut")) {
             $Shortcut.Save()
+            Write-Verbose -Message "Shortcut saved to: $($Shortcut.FullName)"
             if ($PassThru) {
-                Get-Item -Path $Shortcut.Fullname -Verbose
+                Get-Item -Path $Shortcut.FullName
             }
         }
     }
