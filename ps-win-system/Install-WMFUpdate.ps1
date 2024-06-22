7b6f6f68aeb608162a06150c73ff824fbae611cf
theFest <8981383+theFest@users.noreply.github.com>
2024-06-22 23:07:11 +0200

    Update Install-WMFUpdate
    small changes

--- ps-win-system/Install-WMFUpdate.ps1
+++ ps-win-system/Install-WMFUpdate.ps1
@@ -1,51 +1,39 @@
-Function Install-WMFUpdate {
+function Install-WMFUpdate {
     <#
     .SYNOPSIS
-    Upgrade Windows Management Framework 5.1.
+    Installs the Windows Management Framework (WMF) 5.1 update if not already installed.
 
     .DESCRIPTION
-    This function is used to download, extract, and upgrade Windows Management Framework and PowerShell 5.1. Both KB3191566 and KB2872035 are installed with KB3191566.
+    This function checks if the WMF 5.1 update (KB3191566) is installed on the system. If not, it downloads and installs the update. The script logs the process and can optionally restart the computer after installation if the -Restart switch is provided.
 
-    .PARAMETER Key
-    NotMandatory - Credentials to authenticate against a source for downloading.
-    .PARAMETER Restart
-    NotMandatory - Reboot is necessary for the update to apply. Specify this switch if you want to reboot automatically after installation.
-
-    .EXAMPLES
+    .EXAMPLE
     Install-WMFUpdate -Restart
-    Install-WMFUpdate -Key $Key
 
     .NOTES
-    v0.0.2
+    v0.8.0
     #>
     [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $false)]
-        [PSCredential]$Key,
-
-        [Parameter(Mandatory = $false)]
+    param (
+        [Parameter(Mandatory = $false, HelpMessage = "Restarts the computer after applying changes")]
+        [Alias("r")]
         [switch]$Restart
     )
     BEGIN {
         Start-Transcript -Path "$env:TEMP\WMF_Transcript.txt"
         $WMF5 = Get-HotFix -Id KB3191566 -ErrorAction SilentlyContinue -Verbose
-        Write-Host "PS update check has started..."
+        Write-Host "PS update check has started..." -ForegroundColor Cyan
     }
     PROCESS {
         if (!$WMF5) {
             Write-Host "WMF KB3191566 is missing, installing..."
             New-Item -Path $env:TEMP -Name WMF -ItemType Directory -Force -Verbose
             $WMF_url = 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7AndW2K8R2-KB3191566-x64.zip'
-            if ($Key) {
-                Invoke-WebRequest -Uri $WMF_url -OutFile "$env:TEMP\WMF\Win7AndW2K8R2-KB3191566-x64.zip" -Credential $Key
-            } else {
-                Invoke-WebRequest -Uri $WMF_url -OutFile "$env:TEMP\WMF\Win7AndW2K8R2-KB3191566-x64.zip"
-            }
-            Write-Host "WMF KB3191566 is downloading..."
+            Invoke-WebRequest -Uri $WMF_url -OutFile "$env:TEMP\WMF\Win7AndW2K8R2-KB3191566-x64.zip"
+            Write-Verbose -Message "WMF KB3191566 is downloading..."
             Expand-Archive -Path "$env:TEMP\WMF\Win7AndW2K8R2-KB3191566-x64.zip" -DestinationPath "$env:TEMP\WMF" -Force -Verbose
             $KB_path = Test-Path -Path "$env:TEMP\WMF\Win7AndW2K8R2-KB3191566-x64.msu" -Verbose
             if ($KB_path) {
-                Write-Host "WMF KB3191566 downloaded and extracted."
+                Write-Host "WMF KB3191566 downloaded and extracted..." -ForegroundColor DarkCyan
                 while ((Get-Service -Name wuauserv).StartType -ne "Manual") {
                     Set-Service -Name wuauserv -StartupType Manual -Verbose
                     try {
@@ -59,12 +47,12 @@ Function Install-WMFUpdate {
                 }
             }
             else {
-                Write-Host "Error in download and/or extract of WMF files!"
+                Write-Host "Error in download and/or extract of WMF files!" -ForegroundColor DarkRed
                 return $false
             }
         }
         else {
-            Write-Host "KB3191566 is already installed."
+            Write-Host "KB3191566 is already installed" -ForegroundColor Green
             return $true
         }
     }
@@ -74,13 +62,13 @@ Function Install-WMFUpdate {
             return $true
         }
         else {
-            Write-Host "WMF PowerShell 5.1 installation has failed!"
+            Write-Host "WMF PowerShell 5.1 installation has failed!" -ForegroundColor Red
             return $false
         }
         Stop-Transcript
         Move-Item -Path "$env:TEMP\WMF_Transcript.txt" -Destination "$env:TEMP\WMF\WMF_Transcript.txt" -Verbose -Force
         if ($Restart) {
-            Write-Host "Restarting computer."
+            Write-Host "Restarting computer..." -ForegroundColor Cyan
             Restart-Computer -Force
         }
     }
