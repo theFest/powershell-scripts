ccba2cd74be5de37a351088364a3dd281113f89d
theFest <8981383+theFest@users.noreply.github.com>
2024-07-03 08:39:17 +0200

    Update Send-SimulatedKey
    fixed, etc.

--- ps-win-system/Send-SimulatedKey.ps1
+++ ps-win-system/Send-SimulatedKey.ps1
@@ -1,69 +1,93 @@
-Function Send-SimulatedKey {
+function Send-SimulatedKey {
     <#
     .SYNOPSIS
-    This function sends keyboard keys to the active window using the .NET framework's System.Windows.Forms.SendKeys class.
-    
+    Simulates key presses and releases.
+
     .DESCRIPTION
-    This function allows you to send keyboard keys to the active window in a controlled manner.
-    You can specify the key to be sent, the action (press or release), the number of times to repeat the action, and the key modifiers to be used (Alt, Ctrl, or Shift).
-    
-    .PARAMETER Key
-    Mandatory - specifies the key to be sent. The valid values are predefined in ValidateSet option.
-    .PARAMETER Action
-    NotMandatory - the action to be performed on the key. The valid values are "Press" and "Release". If not specified, the default value is "Press".
-    .PARAMETER Repeat
-    NotMandatory - number of times to repeat the action. The value must be in the range of 1 to 60. If not specified, the default value is 1.
-    .PARAMETER Modifiers
-    NotMandatory - specifies the key modifiers to be used when sending the key. The valid values are "Alt", "Ctrl", and "Shift".
-    
+    This function simulates key presses and releases using the SendKeys method from the System.Windows.Forms assembly. It supports a variety of keys and can handle modifier keys (Alt, Ctrl, Shift) as well.
+
     .EXAMPLE
-    Send-SimulatedKey -Key "Enter" -Repeat 5
-    Send-SimulatedKey -Key "Tab" -Modifiers "Ctrl"
-    Send-SimulatedKey -Key "Shift" -Action "Release"
-    Send-SimulatedKey -Key "F5" -Modifiers "Ctrl", "Alt" -Repeat 3
-    Send-SimulatedKey -Key "Tab" -Action "Press" -Repeat 2 -Modifiers @("Shift")
-    Send-SimulatedKey -Key "Enter" -Repeat 1 ; Read-Host "I will autocomplete Hosts input by adding 'ENTER'"
-    
+    Send-SimulatedKey -Key F1
+
     .NOTES
-    v0.1.1
+    v0.4.2
     #>
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, HelpMessage = "Specifies the key to be simulated")]
         [ValidateSet("Enter", "Backspace", "Tab", "Escape", `
                 "Insert", "Shift", "Delete", "Home", "End", "PageUp", "PageDown", `
                 "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "PrintScreen", "PauseBreak", `
                 "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12")]
+        [Alias("k")]
         [string]$Key,
     
-        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, Position = 1)]
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, HelpMessage = "Action to be performed on the key")]
         [ValidateSet("Press", "Release")]
+        [Alias("a")]
         [string]$Action = "Press",
     
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true, Position = 2)]
+        [Parameter(Mandatory = $false, HelpMessage = "Number of times to repeat the key action, must be an integer between 1 and 60")]
         [ValidateRange(1, 60)]
+        [Alias("r")]
         [int]$Repeat = 1,
     
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true, Position = 3)]
+        [Parameter(Mandatory = $false, HelpMessage = "Modifier keys to be held during the key action, can be an array")]
         [ValidateSet("Alt", "Ctrl", "Shift")]
+        [Alias("m")]
         [string[]]$Modifiers = @()
     )
     Add-Type -AssemblyName System.Windows.Forms
+    $ModifiersMap = @{
+        "Alt"   = "%"
+        "Ctrl"  = "^"
+        "Shift" = "+"
+    }
+    $KeyMap = @{
+        "Enter"       = "~"
+        "Backspace"   = "{BACKSPACE}"
+        "Tab"         = "{TAB}"
+        "Escape"      = "{ESC}"
+        "Insert"      = "{INSERT}"
+        "Delete"      = "{DELETE}"
+        "Home"        = "{HOME}"
+        "End"         = "{END}"
+        "PageUp"      = "{PGUP}"
+        "PageDown"    = "{PGDN}"
+        "ArrowUp"     = "{UP}"
+        "ArrowDown"   = "{DOWN}"
+        "ArrowLeft"   = "{LEFT}"
+        "ArrowRight"  = "{RIGHT}"
+        "PrintScreen" = "{PRTSC}"
+        "PauseBreak"  = "{BREAK}"
+        "F1"          = "{F1}"
+        "F2"          = "{F2}"
+        "F3"          = "{F3}"
+        "F4"          = "{F4}"
+        "F5"          = "{F5}"
+        "F6"          = "{F6}"
+        "F7"          = "{F7}"
+        "F8"          = "{F8}"
+        "F9"          = "{F9}"
+        "F10"         = "{F10}"
+        "F11"         = "{F11}"
+        "F12"         = "{F12}"
+    }
+    $ModifiersNotation = ""
+    foreach ($Modifier in $Modifiers) {
+        $ModifiersNotation += $ModifiersMap[$Modifier]
+    }
+    $SendKeysKey = $KeyMap[$Key]
+    $SendKeysSequence = $ModifiersNotation + $SendKeysKey
     switch ($Action) {
         "Press" {
             for ($i = 0; $i -lt $Repeat; $i++) {
-                foreach ($Modifier in $Modifiers) {
-                    [System.Windows.Forms.SendKeys]::SendWait("+$Modifier")
-                }
-                [System.Windows.Forms.SendKeys]::SendWait("$Key")
-                foreach ($Modifier in $Modifiers) {
-                    [System.Windows.Forms.SendKeys]::SendWait("-$Modifier")
-                }
+                [System.Windows.Forms.SendKeys]::SendWait($SendKeysSequence)
             }
         }
         "Release" {
             for ($i = 0; $i -lt $Repeat; $i++) {
-                [System.Windows.Forms.SendKeys]::SendWait("-$Key")
+                [System.Windows.Forms.SendKeys]::SendWait($SendKeysSequence)
             }
         }
     }
