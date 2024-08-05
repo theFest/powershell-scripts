83055db02e1b30ded697110e9b242ac6175d3b26
theFest <8981383+theFest@users.noreply.github.com>
2024-08-06 00:47:33 +0200

    Update Set-WindowsInsider
    help, error handle, etc.

--- ps-win-updates/Set-WindowsInsider.ps1
+++ ps-win-updates/Set-WindowsInsider.ps1
@@ -1,55 +1,55 @@
-Function Set-WindowsInsider {
+function Set-WindowsInsider {
     <#
     .SYNOPSIS
     Enables or disables the Windows Insider Program and optionally restarts the system.
 
     .DESCRIPTION
-    This function allows you to enable or disable the Windows Insider Program on a Windows system, it can also restart the system if needed.
-
-    .PARAMETER Action
-    Specifies whether to enable or disable the Windows Insider Program, valid values are "Enable" or "Disable".
-    .PARAMETER Restart
-    Indicates whether to restart the system after performing the action, if this switch is present, the system will be restarted.
+    This function allows you to enable or disable the Windows Insider Program on a Windows system by modifying the appropriate registry keys. It can also restart the system if needed.
 
     .EXAMPLE
     Set-WindowsInsider -Action Enable
     Set-WindowsInsider -Action Disable -Restart
 
     .NOTES
-    v0.0.1
+    v0.1.2
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, HelpMessage = "Specifies whether to enable or disable the Windows Insider Program")]
         [ValidateSet("Enable", "Disable")]
         [string]$Action,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, HelpMessage = "Restart the system after performing the action")]
         [switch]$Restart
     )
     BEGIN {
         $RegPath = "HKLM:\SOFTWARE\Microsoft\WindowsSelfHost\Applicability"
         $RegName = "EnablePreviewBuilds"
+        $ErrorActionPreference = "Stop"
     }
     PROCESS {
-        switch ($Action) {
-            "Enable" {
-                if (-not (Test-Path -Path $RegPath)) {
-                    New-Item -Path $RegPath -Force | Out-Null
+        try {
+            switch ($Action) {
+                "Enable" {
+                    if (-not (Test-Path -Path $RegPath)) {
+                        New-Item -Path $RegPath -Force | Out-Null
+                    }
+                    Set-ItemProperty -Path $RegPath -Name $RegName -Value 1
+                    Write-Host "Windows Insider Program enabled" -ForegroundColor Green
                 }
-                Set-ItemProperty -Path $RegPath -Name $RegName -Value 1
-                Write-Host "Windows Insider Program enabled" -ForegroundColor Green
+                "Disable" {
+                    if (Test-Path -Path $RegPath) {
+                        Remove-ItemProperty -Path $RegPath -Name $RegName -Force -Verbose
+                        Write-Host "Windows Insider Program disabled" -ForegroundColor Green
+                    }
+                    else {
+                        Write-Host "Windows Insider Program is not enabled" -ForegroundColor Yellow
+                    }
+                }
             }
-            "Disable" {
-                if (Test-Path -Path $RegPath) {
-                    Remove-ItemProperty -Path $RegPath -Name $RegName -Force -Verbose
-                    Write-Host "Windows Insider Program disabled" -ForegroundColor Green
-            
-                }
-                else {
-                    Write-Host "Windows Insider Program is not enabled" -ForegroundColor Yellow
-                }
-            }
+        }
+        catch {
+            Write-Error "An error occurred: $_"
         }
     }
     END {
