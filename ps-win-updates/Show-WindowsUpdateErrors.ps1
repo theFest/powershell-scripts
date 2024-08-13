8067b3f5f1fce3c133626fb0fbe17e4e68b89d96
theFest <8981383+theFest@users.noreply.github.com>
2024-08-14 00:53:25 +0200

    Update Show-WindowsUpdateErrors
    fix, sec, etc.

--- ps-win-updates/Show-WindowsUpdateErrors.ps1
+++ ps-win-updates/Show-WindowsUpdateErrors.ps1
@@ -1,43 +1,73 @@
-Function Show-WindowsUpdateErrors {
+function Show-WindowsUpdateErrors {
     <#
     .SYNOPSIS
-    Retrieves and displays Windows Update errors from specified event log.
+    Retrieves and displays Windows Update errors from the specified event log, either locally or on a remote computer.
 
     .DESCRIPTION
-    This function retrieves and displays Windows Update errors from the specified event log.
-
-    .PARAMETER LogName
-    Name of the event log to search for Windows Update errors, valid values are "System," "Application," or "Security.", default is "System."
+    This function retrieves and displays Windows Update errors from the specified event log on either a local or remote computer. It supports querying remote computers by specifying a ComputerName, and optionally, Username and Password for authentication.
 
     .EXAMPLE
     Show-WindowsUpdateErrors
+    Show-WindowsUpdateErrors -ComputerName "remote_pass" -User "remote_pass" -Pass (ConvertTo-SecureString "remote_pass" -AsPlainText -Force)
 
     .NOTES
-    v0.0.1
+    v0.2.7
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, HelpMessage = "Name of the event log to search for Windows Update errors")]
         [ValidateSet("System", "Application", "Security")]
-        [string]$LogName = "System"
+        [string]$LogName = "System",
+
+        [Parameter(Mandatory = $false, HelpMessage = "Target computer to retrieve the Windows Update errors from. If not specified, the local computer is used")]
+        [string]$ComputerName = $env:COMPUTERNAME,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Username to use for remote authentication")]
+        [string]$User,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Password to use for remote authentication")]
+        [System.Security.SecureString]$Pass
     )
-    $FilterXPath = "*[System[Provider[@Name='Microsoft-Windows-WindowsUpdateClient']]]"
-    $ErrorLog = Get-WinEvent -LogName $LogName -FilterXPath $FilterXPath -ErrorAction SilentlyContinue
-    if ($ErrorLog.Count -eq 0) {
-        Write-Warning -Message "No Windows Update errors found in the $LogName event log!"
-    }
-    else {
-        Write-Host "Windows Update Errors:" -ForegroundColor DarkCyan
-        $ErrorLog | ForEach-Object {
-            $Properties = $_.Properties
-            $UpdateDetails = @{
-                "Message"     = $Properties[1].Value
-                "EventID"     = $_.Id
-                "TimeCreated" = $_.TimeCreated
-                "Provider"    = $Properties[0].Value
-                "Level"       = $Properties[2].Value
+    BEGIN {
+        $FilterXPath = "*[System[Provider[@Name='Microsoft-Windows-WindowsUpdateClient']]]"
+        if ($ComputerName -ne $env:COMPUTERNAME) {
+            if ($User -and $Pass) {
+                $Credential = New-Object System.Management.Automation.PSCredential($User, $Pass)
             }
-            $UpdateDetails
+            elseif ($User -and -not $Pass) {
+                Write-Error -Message "Password must be provided if Username is specified!"
+                return
+            }
+        }
+    }
+    PROCESS {
+        try {
+            if ($ComputerName -eq $env:COMPUTERNAME) {
+                $ErrorLog = Get-WinEvent -LogName $LogName -FilterXPath $FilterXPath -ErrorAction SilentlyContinue
+            }
+            else {
+                $ErrorLog = Get-WinEvent -ComputerName $ComputerName -Credential $Credential -LogName $LogName -FilterXPath $FilterXPath -ErrorAction SilentlyContinue
+            }
+            if ($ErrorLog.Count -eq 0) {
+                Write-Warning -Message "No Windows Update errors found in the $LogName event log on $ComputerName!"
+            }
+            else {
+                Write-Host "Windows Update Errors on $ComputerName :" -ForegroundColor DarkCyan
+                $ErrorLog | ForEach-Object {
+                    $Properties = $_.Properties
+                    $UpdateDetails = @{
+                        "Message"     = $Properties[1].Value
+                        "EventID"     = $_.Id
+                        "TimeCreated" = $_.TimeCreated
+                        "Provider"    = $Properties[0].Value
+                        "Level"       = $_.LevelDisplayName
+                    }
+                    $UpdateDetails
+                } | Format-Table -AutoSize
+            }
+        }
+        catch {
+            Write-Error -Message "An error occurred while retrieving Windows Update errors: $_"
         }
     }
 }
