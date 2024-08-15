3fe0339c690316ab99fda75e22aed34d328bf5bd
theFest <8981383+theFest@users.noreply.github.com>
2024-08-15 13:30:45 +0200

    Update Invoke-WindowsUpdateCleanup
    fixed, checks, aligned, etc.

--- ps-win-updates/Invoke-WindowsUpdateCleanup.ps1
+++ ps-win-updates/Invoke-WindowsUpdateCleanup.ps1
@@ -1,57 +1,101 @@
-Function Invoke-WindowsUpdateCleanup {
+function Invoke-WindowsUpdateCleanup {
     <#
     .SYNOPSIS
-    Performs Windows Update cleanup using DISM commands.
+    Performs a Windows Update cleanup using the Deployment Imaging Service and Management Tool (DISM).
 
     .DESCRIPTION
-    This function initiates Windows Update cleanup using DISM commands, provides options to perform various cleanup tasks, such as starting component cleanup, resetting the base image, and superseding service packs.
-
-    .PARAMETER StartComponentCleanup
-    Specifies whether to start component cleanup, this option deletes the updated components immediately.
-    .PARAMETER ResetBase
-    Specifies whether to delete all superseded versions of every component, be aware that all existing service packs and updates cannot be uninstalled after this command is completed.
-    .PARAMETER SPSuperseded
-    Whether to remove all backup components needed to uninstall a service pack, after using this command, you cannot remove the service pack anymore.
+    This unction performs cleanup of Windows Update components on the local or a remote machine. It supports multiple options including component cleanup, base reset, and cleaning up superseded service pack packages.
 
     .EXAMPLE
     Invoke-WindowsUpdateCleanup -StartComponentCleanup
+    Invoke-WindowsUpdateCleanup -ComputerName "remote_host" -User "remote_user" -Pass "remote_pass" -SPSuperseded
 
     .NOTES
-    v0.0.1
+    v0.4.0
     #>
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $true)]
     param (
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, HelpMessage = "Performs a component cleanup on the running operating system, can free up space by removing previous versions of components that are no longer needed")]
         [switch]$StartComponentCleanup,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, HelpMessage = "Resets the base of all packages to the current versions of the packages, all previous versions of components are removed, making it impossible to uninstall updates")]
         [switch]$ResetBase,
 
-        [Parameter(Mandatory = $false)]
-        [switch]$SPSuperseded
+        [Parameter(Mandatory = $false, HelpMessage = "Cleans up superseded service pack packages, can help in reducing the disk space used by the operating system")]
+        [switch]$SPSuperseded,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Hostname of the remote computer on which the Windows Update cleanup should be performed. If not specified, the cleanup is performed on the local machine")]
+        [string]$ComputerName,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Username for the remote connection, required when the `ComputerName` is specified")]
+        [string]$User,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Password for the remote connection, required when the `ComputerName` is specified")]
+        [string]$Pass
     )
-    try {
-        Write-Verbose -Message "Initiating Windows Update cleanup..."
-        $DismArgs = @("/online", "/Cleanup-Image")
-        if ($StartComponentCleanup) {
-            $DismArgs += "/StartComponentCleanup"
+    $IsRebootPending = {
+        $RebootPending = $false
+        $PendingFile = "$env:windir\WinSxS\pending.xml"
+        $RebootKeys = @(
+            "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending",
+            "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootInProgress",
+            "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\PackagesPending",
+            "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\PendingFileRenameOperations",
+            "HKLM:\SOFTWARE\Microsoft\Updates\UpdateExeVolatile"
+        )
+        foreach ($Key in $RebootKeys) {
+            if (Test-Path -Path $Key) {
+                $RebootPending = $true
+                break
+            }
         }
-        if ($ResetBase) {
-            $DismArgs += "/ResetBase"
+        if (Test-Path -Path $pendingFile) {
+            $RebootPending = $true
         }
-        if ($SPSuperseded) {
-            $DismArgs += "/SPSuperseded"
-        }
-        $CmdArguments = "cmd /c dism.exe $($DismArgs -join ' ')"
-        $Result = Start-Process -FilePath "cmd.exe" -ArgumentList "/c $CmdArguments" -PassThru -Wait
-        if ($Result.ExitCode -eq 0) {
-            Write-Host "Windows Update cleanup completed" -ForegroundColor Green
+        return $RebootPending
+    }
+    if (& $isRebootPending) {
+        Write-Warning -Message "A reboot is pending. Please restart the system and try again!"
+        return
+    }
+    $DismArgs = @("/Cleanup-Image")
+    if ($StartComponentCleanup) { $DismArgs += "/StartComponentCleanup" }
+    if ($ResetBase) { $DismArgs += "/ResetBase" }
+    if ($SPSuperseded) { $DismArgs += "/SPSuperseded" }
+    $LogPath = "$env:windir\Logs\DISM\dism.log"
+    $DismArgs += "/LogPath:$LogPath"
+    $DismArgs += "/LogLevel:3"
+    $ExecuteDismCommand = {
+        param (
+            [string[]]$DismArgs,
+            [string]$LogPath
+        )
+        $DismCommand = "dism.exe /online $($DismArgs -join ' ')"
+        Write-Verbose -Message "Executing command: $DismCommand"
+        $process = Start-Process -FilePath "cmd.exe" -ArgumentList "/c $DismCommand" -Wait -PassThru
+        if ($process.ExitCode -eq 0) {
+            Write-Host "Windows Update cleanup completed successfully." -ForegroundColor Green
         }
         else {
-            throw "Error: Windows Update cleanup failed with exit code $($Result.ExitCode)!"
+            throw "DISM failed with exit code $($process.ExitCode). See log at $LogPath for details!"
         }
     }
-    catch {
-        Write-Error -Message "Error: $_"
+    if ($ComputerName) {
+        if ($PSCmdlet.ShouldProcess($ComputerName, "Run Windows Update Cleanup")) {
+            $Credential = New-Object System.Management.Automation.PSCredential ($User, (ConvertTo-SecureString $Pass -AsPlainText -Force))
+            $SessionOptions = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
+            $Session = New-PSSession -ComputerName $ComputerName -Credential $Credential -SessionOption $SessionOptions
+            try {
+                Invoke-Command -Session $Session -ScriptBlock $ExecuteDismCommand -ArgumentList ($DismArgs, $LogPath)
+            }
+            finally {
+                Remove-PSSession -Session $Session -Verbose
+            }
+        }
+    }
+    else {
+        if ($PSCmdlet.ShouldProcess("localhost", "Run Windows Update Cleanup")) {
+            & $ExecuteDismCommand -DismArgs $DismArgs -LogPath $LogPath
+        }
     }
 }
