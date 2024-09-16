d24cbd048f492aedffd55eb9f6a2a8ce95d0969b
theFest <8981383+theFest@users.noreply.github.com>
2024-09-16 20:58:04 +0200

    Update i/e reg key
    refactored, aligned, etc.

--- ps-win-registry/Export-RegistryKey.ps1
+++ ps-win-registry/Export-RegistryKey.ps1
@@ -1,111 +1,87 @@
-Function Export-RegistryKey {
+function Export-RegistryKey {
     <#
     .SYNOPSIS
-    Exports a registry key from a remote or local computer.
+    Exports a registry key to a .reg file, either from the local computer or a remote computer.
 
     .DESCRIPTION
-    This function exports a specified registry key from a remote or local computer and saves it to a specified local path.
-
-    .PARAMETER RegPath
-    Registry path to export, either local or remote.
-    .PARAMETER LocalExportPath
-    Local path where the exported registry file will be saved.
-    .PARAMETER RemoteExportPath
-    Remote path where the exported registry file will be temporarily saved on the remote computer, if not provided a default path will be used.
-    .PARAMETER ComputerName
-    Name of the remote computer from which to export the registry key, if not provided, the export will be performed on the local computer.
-    .PARAMETER User
-    Username to use for the remote connection, required when exporting from a remote computer.
-    .PARAMETER Pass
-    Password to use for the remote connection, required when exporting from a remote computer.
+    This function allows exporting a specified registry key from either a local or remote computer to a .reg file. When run against a remote computer, it creates a PowerShell session and uses `reg.exe` to export the registry key.
+    It also provides options for supplying credentials for the remote connection. The exported file can be saved locally or on a remote system.
 
     .EXAMPLE
     Export-RegistryKey -RegPath "HKCU\AppEvents" -LocalExportPath "$env:USERPROFILE\Desktop\local_reg_export.reg" -Verbose
     "HKLM\SOFTWARE" | Export-RegistryKey -LocalExportPath "$env:USERPROFILE\Desktop\remote_reg_export.reg" -ComputerName "remote_hostname" -User "remote_user" -Pass "remote_pass"
 
     .NOTES
-    Version: 0.1.2
+    v0.5.4
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, HelpMessage = "Specifies the registry path to export, either local or remote")]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, HelpMessage = "Registry path to export, either local or remote")]
         [ValidateNotNullOrEmpty()]
         [Alias("r")]
         [string]$RegPath,
 
-        [Parameter(Mandatory = $true, HelpMessage = "Specifies the local path where the exported registry file will be saved")]
+        [Parameter(Mandatory = $true, HelpMessage = "Local path where the exported registry file will be saved")]
         [ValidateNotNullOrEmpty()]
         [Alias("l")]
         [string]$LocalExportPath,
 
-        [Parameter(Mandatory = $false, HelpMessage = "Specifies the remote path where the exported registry file will be temporarily saved on the remote computer, if not provided a default path will be used")]
+        [Parameter(Mandatory = $false, HelpMessage = "Remote path where the exported registry file will be temporarily saved on the remote computer")]
         [Alias("e")]
         [string]$RemoteExportPath = "C:\Users\Default\AppData\Local\Temp\exported.reg",
 
-        [Parameter(Mandatory = $false, HelpMessage = "Specifies the name of the remote computer from which to export the registry key, if not provided, the export will be performed on the local computer")]
+        [Parameter(Mandatory = $false, HelpMessage = "Remote computer name for the export operation")]
         [Alias("c")]
         [string]$ComputerName,
 
-        [Parameter(Mandatory = $false, HelpMessage = "Specifies the username to use for the remote connection, required when exporting from a remote computer")]
+        [Parameter(Mandatory = $false, HelpMessage = "Username for remote connection")]
         [Alias("u")]
         [string]$User,
 
-        [Parameter(Mandatory = $false, HelpMessage = "Specifies the password to use for the remote connection, required when exporting from a remote computer")]
+        [Parameter(Mandatory = $false, HelpMessage = "Password for remote connection")]
         [Alias("p")]
         [string]$Pass
     )
     try {
         $Session = $null
         if ($ComputerName) {
-            $TestConnectionScriptBlock = {
-                param($Using:ComputerName)
-                $IsReachable = (Test-Connection -ComputerName $Using:ComputerName -Count 1 -Quiet) -and (Test-WSMan -ComputerName $Using:ComputerName -Authentication Default)
-                if ($IsReachable) {
-                    Write-Host "Connection to the remote computer '$Using:ComputerName' established..." -ForegroundColor Green
-                }
-                else {
-                    Write-Error -Message "Failed to establish a connection to the remote computer '$Using:ComputerName'"
-                }
-                $IsReachable
-            }
-            $IsComputerReachable = Invoke-Command -ComputerName $ComputerName -ScriptBlock $TestConnectionScriptBlock -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (ConvertTo-SecureString -String $Pass -AsPlainText -Force))
+            $SecurePassword = ConvertTo-SecureString -String $Pass -AsPlainText -Force
+            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $SecurePassword
+            $IsComputerReachable = Test-Connection -ComputerName $ComputerName -Count 1 -Quiet
             if ($IsComputerReachable) {
-                $Session = New-PSSession -ComputerName $ComputerName -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (ConvertTo-SecureString -String $Pass -AsPlainText -Force))
+                Write-Verbose "Connecting to remote computer: $ComputerName"
+                $Session = New-PSSession -ComputerName $ComputerName -Credential $Credential
                 $ExportScriptBlock = {
                     param($RegPath, $RemoteExportPath)
-                    Write-Verbose -Message "Export on the remote computer has started..."
                     Start-Process -FilePath 'reg.exe' -ArgumentList 'export', $RegPath, $RemoteExportPath -Wait -NoNewWindow
                 }
-                $ExportJob = Invoke-Command -Session $Session -AsJob -ScriptBlock $ExportScriptBlock -ArgumentList $RegPath, $RemoteExportPath
-                Wait-Job $ExportJob | Out-Null ; Receive-Job $ExportJob | Out-Null
-                Copy-Item -FromSession $Session -Path $RemoteExportPath -Destination $LocalExportPath -Force -Verbose
-                Invoke-Command -Session $Session -ScriptBlock {
-                    param($RemoteExportPath)
-                    Remove-Item -Path $RemoteExportPath -Force -Verbose
-                } -ArgumentList $RemoteExportPath
+                Invoke-Command -Session $Session -ScriptBlock $ExportScriptBlock -ArgumentList $RegPath, $RemoteExportPath
+                Copy-Item -FromSession $Session -Path $RemoteExportPath -Destination $LocalExportPath -Force
+                Invoke-Command -Session $Session -ScriptBlock { Remove-Item -Path $using:RemoteExportPath -Force }
+                Write-Host "Registry key exported from remote computer to '$LocalExportPath'" -ForegroundColor Green
             }
             else {
-                Write-Error -Message "Failed to establish a connection to the remote computer '$ComputerName'"
+                Write-Error -Message "Failed to connect to remote computer: $ComputerName"
             }
         }
         else {
-            Write-Verbose -Message "Export on the local computer has started..."
+            Write-Verbose -Message "Exporting registry key from local machine..."
             Start-Process -FilePath 'reg.exe' -ArgumentList 'export', $RegPath, $LocalExportPath -Wait -NoNewWindow
-        }
-        if (Test-Path $LocalExportPath) {
-            Write-Host "Registry key exported to '$LocalExportPath'" -ForegroundColor Green
-        }
-        else {
-            Write-Error -Message "Failed to export the registry key!"
+            if (Test-Path $LocalExportPath) {
+                Write-Host "Registry key exported to '$LocalExportPath'" -ForegroundColor Green
+            }
+            else {
+                Write-Error -Message "Failed to export registry key to '$LocalExportPath'"
+            }
         }
     }
     catch {
-        Write-Error -Message $_.Exception.Message
+        Write-Error -Message "Error occurred: $_"
     }
     finally {
         if ($Session) {
-            Write-Verbose -Message "Finished, closing PS Session and exiting..."
-            Remove-PSSession -Session $Session -Verbose -ErrorAction SilentlyContinue
+            Write-Verbose -Message "Closing remote session..."
+            Remove-PSSession -Session $Session -Verbose
         }
     }
 }
--- ps-win-registry/Import-RegistryKey.ps1
+++ ps-win-registry/Import-RegistryKey.ps1
@@ -1,28 +1,17 @@
-Function Import-RegistryKey {
+function Import-RegistryKey {
     <#
     .SYNOPSIS
-    Imports a registry key from a local or remote machine.
+    Imports a registry key from a .reg file, either to the local computer or to a remote computer.
 
     .DESCRIPTION
-    This function imports a registry key from a specified location to either the local computer or a remote computer.
-    It establishes a remote PowerShell session if the ComputerName parameter is provided, copies the registry file to the remote machine, imports the registry key using the 'reg.exe' command, and removes the copied file afterwards. If no ComputerName is specified, the function imports the registry key locally.
-
-    .PARAMETER ImportPath
-    Specifies the path of the registry key to be imported.
-    .PARAMETER RemoteImportPath
-    Path where the registry key will be temporarily stored on the remote computer before importing.
-    .PARAMETER ComputerName
-    Name of the remote computer where the registry key will be imported.
-    .PARAMETER User
-    User account to be used for authentication when connecting to the remote computer.
-    .PARAMETER Pass
-    Password for the user account used for authentication.
+    This function allows importing a registry key from a specified .reg file into the local or remote computer. If a remote computer is specified, it transfers the registry file to the remote system, imports it using `reg.exe`, and cleans up the temporary file.
 
     .EXAMPLE
-    Import-RegistryKey -ImportPath "$env:USERPROFILE\Desktop\import.reg" -ComputerName "remote_host" -User "remote_user" -Pass "remote_pass" -Verbose
+    Import-RegistryKey -ImportPath "$env:USERPROFILE\Desktop\local_reg_export.reg" -Verbose
+    Import-RegistryKey -ImportPath "$env:USERPROFILE\Desktop\local_reg_export.reg" -ComputerName "remote_host" -User "remote_user" -Pass "remote_pass"
 
     .NOTES
-    v0.1.2
+    v0.5.4
     #>
     [CmdletBinding()]
     param (
@@ -31,56 +20,67 @@ Function Import-RegistryKey {
         [Alias("i")]
         [string]$ImportPath,
 
-        [Parameter(Mandatory = $false, HelpMessage = "Specifies the temporary path on the remote computer where the registry key will be stored before importing")]
+        [Parameter(Mandatory = $false, HelpMessage = "Temporary path on the remote computer where the registry key will be stored before importing")]
         [Alias("r")]
         [string]$RemoteImportPath = "C:\Users\Default\AppData\Local\Temp\import.reg",
 
-        [Parameter(Mandatory = $false, HelpMessage = "Specifies the name of the remote computer where the registry key will be imported")]
+        [Parameter(Mandatory = $false, HelpMessage = "Name of the remote computer where the registry key will be imported")]
         [Alias("c")]
         [string]$ComputerName,
 
-        [Parameter(Mandatory = $false, HelpMessage = "Specifies the user account used for authentication when connecting to the remote computer")]
+        [Parameter(Mandatory = $false, HelpMessage = "User account used for authentication when connecting to the remote computer")]
         [Alias("u")]
         [string]$User,
-        
-        [Parameter(Mandatory = $false, HelpMessage = "Specifies the password for the user account used for authentication")]
+
+        [Parameter(Mandatory = $false, HelpMessage = "Password for the user account used for authentication")]
         [Alias("p")]
         [string]$Pass
     )
     try {
+        $Session = $null
         if ($ComputerName) {
-            $Session = New-PSSession -ComputerName $ComputerName -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (ConvertTo-SecureString -String $Pass -AsPlainText -Force))
-            $TestConnectionScriptBlock = {
-                (Test-Connection -ComputerName $Using:ComputerName -Count 1 -Quiet) -and (Test-WSMan -ComputerName $Using:ComputerName -Authentication Default)
-            }
-            $isComputerReachable = Invoke-Command -Session $Session -ScriptBlock $TestConnectionScriptBlock
-            if ($isComputerReachable) {
+            $SecurePassword = ConvertTo-SecureString -String $Pass -AsPlainText -Force
+            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $SecurePassword
+            $IsComputerReachable = Test-Connection -ComputerName $ComputerName -Count 1 -Quiet
+            if ($IsComputerReachable) {
+                Write-Verbose -Message "Connecting to remote computer: $ComputerName"
+                $Session = New-PSSession -ComputerName $ComputerName -Credential $Credential
                 $ImportScriptBlock = {
                     param($Path)
                     Start-Process -FilePath 'reg.exe' -ArgumentList 'import', $Path -Wait -NoNewWindow
                 }
-                Copy-Item -Path $ImportPath -Destination $RemoteImportPath -ToSession $Session -Force -Verbose
+                Write-Verbose -Message "Copying registry file to remote computer..."
+                Copy-Item -Path $ImportPath -Destination $RemoteImportPath -ToSession $Session -Force
+                Write-Verbose -Message "Starting registry import on remote computer..."
                 Invoke-Command -Session $Session -ScriptBlock $ImportScriptBlock -ArgumentList $RemoteImportPath
                 Invoke-Command -Session $Session -ScriptBlock {
                     param($Path)
-                    Remove-Item $Path -Force -Verbose
+                    Remove-Item -Path $Path -Force -Verbose
                 } -ArgumentList $RemoteImportPath
+                Write-Host "Registry key successfully imported on remote computer '$ComputerName' from '$ImportPath'" -ForegroundColor Green
             }
             else {
-                Write-Error -Message "Failed to establish a connection to the remote computer '$ComputerName'."
+                Write-Error -Message "Failed to connect to remote computer: $ComputerName"
             }
         }
         else {
+            Write-Verbose -Message "Importing registry key on local machine..."
             Start-Process -FilePath 'reg.exe' -ArgumentList 'import', $ImportPath -Wait -NoNewWindow
+            if (Test-Path $ImportPath) {
+                Write-Host "Registry key successfully imported on local machine from '$ImportPath'" -ForegroundColor Green
+            }
+            else {
+                Write-Error -Message "Failed to import registry key from '$ImportPath'"
+            }
         }
-        Write-Host "Registry key imported from '$ImportPath'." -ForegroundColor Green
     }
     catch {
-        Write-Error -Message $_.Exception.Message
+        Write-Error -Message "Error occurred: $_"
     }
     finally {
         if ($Session) {
-            Remove-PSSession -Session $Session -Verbose
+            Write-Verbose -Message "Closing remote session..."
+            Remove-PSSession -Session $Session
         }
     }
 }
