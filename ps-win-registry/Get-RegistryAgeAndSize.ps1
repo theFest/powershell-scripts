97170788d54ce3852245d5e9ec6bc1468f8780a5
theFest <8981383+theFest@users.noreply.github.com>
2024-09-16 13:00:05 +0200

    Update Get-RegistryAgeAndSize
    remote support, aligned, etc.

--- ps-win-registry/Get-RegistryAgeAndSize.ps1
+++ ps-win-registry/Get-RegistryAgeAndSize.ps1
@@ -1,60 +1,74 @@
-Function Get-RegistryAgeAndSize {
+function Get-RegistryAgeAndSize {
     <#
     .SYNOPSIS
-    Retrieves registry information including age and size from specified remote computers.
+    Retrieves the size, free space, and age of the registry on one or more remote or local computers.
 
     .DESCRIPTION
-    This function retrieves registry details, such as current size, maximum size, free space, percent free, age since installation, and status, from remote computers.
-
-    .PARAMETER ComputerName
-    Name of the remote computer(s) to retrieve registry information from.
-    .PARAMETER User
-    Specifies the username for authentication on remote computers.
-    .PARAMETER Pass
-    Specifies the password for authentication on remote computers.
+    This function retrieves information about the Windows registry, including the current size, maximum size, free space, and the age of the registry since its installation on the specified computer(s).
 
     .EXAMPLE
-    Get-RegistryAgeAndSize -ComputerName "remote_host" -Username "remote_user" -Pass "remote_pass"
+    Get-RegistryAgeAndSize -Verbose
+    Get-RegistryAgeAndSize -ComputerName "remote_host" -User "remote_user" -Pass "remote_pass"
 
     .NOTES
-    v0.0.1
+    v0.2.3
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Specify the computer name(s)")]
         [string[]]$ComputerName = $env:COMPUTERNAME,
-        
-        [Parameter(Mandatory = $false)]
+
+        [Parameter(Mandatory = $false, HelpMessage = "Username for remote authentication, if not provided, the current user is assumed")]
         [string]$User,
-        
-        [Parameter(Mandatory = $false)]
+
+        [Parameter(Mandatory = $false, HelpMessage = "Password for remote authentication, must be used in conjunction with the User parameter")]
         [string]$Pass
     )
     BEGIN {
-        Write-Verbose -Message "Starting $($MyInvocation.MyCommand)"
-        if ($User -or $Pass) {
-            $SecurePassword = ConvertTo-SecureString -String $Pass -AsPlainText -Force
-            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $SecurePassword
+        Write-Verbose -Message "Starting function: $($MyInvocation.MyCommand)"
+        if ($User -and $Pass) {
+            try {
+                $SecurePassword = ConvertTo-SecureString -String $Pass -AsPlainText -Force
+                $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $SecurePassword
+                Write-Verbose -Message "Credentials created successfully."
+            }
+            catch {
+                Write-Error -Message "Failed to create credentials: $_"
+                return
+            }
         }
     }
     PROCESS {
         foreach ($Computer in $ComputerName) {
-            Write-Verbose -Message "Processing $Computer..."
+            Write-Verbose -Message "Processing computer: $Computer"
             try {
-                $Data = Get-WmiObject -Class Win32_Registry -ComputerName $Computer -Credential $Credential -ErrorAction Stop
-                $Data | Select-Object -Property @{Name = "ComputerName"; Expression = { $_.__SERVER } },
-                Status, CurrentSize, MaximumSize, @{Name = "FreeSize"; Expression = { $_.MaximumSize - $_.CurrentSize } },
-                @{Name = "PercentFree"; Expression = { (1 - ($_.CurrentSize / $_.MaximumSize)) * 100 } },
-                @{Name = "Created"; Expression = { $_.ConvertToDateTime($_.InstallDate) } },
-                @{Name = "Age"; Expression = { (Get-Date) - ( $_.ConvertToDateTime($_.InstallDate)) } }
+                if ($Credential) {
+                    $Data = Get-WmiObject -Class Win32_Registry -ComputerName $Computer -Credential $Credential -ErrorAction Stop
+                }
+                else {
+                    $Data = Get-WmiObject -Class Win32_Registry -ComputerName $Computer -ErrorAction Stop
+                }
+                $Results = $Data | Select-Object -Property @{
+                    Name = "ComputerName"; Expression = { $_.__SERVER }
+                }, Status, CurrentSize, MaximumSize, @{
+                    Name = "FreeSize"; Expression = { $_.MaximumSize - $_.CurrentSize }
+                }, @{
+                    Name = "PercentFree"; Expression = { (1 - ($_.CurrentSize / $_.MaximumSize)) * 100 }
+                }, @{
+                    Name = "Created"; Expression = { $_.ConvertToDateTime($_.InstallDate) }
+                }, @{
+                    Name = "Age"; Expression = { (Get-Date) - $_.ConvertToDateTime($_.InstallDate) }
+                }
+                Write-Output -InputObject $Results
             }
             catch {
-                Write-Warning -Message "Failed to retrieve registry information from $($Computer.ToUpper())!"
-                Write-Error -Exception $_.Exception.Message
+                Write-Warning -Message "Failed to retrieve registry information from $Computer."
+                Write-Error -Message "Error details: $($_.Exception.Message)"
             }
         }
     }
+
     END {
-        Write-Verbose -Message "Ending $($MyInvocation.MyCommand)"
+        Write-Verbose -Message "Completed function: $($MyInvocation.MyCommand)"
     }
 }
