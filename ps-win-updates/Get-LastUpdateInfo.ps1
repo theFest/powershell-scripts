e72d6b81f3bae0a4fcf778befbbef71741d3f0e0
theFest <8981383+theFest@users.noreply.github.com>
2024-10-15 00:25:21 +0200

    u Get-LastUpdateInfo
    help, error handle, etc.

--- ps-win-updates/Get-LastUpdateInfo.ps1
+++ ps-win-updates/Get-LastUpdateInfo.ps1
@@ -1,4 +1,4 @@
-Function Get-LastUpdateInfo {
+function Get-LastUpdateInfo {
     <#
     .SYNOPSIS
     Retrieves information about the last Windows Update check time and scan date.
@@ -6,63 +6,61 @@ Function Get-LastUpdateInfo {
     .DESCRIPTION
     This function fetches information about the last Windows Update check time and scan date. It can be used both locally and remotely, with optional credentials for remote access.
 
-    .PARAMETER ComputerName
-    Name of the target computer, defaults to the local computer.
-    .PARAMETER User
-    Specifies the username for remote computer access.
-    .PARAMETER Pass
-    Specifies the password for remote computer access.
-
     .EXAMPLE
-    Get-LastUpdateInfo
-    Get-LastUpdateInfo -ComputerName "remote_host" -Username "remote_user" -Password "remote_pass"
+    Get-LastUpdateInfo -Verbose
+    Get-LastUpdateInfo -ComputerName "remote_host" -User "remote_user" -Pass "remote_pass"
 
     .NOTES
-    v0.0.1
+    v0.1.6
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false)]
-        [string]$ComputerName = $env:COMPUTERNAME,
+        [Parameter(Mandatory = $false, HelpMessage = "Name of the remote computer to check, if not provided, the function checks the local system")]
+        [string]$ComputerName,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, HelpMessage = "Username for authenticating to the remote computer, required for remote checks")]
         [string]$User,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, HelpMessage = "Password for authenticating to the remote computer, required for remote checks")]
         [string]$Pass
     )
     if ($ComputerName -ne $env:COMPUTERNAME -and (-not $User -or -not $Pass)) {
-        Write-Warning -Message "Please provide both Username and Password for remote computer access."
+        Write-Warning "Please provide both Username and Password for remote computer access."
         return
     }
-    $AutomaticUpdates = New-Object -ComObject Microsoft.Update.AutoUpdate
-    $LastCheckTime = $AutomaticUpdates.Results.LastSearchSuccessDate
-    Write-Host "Last Windows Update check time on ${ComputerName}: $LastCheckTime" -ForegroundColor Green
-    $SessionOption = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
-    $SessionCredential = $null
-    if ($Pass) {
-        $SessionCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (ConvertTo-SecureString -String $Pass -AsPlainText -Force)
-    }
-    $RemoteScriptBlock = {
+    $ScriptBlock = {
         $UpdateSearcher = (New-Object -ComObject Microsoft.Update.Session).CreateUpdateSearcher()
+        $AutomaticUpdates = New-Object -ComObject Microsoft.Update.AutoUpdate
+        $LastCheckTime = $AutomaticUpdates.Results.LastSearchSuccessDate
+        Write-Host "Last Windows Update check time on $($env:COMPUTERNAME): $LastCheckTime" -ForegroundColor Green
         $History = $UpdateSearcher.GetTotalHistoryCount()
         if ($History -gt 0) {
             $LastScanDate = $UpdateSearcher.QueryHistory(0, $History) | 
             Sort-Object -Property Date -Descending | 
             Select-Object -First 1 -ExpandProperty Date
-
             Write-Host "Last Windows Update scan date on $($env:COMPUTERNAME): $LastScanDate" -ForegroundColor Green
         }
         else {
-            Write-Warning -Message "No Windows Update scan history found on $($env:COMPUTERNAME)!"
+            Write-Warning "No Windows Update scan history found on $($env:COMPUTERNAME)!"
         }
     }
-    if ($ComputerName -ne $env:COMPUTERNAME) {
-        $RemoteSession = New-PSSession -ComputerName $ComputerName -Credential $SessionCredential -SessionOption $SessionOption
-        Invoke-Command -ScriptBlock $RemoteScriptBlock -Session $RemoteSession
-        Remove-PSSession -Session $RemoteSession -Verbose
+    if ($ComputerName -eq $env:COMPUTERNAME) {
+        & $ScriptBlock
     }
     else {
-        Invoke-Command -ScriptBlock $RemoteScriptBlock
+        $SessionOption = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
+        $SessionCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (ConvertTo-SecureString -String $Pass -AsPlainText -Force)
+        try {
+            $RemoteSession = New-PSSession -ComputerName $ComputerName -Credential $SessionCredential -SessionOption $SessionOption
+            Invoke-Command -Session $RemoteSession -ScriptBlock $ScriptBlock
+        }
+        catch {
+            Write-Error "Failed to connect to $ComputerName. Error: $_"
+        }
+        finally {
+            if ($RemoteSession) {
+                Remove-PSSession -Session $RemoteSession -Verbose
+            }
+        }
     }
 }
