8430a75c6e35e76d632651db28fa57839181b571
theFest <8981383+theFest@users.noreply.github.com>
2024-10-15 00:27:25 +0200

    u Get-WindowsUpdateDiskUsage
    minor addons and checks

--- ps-win-updates/Get-WindowsUpdateDiskUsage.ps1
+++ ps-win-updates/Get-WindowsUpdateDiskUsage.ps1
@@ -1,26 +1,17 @@
-Function Get-WindowsUpdateDiskUsage {
+function Get-WindowsUpdateDiskUsage {
     <#
     .SYNOPSIS
-    Gets disk usage information, including space used by Windows Updates.
+    Retrieves disk usage statistics related to Windows Updates, including total space used by Windows updates.
 
     .DESCRIPTION
-    This function retrieves information about the disk space on a specified drive, including the space used by Windows Updates in the 'SoftwareDistribution\Download' directory.
-
-    .PARAMETER DriveLetter
-    Drive letter for which disk usage information is to be retrieved.
-    .PARAMETER ComputerName
-    Name of the remote computer, if specified, the function retrieves disk usage information from the specified remote computer.
-    .PARAMETER User
-    Specifies the username for authentication on the remote computer.
-    .PARAMETER Pass
-    Specifies the password for authentication on the remote computer.
+    This function calculates the total disk space used by Windows updates on a specified drive, can be run on both local and remote systems. For remote systems, the function accepts credentials to securely retrieve the information and provides details such as total drive size, free space, space used by Windows updates, and space used excluding the Windows updates.
 
     .EXAMPLE
     Get-WindowsUpdateDiskUsage
     Get-WindowsUpdateDiskUsage -ComputerName "remote_host" -User "remote_user" -Pass "remote_pass"
 
     .NOTES
-    v0.0.1
+    v0.1.0
     #>
     [CmdletBinding()]
     param (
@@ -36,36 +27,46 @@ Function Get-WindowsUpdateDiskUsage {
         [Parameter(Mandatory = $false)]
         [string]$Pass
     )
+    if ($DriveLetter.Length -ne 1 -or $DriveLetter -notmatch '^[A-Za-z]$') {
+        Write-Error -Message "Invalid Drive Letter. Please specify a single letter (e.g., C)."
+        return
+    }
+    $WindowsUpdatePath = Join-Path -Path $env:SystemRoot -ChildPath 'SoftwareDistribution\Download'
     try {
-        $WindowsUpdatePath = Join-Path -Path $env:SystemRoot -ChildPath 'SoftwareDistribution\Download'
         if ($ComputerName -ne $env:COMPUTERNAME) {
+            if (-not $User -or -not $Pass) {
+                Write-Error -Message "Username and Password are required for remote access."
+                return
+            }
             $SecPass = ConvertTo-SecureString $Pass -AsPlainText -Force
             $Credential = New-Object System.Management.Automation.PSCredential ($User, $SecPass)
             $UsedSpaceGB = Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
                 param($Path)
                 if (Test-Path -Path $Path) {
                     $TotalSize = Get-ChildItem -Path $Path -Recurse -File | Measure-Object -Property Length -Sum
-                    $TotalSize.Sum / 1GB
+                    return [math]::Round($TotalSize.Sum / 1GB, 2)
                 }
-                else {
-                    0
-                }
+                return 0
             } -ArgumentList $WindowsUpdatePath -ErrorAction Stop
         }
         else {
             if (Test-Path -Path $WindowsUpdatePath) {
                 $TotalSize = Get-ChildItem -Path $WindowsUpdatePath -Recurse -File | Measure-Object -Property Length -Sum
-                $UsedSpaceGB = $TotalSize.Sum / 1GB
+                $UsedSpaceGB = [math]::Round($TotalSize.Sum / 1GB, 2)
             }
             else {
                 $UsedSpaceGB = 0
             }
         }
         $Volume = Get-Volume -DriveLetter $DriveLetter -ErrorAction Stop
+        $TotalSizeGB = [math]::Round($Volume.Size / 1GB, 2)
         $TotalFreeSpaceGB = [math]::Round($Volume.SizeRemaining / 1GB, 2)
-        Write-Host "Total Free Space on Drive $DriveLetter : ${TotalFreeSpaceGB}GB"
-        Write-Host "Used Space for Windows Updates: ${UsedSpaceGB}GB"
-        Write-Host "Used Space (excluding Windows Updates): $([math]::Round(($Volume.Size - $Volume.SizeRemaining - $UsedSpaceGB), 2))GB"
+        $UsedSpaceExcludingUpdatesGB = [math]::Round(($TotalSizeGB - $TotalFreeSpaceGB - $UsedSpaceGB), 2)
+        Write-Host "Drive Letter: $DriveLetter" -ForegroundColor Yellow
+        Write-Host "Total Size of Drive: ${TotalSizeGB}GB" -ForegroundColor DarkYellow
+        Write-Host "Total Free Space on Drive: ${TotalFreeSpaceGB}GB" -ForegroundColor DarkCyan
+        Write-Host "Used Space for Windows Updates: ${UsedSpaceGB}GB" -ForegroundColor Green
+        Write-Host "Used Space (excluding Windows Updates): ${UsedSpaceExcludingUpdatesGB}GB" -ForegroundColor Gray
     }
     catch {
         Write-Error -Message "Error: $($_.Exception.Message)"
