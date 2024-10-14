a98070b04fff070c4109da72f083a0ffcd8cdf0d
theFest <8981383+theFest@users.noreply.github.com>
2024-10-15 00:23:53 +0200

    u Export-WindowsUpdateHistory
    remote, help, aligned.

--- ps-win-updates/Export-WindowsUpdateHistory.ps1
+++ ps-win-updates/Export-WindowsUpdateHistory.ps1
@@ -1,4 +1,4 @@
-Function Export-WindowsUpdateHistory {
+function Export-WindowsUpdateHistory {
     <#
     .SYNOPSIS
     Exports Windows Update history to a CSV file.
@@ -6,31 +6,56 @@ Function Export-WindowsUpdateHistory {
     .DESCRIPTION
     This function retrieves information about installed updates using the Get-HotFix cmdlet and exports the data to a CSV file, exported file includes details such as HotFixID, Description, and InstalledOn.
 
-    .PARAMETER OutputPath
-    Path where the CSV file will be saved. If not provided, the file will be saved on the user's desktop with the default name "win_update_history.csv".
-    .PARAMETER NumberOfEntries
-    Specifies the number of update entries to include in the exported history, defaults to 100 if not specified.
-
     .EXAMPLE
-    Export-WindowsUpdateHistory
+    Export-WindowsUpdateHistory -Verbose
+    Export-WindowsUpdateHistory -ComputerName "remote_host" -User "remote_user" -Pass "remote_pass"
 
     .NOTES
-    v0.0.1
+    v0.3.0
     #>
     param (
-        [Parameter(Mandatory = $false, Position = 0)]
+        [Parameter(Mandatory = $false, Position = 2, HelpMessage = "Specify the remote computer name")]
+        [string]$ComputerName = $env:COMPUTERNAME,
+
+        [Parameter(Mandatory = $false, Position = 3, HelpMessage = "Provide the username for remote authentication")]
+        [string]$User,
+
+        [Parameter(Mandatory = $false, Position = 4, HelpMessage = "Provide the password for the specified username")]
+        [string]$Pass,
+
+        [Parameter(Mandatory = $false, Position = 0, HelpMessage = "Specify the output path for the CSV file")]
         [string]$OutputPath = "$env:USERPROFILE\Desktop\win_update_history.csv",
 
-        [Parameter(Mandatory = $false, Position = 1)]
+        [Parameter(Mandatory = $false, Position = 1, HelpMessage = "Specify the number of update entries to export")]
         [int]$NumberOfEntries = 100
     )
     try {
-        $UpdateHistory = Get-HotFix | Select-Object -First $NumberOfEntries
-        if ($null -eq $UpdateHistory) {
-            throw "Failed to query update history, no data available!"
+        Write-Host "Retrieving Windows Update history from $ComputerName..." -ForegroundColor Cyan
+        $Credential = $null
+        if ($User -and $Pass) {
+            $SecurePassword = ConvertTo-SecureString -String $Pass -AsPlainText -Force
+            $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $SecurePassword
         }
-        $UpdateHistory | Export-Csv -Path $OutputPath -NoTypeInformation
-        Write-Host "WU history exported to $OutputPath" -ForegroundColor Cyan
+        $ScriptBlock = {
+            param (
+                [int]$NumberOfEntries
+            )
+            Get-HotFix | Select-Object -First $NumberOfEntries
+        }
+        if ($ComputerName -ne $env:COMPUTERNAME) {
+            Write-Host "Establishing a remote session with $ComputerName..." -ForegroundColor DarkCyan
+            $Session = New-PSSession -ComputerName $ComputerName -Credential $Credential -ErrorAction Stop
+            $UpdateHistory = Invoke-Command -Session $Session -ScriptBlock $ScriptBlock -ArgumentList $NumberOfEntries
+            Remove-PSSession -Session $Session -Verbose -ErrorAction SilentlyContinue
+        }
+        else {
+            $UpdateHistory = & $ScriptBlock -NumberOfEntries $NumberOfEntries
+        }
+        if ($null -eq $UpdateHistory -or $UpdateHistory.Count -eq 0) {
+            throw "No update history data available!"
+        }
+        $UpdateHistory | Export-Csv -Path $OutputPath -NoTypeInformation -Force
+        Write-Host "Windows Update history exported to $OutputPath" -ForegroundColor Green
     }
     catch {
         Write-Error -Message "Error: $_"
