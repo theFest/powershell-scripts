fcf5d0f66aacdfbadf6b191e09a50e5862199d84
theFest <8981383+theFest@users.noreply.github.com>
2024-10-16 23:48:06 +0200

    u Uninstall-Update
    remote, aligned, etc.

--- ps-win-updates/Uninstall-Update.ps1
+++ ps-win-updates/Uninstall-Update.ps1
@@ -1,83 +1,131 @@
-Function Uninstall-Update {
+function Uninstall-Update {
     <#
     .SYNOPSIS
-    Uninstalls a specified Windows update.
+    Uninstalls a specified Windows update locally or on a remote computer.
 
     .DESCRIPTION
-    This function allows you to interactively choose and uninstall a Windows update, provides a list of currently installed updates and prompts you to select the update to uninstall.
-
-    .PARAMETER UpdateTitle
-    Specifies the title of the update to be uninstalled. If not provided, the function displays a list of installed updates, and you can choose the update to uninstall interactively.
+    This function allows you to interactively choose and uninstall one or more Windows updates based on their title and KB number. It supports both local and remote uninstallation and provides detailed feedback for each operation, including reboot requirements.
 
     .EXAMPLE
-    Uninstall-Update
+    Uninstall-Update -Verbose
+    Uninstall-Update -ComputerName "remote_host" -User "remote_user" -Pass "remote_pass"
 
     .NOTES
-    v0.0.1
+    v0.3.2
     #>
-    [CmdletBinding()]
+    [CmdletBinding(ConfirmImpact = "High")]
     param (
-        [Parameter(Mandatory = $false)]
-        [string]$UpdateTitle
+        [Parameter(Mandatory = $false, HelpMessage = "Specifies the title or KB number of the update to uninstall")]
+        [string]$UpdateTitle,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Name of the remote computer to uninstall updates from, defaults to the local computer")]
+        [string]$ComputerName = $env:COMPUTERNAME,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Username for remote authentication")]
+        [string]$User,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Password for remote authentication")]
+        [string]$Pass,
+
+        [Parameter(Mandatory = $false, HelpMessage = "Allows selecting and uninstalling multiple updates")]
+        [switch]$MultipleUpdates
     )
     BEGIN {
-        $UpdateSession = New-Object -ComObject Microsoft.Update.Session
-        $UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
-        $SearchResult = $UpdateSearcher.Search("IsInstalled=1")
-        $UpdateTitles = $SearchResult.Updates | ForEach-Object { $_.Title }
-        if ($UpdateTitles.Count -eq 0) {
-            Write-Host "No updates found" -ForegroundColor DarkGreen
-            return
+        Write-Host "Starting update uninstallation on $ComputerName..." -ForegroundColor Cyan
+        if ($Pass) {
+            $SecurePassword = ConvertTo-SecureString -String $Pass -AsPlainText -Force
         }
-        Write-Host "Choose an update to uninstall:" -ForegroundColor DarkCyan
-        for ($i = 0; $i -lt $UpdateTitles.Count; $i++) {
-            Write-Host "$($i + 1). $($UpdateTitles[$i])"
-        }
-        if (-not $UpdateTitle) {
-            $Choice = Read-Host "Enter the number of the update to uninstall"
-            if ($Choice -ge 1 -and $Choice -le $UpdateTitles.Count) {
-                $UpdateTitle = $UpdateTitles[$Choice - 1]
+        $Session = $null
+        if ($ComputerName -ne $env:COMPUTERNAME) {
+            try {
+                $RemoteCredential = New-Object PSCredential ($User, $SecurePassword)
+                $Session = New-PSSession -ComputerName $ComputerName -Credential $RemoteCredential
+                Write-Host "Established session with $ComputerName." -ForegroundColor Green
             }
-            else {
-                Write-Host "Invalid choice. Please enter a valid number!" -ForegroundColor DarkRed
+            catch {
+                Write-Error "Failed to establish a session with $ComputerName. Error: $_"
                 return
             }
         }
-    }
-    PROCESS {
-        $UpdateToUninstall = $SearchResult.Updates | Where-Object { $_.Title -eq $UpdateTitle }
-        if ($null -eq $UpdateToUninstall) {
-            Write-Host "Update '$UpdateTitle' not found."
-        }
-        elseif ($UpdateToUninstall.IsInstalled -and $UpdateToUninstall.EulaAccepted) {
-            $UpdateInstaller = $UpdateSession.CreateUpdateInstaller()
-            $UpdateInstaller.Updates = New-Object -ComObject Microsoft.Update.UpdateColl
-            $UpdateInstaller.Updates.Add($UpdateToUninstall)
-            try {
-                $UninstallResult = $UpdateInstaller.Uninstall()
-                if ($UninstallResult.ResultCode -eq 2) {
-                    Write-Host "Update '$UpdateTitle' uninstalled successfully" -ForegroundColor Green
-                }
-                else {
-                    Write-Error -Message "Failed to uninstall update. Result code: $($UninstallResult.ResultCode)"
+        $ScriptBlock = {
+            $updates = dism /online /get-packages | Select-String -Pattern "Package Identity" | Where-Object { $_ -match "KB" }
+            $updates -replace ".*: ", "" | ForEach-Object {
+                [PSCustomObject]@{
+                    PackageIdentity = $_
+                    KB              = ($_ -split " ")[0] -replace "KB", ""
                 }
             }
-            catch [System.Runtime.InteropServices.COMException] {
-                if ($_.Exception.HResult -eq 0x80240028) {
-                    Write-Host "Failed to uninstall update. The update may not be applicable for uninstallation" -ForegroundColor DarkYellow
+        }
+        if ($Session) {
+            $InstalledUpdates = Invoke-Command -Session $Session -ScriptBlock $ScriptBlock
+        }
+        else {
+            $InstalledUpdates = & $ScriptBlock
+        }
+        if ($InstalledUpdates.Count -eq 0) {
+            Write-Host "No installed updates found on $ComputerName." -ForegroundColor DarkGreen
+            return
+        }
+    }
+    PROCESS {
+        if (-not $UpdateTitle) {
+            Write-Host "Available updates on $ComputerName :" -ForegroundColor DarkCyan
+            for ($i = 0; $i -lt $InstalledUpdates.Count; $i++) {
+                Write-Host "$($i + 1). $($InstalledUpdates[$i].PackageIdentity) (KB$($InstalledUpdates[$i].KB))"
+            }
+            if ($MultipleUpdates) {
+                $Choices = Read-Host "Enter the numbers of updates to uninstall (comma-separated)"
+                $UpdateIndexes = $Choices.Split(',') | ForEach-Object { $_.Trim() - 1 }
+                $UpdateTitlesToUninstall = $UpdateIndexes | ForEach-Object { $InstalledUpdates[$_].PackageIdentity }
+            }
+            else {
+                $Choice = Read-Host "Enter the number of the update to uninstall"
+                if ($Choice -ge 1 -and $Choice -le $InstalledUpdates.Count) {
+                    $UpdateTitle = $InstalledUpdates[$Choice - 1].PackageIdentity
+                    $UpdateTitlesToUninstall = @($InstalledUpdates[$Choice - 1].PackageIdentity)
                 }
                 else {
-                    Write-Error -Message "Failed to uninstall update. Error: $($_.Exception.Message)"
+                    Write-Host "Invalid choice. Exiting..." -ForegroundColor DarkRed
+                    return
                 }
             }
         }
         else {
-            Write-Warning -Message "Update '$UpdateTitle' is not installed or EULA not accepted!"
+            $UpdateTitlesToUninstall = $InstalledUpdates | Where-Object { $_.PackageIdentity -like "*$UpdateTitle*" -or $_.KB -eq $UpdateTitle } | ForEach-Object { $_.PackageIdentity }
+            if ($UpdateTitlesToUninstall.Count -eq 0) {
+                Write-Host "Update '$UpdateTitle' not found on $ComputerName."
+                return
+            }
+        }
+        foreach ($Update in $UpdateTitlesToUninstall) {
+            $UninstallBlock = {
+                param ($UpdateIdentity)
+                $result = dism /online /remove-package /PackageName:$UpdateIdentity
+                return $result
+            }
+            try {
+                if ($Session) {
+                    $UninstallResult = Invoke-Command -Session $Session -ScriptBlock $UninstallBlock -ArgumentList $Update
+                }
+                else {
+                    $UninstallResult = & $UninstallBlock $Update
+                }
+                if ($UninstallResult -match "Successfully") {
+                    Write-Host "Update '$Update' uninstalled successfully from $ComputerName." -ForegroundColor Green
+                }
+                else {
+                    Write-Error "Failed to uninstall '$Update' from $ComputerName. Result: $UninstallResult"
+                }
+            }
+            catch {
+                Write-Error "Error uninstalling update '$Update'. Error: $($_.Exception.Message)"
+            }
         }
     }
     END {
-        if ($UpdateSession) {
-            [System.Runtime.InteropServices.Marshal]::ReleaseComObject($UpdateSession) | Out-Null
+        if ($Session) {
+            Remove-PSSession -Session $Session -Verbose
         }
+        Write-Host "Update uninstallation completed on $ComputerName." -ForegroundColor Cyan
     }
 }
